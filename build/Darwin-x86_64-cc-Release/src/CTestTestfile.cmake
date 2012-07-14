1afb94dca34f74175afe0b3db4464cc38859aa97
Jeffrey Johnson <jjphatt@gmail.com>
2012-07-13 22:03:04 -0700

    Beginning of name change!
    Former-commit-id: c4258cd4aeb425ffc50dfdb609ed518a3f38ddbe

--- build/Darwin-x86_64-cc-Release/src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/jjphatt/clide/src
-# Build directory: /Users/jjphatt/clide/build/Darwin-x86_64-cc-Release/src
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
