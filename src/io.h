69e3a718ac3f087f36729c463fef5c0927807c67
Jeffrey Johnson <jjphatt@gmail.com>
2012-07-21 15:12:10 -0700

    Added a space-time function.
    Former-commit-id: d48d2fbe2502b0f22792bfda80f75b827b981717

--- src/io.h
+++ src/io.h
@@ -13,13 +13,33 @@ extern "C" {
 // read and write parallel data.
 typedef struct io_interface_t io_interface_t;
 
-// Modes for descriptor operation.
+// Modes for mesh descriptor operation.
 typedef enum
 {
   IO_CLOSED,
   IO_READ,
   IO_WRITE
 } io_mode_t;
+
+// This iterator allows one to traverse the data sets on heavy meshes 
+// in a descriptor.
+typedef struct io_mesh_iter io_mesh_iter;
+
+// This iterator allows one to traverse the data sets on light meshes 
+// in a descriptor.
+typedef struct io_lite_mesh_iter io_lite_mesh_iter;
+
+// This iterator allows one to traverse fields on a mesh.
+typedef struct io_field_iter io_field_iter;
+
+// Field centerings.
+typedef enum
+{
+  IO_NODE,
+  IO_EDGE,
+  IO_FACE,
+  IO_CELL
+} io_field_centering_t;
 
 // A function pointer type for opening file descriptors.
 typedef int (*io_open_func)(void*, const char*, const char*, io_mode_t, int, MPI_Comm, int, int);
@@ -28,8 +48,10 @@ typedef int (*io_open_func)(void*, const char*, const char*, io_mode_t, int, MPI
 typedef int (*io_close_func)(void*);
 
 // Function pointer types for querying meshes from a descriptor.
-typedef int (*io_query_meshes_func)(void*, char**, int*);
-typedef int (*io_query_lite_meshes_func)(void*, char**, int*);
+typedef int (*io_query_num_meshes_func)(void*, int*);
+typedef int (*io_query_mesh_names_func)(void*, char**);
+typedef int (*io_query_num_lite_meshes_func)(void*, int*);
+typedef int (*io_query_lite_meshe_names_func)(void*, char**);
 
 // A function pointer type for reading a (heavy) mesh from an open descriptor.
 typedef int (*io_read_mesh_func)(void*, const char*, mesh_t*);
@@ -38,8 +60,9 @@ typedef int (*io_read_mesh_func)(void*, const char*, mesh_t*);
 typedef int (*io_read_lite_mesh_func)(void*, const char*, lite_mesh_t*);
 
 // Function pointer types for querying data fields from a descriptor.
-typedef int (*io_query_fields_func)(void*, char**, int*);
-typedef int (*io_query_field_func)(void*, const char*, int*, int*);
+typedef int (*io_query_num_fields_func)(void*, const char*, int*);
+typedef int (*io_query_field_names_func)(void*, const char*, char**);
+typedef int (*io_query_field_func)(void*, const char*, const char*, int*, io_field_centering_t*);
 
 // A function pointer type for reading a data field from a descriptor.
 typedef int (*io_read_field_func)(void*, const char*, double*);
@@ -60,19 +83,24 @@ typedef void (*io_dtor)(void*);
 // within arbi.
 typedef struct 
 {
-  io_open_func              open;
-  io_close_func             close;
-  io_query_meshes_func      query_meshes;
-  io_query_lite_meshes_func query_lite_meshes;
-  io_read_mesh_func         read_mesh;
-  io_read_lite_mesh_func    read_lite_mesh;
-  io_query_fields_func      query_fields;
-  io_query_field_func       query_field;
-  io_read_field_func        read_field;
-  io_write_mesh_func        write_mesh;
-  io_write_lite_mesh_func   write_lite_mesh;
-  io_write_field_func       write_field;
-  io_dtor                   dtor;
+  io_open_func                  open;
+  io_close_func                 close;
+  io_query_num_meshes_func      query_num_meshes;
+  io_query_mesh_names_func      query_mesh_names;
+  io_query_mesh_func            query_mesh;
+  io_query_num_lite_meshes_func query_num_lite_meshes;
+  io_query_lite_mesh_names_func query_lite_mesh_names;
+  io_query_lite_mesh_func       query_lite_mesh;
+  io_read_mesh_func             read_mesh;
+  io_read_lite_mesh_func        read_lite_mesh;
+  io_query_num_fields_func      query_field_names;
+  io_query_field_names_func     query_field_names;
+  io_query_field_func           query_field;
+  io_read_field_func            read_field;
+  io_write_mesh_func            write_mesh;
+  io_write_lite_mesh_func       write_lite_mesh;
+  io_write_field_func           write_field;
+  io_dtor                       dtor;
 } io_vtable;
 
 // Construct an I/O interface (subclass) object from the given name and 
@@ -82,12 +110,11 @@ io_interface_t* io_interface_new(void* context, const char* name, io_vtable vtab
 // Frees the given I/O interface.
 void io_free(io_interface_t* interface);
 
-// Open a file descriptor with the given interface.
+// Open an I/O descriptor with the given interface.
 void io_open(io_interface_t* interface, 
              const char* prefix, 
              const char* directory,  
              io_mode_t mode,
-             int cycle, 
              MPI_Comm comm,
              int num_files,
              int mpi_tag);
@@ -95,36 +122,64 @@ void io_open(io_interface_t* interface,
 // Close the given file descriptor.
 void io_close(io_interface_t* interface);
 
-// Points meshes to an array of names of heavy meshes accessible by the descriptor.
-void io_query_meshes(io_interface_t* interface, char** mesh_names, int* num_meshes);
+// Returns an iterator that can be used to traverse the heavy mesh 
+// datasets available to this descriptor.
+io_mesh_iter* io_meshes(io_interface_t* interface);
 
-// Reads the heavy mesh with the given name from the descriptor.
-void io_read_mesh(io_interface_t* interface, const char* mesh_name, mesh_t* mesh);
+// Returns true if the given mesh iterator points to a mesh/dataset, 
+// false if it does not.
+bool io_mesh_iter_ok(io_mesh_iter* iter);
 
-// Points lite_meshes to an array of names of heavy meshes accessible by the descriptor.
-void io_query_lite_meshes(io_interface_t* interface, char** lite_mesh_names, int* num_lite_meshes);
+// Reads the heavy mesh from the descriptor, setting mesh_name to the name 
+// of the mesh/dataset.
+void io_mesh_iter_read(io_mesh_iter* iter, char** mesh_name, mesh_t* mesh);
 
-// A function pointer type for reading a light mesh from an open descriptor.
-void io_read_lite_mesh(io_interface_t* interface, const char* lite_mesh_name, lite_mesh_t* lite_mesh);
+// Writes a (heavy) mesh to an open descriptor via the given iterator.
+void io_mesh_iter_write(io_mesh_iter* iter, const char* mesh_name, mesh_t* mesh);
 
-// Points field_names at an array containing the names of the fields within 
-// the file.
-void io_query_fields(io_interface_t* interface, char** field_names, int* num_fields);
+// Increments the iterator.
+void io_mesh_iter_next(io_mesh_iter* iter);
 
-// Fills num_data and num_components with the values for the given field.
-void io_query_field(io_interface_t* interface, const char* field_name, int* num_data, int* num_components);
+// Returns an iterator that can traverse the fields for the dataset.
+io_field_iter* io_mesh_iter_fields(io_mesh_iter* iter);
 
-// Fills field_data with the data from the field.
-void io_read_field(io_interface_t* interface, const char* field_name, double* field_data);
+// Returns an iterator that can be used to traverse the lite mesh
+// datasets available to this descriptor.
+io_lite_mesh_iter* io_lite_meshes(io_interface_t* interface);
 
-// Writes a (heavy) mesh to an open descriptor.
-void io_write_mesh(io_interface_t* interface, const char* mesh_name, mesh_t* mesh);
+// Returns true if the given mesh iterator points to a lite mesh/dataset, 
+// false if it does not.
+bool io_lite_mesh_iter_ok(io_lite_mesh_iter* iter);
 
-// Writes a light mesh to an open descriptor.
-void io_write_lite_mesh(io_interface_t* interface, const char* lite_mesh_name, lite_mesh_t* lite_mesh);
+// Reads the lite mesh from the descriptor, setting mesh_name to the name 
+// of the mesh/dataset.
+void io_lite_mesh_iter_read(io_lite_mesh_iter* interface, char** mesh_name, lite_mesh_t* lite_mesh);
 
-// Writes a field to an open descriptor.
-void io_write_field(io_interface_t* interface, const char* field_name, double* field_data, int num_data, int num_components);
+// Writes a lite mesh to an open descriptor via the given iterator.
+void io_lite_mesh_iter_write(io_lite_mesh_iter* iter, const char* mesh_name, mesh_t* mesh);
+
+// Increments the iterator.
+void io_lite_mesh_iter_next(io_lite_mesh_iter* iter);
+
+// Returns an iterator that can traverse the fields for the dataset.
+io_field_iter* io_lite_mesh_iter_fields(io_lite_mesh_iter* iter);
+
+// Returns true if the given field iterator points to a field
+// false if it does not.
+bool io_field_iter_ok(io_field_iter* iter);
+
+// Increments the iterator.
+void io_field_iter_next(io_field_iter* iter);
+
+// Gathers metadata about the current field. The size of the field is 
+// consistent with its associated mesh.
+void io_query_field(io_field_iter* iter, const char** field_name, int* num_components, io_field_centering_t* centering);
+
+// Reads the field data from the iterator.
+void io_field_iter_read(io_field_iter* iter, double* field_data);
+
+// Writes a field to an open descriptor via the iterator.
+void io_field_iter_write(io_field_iter* iter, const char* field_name, double* field_data, int num_data, int num_components);
 
 #ifdef __cplusplus
 }
---
+++ src/st_func.h
@@ -0,0 +1,79 @@
+#ifndef ARBI_st_func_H
+#define ARBI_st_func_H
+
+#include "arbi.h"
+#include "point.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// A "space-time" function is an analytic function of space and time.
+// This opaque type encapsulates the notion of such an analytic function 
+// and any associated metadata (whether it is homogeneous, constant in 
+// time, etc).
+typedef struct st_func_t st_func_t;
+
+// Enumerated type indicating whether a function is homogeneous in space.
+typedef enum
+{
+  ST_HOMOGENEOUS,
+  ST_INHOMOGENEOUS
+} st_func_homogeneity_t;
+
+// Enumerated type indicating whether a function is constant in time.
+typedef enum
+{
+  ST_CONSTANT,
+  ST_NONCONSTANT
+} st_func_constancy_t;
+
+// A function pointer type for evaluating the function at a point.
+typedef void (*st_eval_func)(void*, point_t*, double, double*);
+
+// A destructor for any given context object.
+typedef void (*st_dtor)(void*);
+
+// This virtual table must be implemented by any space-time function.
+typedef struct 
+{
+  st_eval_func              eval;
+  st_dtor                       dtor;
+} st_vtable;
+
+// Construct a space-time function from the given context, metadata and vtable.
+st_func_t* st_func_new(const char* name, void* context, st_vtable vtable,
+                       st_func_homogeneity_t homogeneity,
+                       st_func_constancy_t constancy,
+                       int num_comp);
+
+// Construct a space-time function from a function pointer with the given metadata.
+st_func_t* st_func_from_func(const char* name, st_eval_func func, 
+                             st_func_homogeneity_t homogeneity,
+                             st_func_constancy_t constancy,
+                             int num_comp);
+
+// Frees the resources used by the function.
+void st_func_free(st_func_t* func);
+
+// Returns the name of the function.
+const char* st_func_name(st_func_t* func);
+
+// Returns true if the function is homogeneous in space, false if not.
+bool st_func_is_homogeneous(st_func_t* func);
+
+// Returns true if the function is constant in time, false if not.
+bool st_func_is_constant(st_func_t* func);
+
+// Returns the number of components in the function's range.
+int st_func_num_comp(st_func_t* func);
+
+// Evaluates the function at the given point, placing the result in result.
+void st_func_eval(st_func_t* func, point_t* x, double t, double* result);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
