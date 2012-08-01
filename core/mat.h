6b75d4a86838a902ca343e4340580d25ad1475fb
Jeffrey Johnson <jjphatt@gmail.com>
2012-07-31 22:57:17 -0700

    Added a very generic/basic linear solver interface.
    Former-commit-id: f7edfbad06beed2dfbd972824fe48f93e02a327f

---
+++ core/mat.h
@@ -0,0 +1,18 @@
+#ifndef ARBI_MAT_H
+#define ARBI_MAT_H
+
+#include "core/arbi.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// mat is an opaque type for representing matrices.
+typedef struct mat_t mat_t;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
