953a8455b0f2462ed4d4ad3cfd82235820b27d4c
Jeffrey Johnson <jjphatt@gmail.com>
2012-08-19 20:25:42 -0700

    Committing some interim changes.
    Former-commit-id: fe686534b72deacecdc8b46730216dacc5fd6a29

--- geometry/voronoi.h
+++ geometry/voronoi.h
@@ -11,18 +11,16 @@ extern "C" {
 // This type represents a planar Voronoi graph.
 typedef struct planar_voronoi_t planar_voronoi_t;
 
-// This function creates a planar Voronoi graph using the Triangle library.
-// Voronoi cells at the boundary are infinite and are marked as such in 
-// the graph.
-planar_voronoi_t* planar_voronoi_from_points(point_t* points, int num_points);
+// This function creates a planar Voronoi graph given a set of Voronoi generators and an 
+// implicit function F(x,y) which is zero at the domain boundary.
+planar_voronoi_t* planar_voronoi_graph(point_t* points, int num_points, sp_func_t* F);
+
+// This function creates a 3D Voronoi tessellation by extruding a planar Voronoi graph.
+mesh_t* voronoi_extrusion(planar_voronoi_t* graph, int num_layers, double length);
 
 // This function creates a Voronoi tessellation of the given points in 
 // three-dimensional space, returning a fully-featured mesh.
-mesh_t* voronoi_tessellation(point_t* points, int num_points, bbox_t* bounding_box);
-
-// This function produces a 3D mesh by linearly extruding the given planar graph 
-// in the z direction.
-mesh_t* extrusion(planar_voronoi_t* planar_graph, int num_extruded_cells, double length);
+mesh_t* voronoi_tessellation(point_t* points, int num_points, sp_func_t* F);
 
 #ifdef __cplusplus
 }
