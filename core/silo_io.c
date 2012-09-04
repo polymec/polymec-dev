0489e37ec945f67b62b0f4bccaf3f60096b2bca7
Jeffrey Johnson <jjphatt@gmail.com>
2012-09-03 22:26:49 -0700

    Starting to play with Silo stuff again.
    Former-commit-id: b8a29227ef9c564a48dac5285748bfbbe927a605

--- core/silo_io.c
+++ core/silo_io.c
@@ -1,21 +1,132 @@
 #include <stdlib.h>
+#include <sys/stat.h>
+#include <dirent.h>
+#include "silo.h"
 #include "core/silo_io.h"
+
+#ifdef USE_MPI
+#include <mpi.h>
+#include "pmpio.h"
+#endif
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
+#ifdef USE_MPI
+
+void* pmpio_create_file(const char* filename,
+                        const char* dirname,
+                        void* userData)
+{
+  int driver = DB_HDF5;
+  DBfile* file = DBCreate(filename, 0, DB_LOCAL, 0, driver);
+  DBMkDir(file, dirname);
+  DBSetDir(file, dirname);
+  return (void*)file;
+}
+
+void* pmpio_open_file(const char* filename, 
+                      const char* dirname,
+                      PMPIO_iomode_t iomode, 
+                      void* userData)
+{
+  int driver = DB_HDF5;
+  DBfile* file;
+  if (iomode == PMPIO_WRITE)
+  { 
+    file = DBCreate(filename, 0, DB_LOCAL, 0, driver);
+    DBMkDir(file, dirname);
+    DBSetDir(file, dirname);
+  }
+  else
+  {
+    file = DBOpen(filename, driver, DB_READ);
+    DBSetDir(file, dirname);
+  }
+  return (void*)file;
+}
+
+void pmpio_close_file(void* file, void* userData)
+{
+  DBClose((DBfile*)file);
+}
+
+#endif
+
 typedef struct
 {
+  char filename[1024];
+  char dir[1024];
+#ifdef USE_MPI
+  PMPIO_baton_t* baton;
+#endif
+  DBfile* file;
 } silo_context;
 
 static int silo_open(void* context, const char* prefix, const char* directory, io_mode_t mode, MPI_Comm comm, int num_files, int mpi_tag)
 {
+  silo_context* silo = (silo_context*)context;
+
+  // Open a file in Silo/HDF5 format.
+  char filename[1024];
+#ifdef USE_MPI
+  int nproc = 1, rank = 0;
+  MPI_Comm_size(comm, &nproc);
+  MPI_Comm_rank(comm, &rank);
+  if (num_files == -1)
+    num_files = nproc;
+  ASSERT(num_files <= nproc);
+
+  // We put the entire data set into a directory named after the 
+  // prefix, and every process gets its own subdirectory therein.
+
+  // Initialize poor man's I/O and figure out group ranks.
+  PMPIO_iomode_t pmpio_mode = (mode == IO_READ) ? PMPIO_READ : PMPIO_WRITE;
+  silo->baton = PMPIO_Init(num_files, pmpio_mode, comm, mpi_tag, 
+                           &pmpio_createFile, &pmpio_openFile, 
+                           &pmpio_closeFile, 0);
+  int group_rank = PMPIO_group_rank(baton, rank);
+  int rank_in_group = PMPIO_rank_in_group(baton, rank);
+
+  // Create a subdirectory for each group.
+  char group_dirname[1024];
+  snprintf(group_dirname, 1024, "%s/%d", directory, group_rank);
+  if ((rank_in_group == 0) && (mode == IO_WRITE))
+  {
+    DIR* group_dir = opendir(group_dirname);
+    if (group_dir == 0)
+      mkdir((char*)group_dirname, S_IRWXU | S_IRWXG);
+    else
+      closedir(group_dir);
+    MPI_Barrier(comm);
+  }
+  else if (mode == IO_WRITE)
+  {
+    MPI_Barrier(comm);
+  }
+
+  // Determine a file name.
+  snprintf(silo->filename, 1024, "%s/%s.silo", group_dirname, prefix);
+  snprintf(silo->dir, 1024, "domain_%d", rank_in_group);
+#else
+
+  snprintf(silo->filename, 1024, "%s/%s.silo", directory, prefix);
+
+  int driver = DB_HDF5;
+  silo->file = DBCreate(silo->filename, 0, DB_LOCAL, 0, driver);
+  DBSetDir(silo->file, "/");
+#endif
+
   return 0;
 }
 
 static int silo_close(void* context)
 {
+  //silo->file = (DBfile*)PMPIO_WaitForBaton(silo->baton, silo->filename, silo->dir);
+  //PMPIO_HandOffBaton(silo->baton, (void*)silo->file); // FIXME
+  //PMPIO_Finish(silo->baton);
+  //DBClose(file);
   return 0;
 }
 
