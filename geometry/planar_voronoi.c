59b422f82e125e6fabdf736857df8f7f84eef55b
Jeffrey Johnson <jjphatt@gmail.com>
2012-09-03 20:04:16 -0700

    Lots of work on Voronoi tessellation.
    Former-commit-id: ddc258d10f4e98ad6d4ba600fa82d6471c358a9d

--- geometry/planar_voronoi.c
+++ geometry/planar_voronoi.c
@@ -1,6 +1,9 @@
 #include <stdlib.h>
+#include <string.h>
 #include "geometry/voronoi.h"
 #include "core/mesh.h"
+#include "core/slist.h"
+#include "core/avl_tree.h"
 
 #define MIN(a, b) ((a > b) ? b : a)
 #define MAX(a, b) ((a > b) ? a : b)
@@ -29,58 +32,22 @@ extern "C" {
 #define incircle triangle_incircle
 #include "triangle.h"
 
-// This represents an edge in a planar Voronoi graph.
-typedef struct planar_voronoi_cell_t planar_voronoi_cell_t;
-typedef struct 
+// This AVL node visitor appends the tree's data to a tag.
+static void append_to_tag(avl_node_t* node, void* p)
 {
-  node_t* node1;
-  node_t* node2;
-  planar_voronoi_cell_t* cell1;
-  planar_voronoi_cell_t* cell2;
-} planar_voronoi_edge_t;
-
-struct planar_voronoi_cell_t
-{
-  int num_edges;
-  planar_voronoi_edge_t** edges;
-};
-
-struct planar_voronoi_t
-{
-  int num_cells;
-  planar_voronoi_cell_t* cells;
-  int num_edges;
-  planar_voronoi_edge_t* edges;
-  int num_nodes;
-  node_t* nodes;
-};
-
-static planar_voronoi_t* planar_voronoi_new(int num_cells, int num_edges, int num_nodes)
-{
-  planar_voronoi_t* v = malloc(sizeof(planar_voronoi_t));
-  v->num_cells = num_cells;
-  v->cells = malloc(num_cells*sizeof(planar_voronoi_cell_t));
-  v->num_edges = num_edges;
-  v->edges = malloc(num_edges*sizeof(planar_voronoi_edge_t));
-  v->num_nodes = num_nodes;
-  v->nodes = malloc(num_nodes*sizeof(node_t));
-  return v;
+  int* tag_p = (int*)p;
+  *tag_p = (int)node->attribute;
+  ++tag_p;
 }
 
-static void planar_voronoi_free(planar_voronoi_t* v)
+mesh_t* voronoi_plane(point_t* points, int num_points, 
+                      point_t* ghost_points, int num_ghost_points)
 {
-  free(v->cells);
-  free(v->edges);
-  free(v->nodes);
-  free(v);
-}
-
-planar_voronoi_t* planar_voronoi_graph(point_t* points, int num_points, sp_func_t* F)
-{
+  ASSERT(num_planes >= 1);
+  ASSERT(z2 > z1);
   ASSERT(points != NULL);
   ASSERT(num_points >= 2);
-  ASSERT(F != NULL);
-  ASSERT(!sp_func_is_homogeneous(F));
+  ASSERT(num_ghost_points >= 0);
 
   // Now we use Triangle to obtain a Voronoi graph.
   struct triangulateio in, delaunay, voro;
@@ -132,108 +99,269 @@ planar_voronoi_t* planar_voronoi_graph(point_t* points, int num_points, sp_func_
   // -Q : Quiet (shaddap!), no output on the terminal except errors.
   // -z : Indices are all numbered from zero.
   // -e : Generates edges and places them in out.edgelist.
-  // -D : Forces a true Delauney triangulation, which makes the Voronoi graph valid.
+  // -D : Forces a true delaunay triangulation, which makes the Voronoi graph valid.
   // -v : Generates the Voronoi graph, storing it in voro.
   triangulate((char*)"QzeDv", &in, &delaunay, &voro);
 
   // Make sure we got something.
   if (delaunay.numberoftriangles == 0)
-    arbi_error("planar_voronoi_new: Delauney triangulation produced 0 triangles!");
+    arbi_error("planar_voronoi_stack: delaunay triangulation produced 0 triangles!");
   if (delaunay.numberofpoints != num_points)
   {
     char err[1024];
-    snprintf(err, 1024, "planar_voronoi_new: Delauney triangulation produced %d triangles\n(%d generating points given)", 
+    snprintf(err, 1024, "planar_voronoi_stack: delaunay triangulation produced %d triangles\n(%d generating points given)", 
              delaunay.numberofpoints, num_points);
     arbi_error(err);
   }
   if (voro.numberofpoints <= 0)
-    arbi_error("planar_voronoi_new: Error occurred generating Voronoi graph.");
+    arbi_error("planar_voronoi_stack: Error occurred generating Voronoi graph.");
 
-  // Now assemble the planar Voronoi graph.
+  // Now assemble the mesh.
   int num_cells = num_points;
-  int num_edges = delaunay.numberofedges;
-  int num_nodes = voro.numberofpoints;
+  int num_ghost_cells = num_ghost_points;
+  int num_nodes = 2*voro.numberofpoints;
+  int horiz_edges = 2*delaunay.numberofedges;
+  int vert_edges = voro.numberofpoints;
+  int num_edges = horiz_edges + vert_edges;
+  int horiz_faces = delaunay.numberofedges;
+  int vert_faces = 2*num_points;
+  int num_faces = horiz_faces + vert_faces;
+  mesh_t* mesh = mesh_new(num_cells, num_ghost_cells,
+                          num_faces, num_edges, num_nodes);
 
-  // Count up the semi-infinite edge rays, add corresponding boundary nodes,
-  // and count the number of boundary edges we need.
-  int num_bnodes = 0, num_bedges = 0;
-  int cell_bedge[num_cells];
-  memset(cell_needs_edge, -1, num_cells*sizeof(int));
-  for (int e = 0; e < num_edges; ++e)
+  // Node coordinates.
+  for (int n = 0; n < num_nodes/2; ++n)
   {
-    if (voro.edgelist[2*e+1] == -1)
+    mesh->nodes[2*n].x = voro.pointlist[2*n+0];
+    mesh->nodes[2*n].y = voro.pointlist[2*n+1];
+    mesh->nodes[2*n].z = -0.5;
+    mesh->nodes[2*n+1].x = voro.pointlist[2*n+0];
+    mesh->nodes[2*n+1].y = voro.pointlist[2*n+1];
+    mesh->nodes[2*n+1].z = 0.5;
+  }
+
+  // Edge <-> node connectivity.
+
+  // Horizontal edges: 
+  // mesh->edges[2*i]   <-- ith bottom edge
+  // mesh->edges[2*i+1] <-- ith top edge
+  // NOTE: We keep track of "outer" edges and tag them accordingly.
+  avl_tree_t* outer_edges = int_avl_tree_new();
+  int num_outer_edges = 0;
+  for (int i = 0; i < delaunay.numberofedges; ++i)
+  {
+    mesh->edges[2*i].node1 = &mesh->nodes[2*voro.edgelist[2*i]];
+    mesh->edges[2*i+1].node1 = &mesh->nodes[2*voro.edgelist[2*i]+1];
+    int n2 = voro.edgelist[2*i+1]; // -1 if ghost
+    if (n2 == -1)
     {
-      ++num_bnodes;
-      int cell1 = delaunay.edgelist[2*e],
-          cell2 = delaunay.edgelist[2*e+1];
-      if (!cell_needs_edge[cell1] && !cell_needs_edge[cell2])
+      int e1 = 2*i, e2 = 2*i+1;
+      avl_tree_insert(outer_edges, (void*)e1);
+      avl_tree_insert(outer_edges, (void*)e2);
+      ++num_outer_edges;
+      mesh->edges[2*i].node2 = NULL;
+      mesh->edges[2*i+1].node2 = NULL;
+    }
+    else
+    {
+      mesh->edges[2*i].node2 = &mesh->nodes[2*n2];
+      mesh->edges[2*i+1].node2 = &mesh->nodes[2*n2+1];
+    }
+  }
+  // Vertical edges.
+  // mesh->edges[horiz_edges+2*i] <-- ith vertical edge
+  for (int i = 0; i < num_nodes; ++i)
+  {
+    mesh->edges[horiz_edges+2*i].node1 = &mesh->nodes[2*i];
+    mesh->edges[horiz_edges+2*i].node2 = &mesh->nodes[2*i+1];
+  }
+  // Tag the outer edges as such.
+  if (num_outer_edges > 0)
+  {
+    int* outer_edge_tag = mesh_create_tag(mesh->edge_tags, "outer_edges", num_outer_edges);
+    avl_node_t* root = avl_tree_root(outer_edges);
+    int* tag_p = outer_edge_tag;
+    avl_node_visit(root, &append_to_tag, (void*)tag_p);
+
+    // Stick the outward rays in a "rays" property on this tag.
+    double* rays = malloc(3*num_outer_edges*sizeof(double));
+    mesh_tag_set_property(mesh->edge_tags, "outer_edges", "rays", rays, &free);
+    for (int i = 0; i < num_outer_edges; ++i)
+    {
+      int j = outer_edge_tag[i];
+      ASSERT(voro.edgelist[2*i+1] == -1);
+      rays[3*i+0] = voro.normlist[2*j];
+      rays[3*i+1] = voro.normlist[2*j+1];
+      rays[3*i+2] = 0.0;
+    }
+  }
+
+  // Face <-> edge/cell connectivity.
+
+  // Horizontal faces:
+  // mesh->faces[i] <-- ith horizontal face
+  avl_tree_t* outer_cells = int_avl_tree_new();
+  int num_outer_cells = 0;
+  for (int i = 0; i < delaunay.numberofedges; ++i)
+  {
+    // Hook up this face to its cells.
+    int cell1 = delaunay.edgelist[2*i];
+    int cell2 = delaunay.edgelist[2*i+1];
+    mesh->faces[i].cell1 = &mesh->cells[cell1];
+    mesh->cells[cell1].num_faces++;
+    mesh->faces[i].cell2 = &mesh->cells[cell2];
+    mesh->cells[cell2].num_faces++;
+
+    // Horizontal faces are quadrilaterals.
+    mesh->faces[i].num_edges = 4;
+    mesh->faces[i].edges = malloc(4*sizeof(edge_t*));
+    int e1 = 2*i, e2 = 2*i+1; // Horizontal edges
+    int e3 = horiz_edges + voro.edgelist[2*i], // Vertical edges
+        e4 = horiz_edges + voro.edgelist[2*i+1];
+    mesh->faces[i].edges[0] = &mesh->edges[e1];
+    mesh->faces[i].edges[1] = &mesh->edges[e2];
+    mesh->faces[i].edges[2] = &mesh->edges[e3];
+    mesh->faces[i].edges[3] = &mesh->edges[e4];
+
+    // If one of these edges is an outer edge, then both cells that share 
+    // the face are outer cells.
+    if ((avl_tree_find(outer_edges, (void*)e1) != NULL) || 
+        (avl_tree_find(outer_edges, (void*)e2) != NULL) || 
+        (avl_tree_find(outer_edges, (void*)e3) != NULL) || 
+        (avl_tree_find(outer_edges, (void*)e4) != NULL))
+    {
+      if (avl_tree_find(outer_cells, (void*)cell1) == NULL)
       {
-        cell_bedge[cell1] = cell_bedge[cell2] = num_bedges;
-        ++num_bedges;
+        avl_tree_insert(outer_cells, (void*)cell1);
+        ++num_outer_cells;
+      }
+      if (avl_tree_find(outer_cells, (void*)cell2) == NULL)
+      {
+        avl_tree_insert(outer_cells, (void*)cell2);
+        ++num_outer_cells;
       }
     }
   }
 
-  // Construct the graph.
-  int num_ghost_cells = num_bedges;
-  planar_voronoi_t* v = planar_voronoi_new(num_cells + num_bedges, 
-                                           num_edges + num_bedges, 
-                                           num_nodes + num_bnodes);
-  for (int n = 0; n < num_nodes; ++n)
+  // Hook up the horizontal faces to cells, leaving room for top and 
+  // bottom faces.
+  for (int i = 0; i < mesh->num_cells; ++i)
   {
-    v->nodes[n].x = voro.pointlist[2*n+0];
-    v->nodes[n].y = voro.pointlist[2*n+1];
-    v->nodes[n].z = 0.0;
+    mesh->cells[i].num_faces += 2;
+    mesh->cells[i].faces = malloc(mesh->cells[i].num_faces*sizeof(face_t*));
   }
-  for (int n = num_nodes; n < num_nodes + num_bnodes; ++n)
+  int cell_next_face[mesh->num_cells];
+  memset(cell_next_face, 0, sizeof(int)*mesh->num_cells);
+  for (int i = 0; i < delaunay.numberofedges; ++i)
   {
-    // Trace back from the given node to the boundary along the
-    // gradient of the implicit function F.
-    // FIXME
-    v->nodes[n].x = 0.0, v->nodes[n].y = 0.0;
+    int cell1 = delaunay.edgelist[2*i];
+    int cell2 = delaunay.edgelist[2*i+1];
+    mesh->cells[cell1].faces[cell_next_face[cell1]++] = &mesh->faces[i];
+    mesh->cells[cell2].faces[cell_next_face[cell2]++] = &mesh->faces[i];
   }
 
-  int bnode = 0;
-  for (int e = 0; e < num_edges; ++e)
+  // Vertical faces (arbitrary polygons).
+  // mesh->faces[horiz_faces+2*i]   <-- bottom face for ith cell
+  // mesh->faces[horiz_faces+2*i+1] <-- top face for ith cell
+  int vface_num_edges[2*delaunay.numberofedges],
+      vface_next_edge[2*delaunay.numberofedges];
+  memset(vface_num_edges, 0, sizeof(int)*delaunay.numberofedges);
+  memset(vface_next_edge, 0, sizeof(int)*delaunay.numberofedges);
+  for (int i = 0; i < delaunay.numberofedges; ++i)
   {
-    // Hook up the nodes.
-    v->edges[e].node1 = &(v->nodes[voro.edgelist[2*e]]);
-    int node2 = voro.edgelist[2*e+1];
-    if (node2 == -1) // Semi-infinite ray!
+    vface_num_edges[2*voro.edgelist[2*i]]++;
+    vface_num_edges[2*voro.edgelist[2*i]+1]++;
+    vface_num_edges[2*voro.edgelist[2*i+1]]++;
+    vface_num_edges[2*voro.edgelist[2*i+1]+1]++;
+  }
+  // Hook up the top and bottom faces to cells.
+  for (int i = 0; i < mesh->num_cells; ++i)
+  {
+    int bottom_face = horiz_faces + 2*i;
+    int top_face = horiz_faces + 2*i + 1;
+    mesh->cells[i].faces[cell_next_face[i]++] = &mesh->faces[bottom_face];
+    mesh->cells[i].faces[cell_next_face[i]++] = &mesh->faces[top_face];
+    mesh->faces[bottom_face].cell1 = &mesh->cells[i];
+    mesh->faces[bottom_face].cell2 = NULL;
+    mesh->faces[top_face].cell1 = &mesh->cells[i];
+    mesh->faces[top_face].cell2 = NULL;
+  }
+  for (int i = 0; i < delaunay.numberofedges; ++i)
+  {
+    int edge1 = voro.edgelist[2*i], edge2 = voro.edgelist[2*i+1];
+
+    // Bottom face - edge1
+    if (mesh->faces[horiz_faces+2*edge1].edges == NULL)
+      mesh->faces[horiz_faces+2*edge1].edges = malloc(vface_num_edges[edge1]*sizeof(edge_t*));
+    mesh->faces[horiz_faces+2*edge1].edges[vface_next_edge[2*edge1]++] = &mesh->edges[2*edge1];
+
+    // Top face - edge1
+    if (mesh->faces[horiz_faces+2*edge1+1].edges == NULL)
+      mesh->faces[horiz_faces+2*edge1+1].edges = malloc(vface_num_edges[edge1]*sizeof(edge_t*));
+    mesh->faces[horiz_faces+2*edge1+1].edges[vface_next_edge[2*edge1+1]++] = &mesh->edges[2*edge1+1];
+
+    // Bottom face - edge2
+    if (mesh->faces[horiz_faces+2*edge2].edges == NULL)
+      mesh->faces[horiz_faces+2*edge2].edges = malloc(vface_num_edges[edge2]*sizeof(edge_t*));
+    mesh->faces[horiz_faces+2*edge2].edges[vface_next_edge[2*edge1]++] = &mesh->edges[2*edge1];
+
+    // Top face - edge2
+    if (mesh->faces[horiz_faces+2*edge2+1].edges == NULL)
+      mesh->faces[horiz_faces+2*edge2+1].edges = malloc(vface_num_edges[edge2]*sizeof(edge_t*));
+    mesh->faces[horiz_faces+2*edge2+1].edges[vface_next_edge[2*edge1+1]++] = &mesh->edges[2*edge1+1];
+  }
+
+  // Tag the outer cells as such.
+  ASSERT(num_outer_cells > 0);
+  int* outer_cell_tag = mesh_create_tag(mesh->cell_tags, "outer_cells", num_outer_cells);
+  avl_node_t* root = avl_tree_root(outer_cells);
+  int* tag_p = outer_cell_tag;
+  avl_node_visit(root, &append_to_tag, (void*)tag_p);
+
+  // Finally, we create properties on the outer_edges and outer_cells tags 
+  // that associate one with the other.
+  slist_t* outer_cell_edges = slist_new(NULL);
+  for (int i = 0; i < num_outer_cells; ++i)
+  {
+    int num_edges = 0;
+    slist_node_t* pos = slist_back(outer_cell_edges);
+    for (int f = 0; f < mesh->cells[i].num_faces; ++f)
     {
-      // This cell is adjacent to the boundary.
-      v->edges[e].node2 = num_nodes + bnode;
-      v->edges[e].cell2 = NULL;
-      ++bnode;
+      for (int e = 0; e < mesh->cells[i].faces[f]->num_edges; ++e)
+      {
+        int edgeid = (int)(mesh->faces[f].edges[e] - &mesh->edges[0]);
+        if (avl_tree_find(outer_edges, (void*)edgeid) != NULL)
+        {
+          slist_append(outer_cell_edges, (void*)edgeid);
+          ++num_edges;
+        }
+      }
     }
-    else
-    {
-      v->edges[e].node2 = &(v->nodes[node2]);
-    }
-
-    // Hook up the cells to the edges and vice versa.
-    int cell1 = MIN(delaunay.edgelist[2*e], delaunay.edgelist[2*e+1]);
-    int cell2 = MAX(delaunay.edgelist[2*e], delaunay.edgelist[2*e+1]);
-    v->cells[cell1].edges[v->cells[cell1].num_edges] = &(v->edges[e]);
-    ++(v->cells[cell1].num_edges);
-    v->cells[cell2].edges[v->cells[cell2].num_edges] = &(v->edges[e]);
-    ++(v->cells[cell2].num_edges);
+    pos = pos->next;
+    slist_insert(outer_cell_edges, pos, (void*)num_edges);
   }
 
-  // Add any needed boundary edges.
-  for (int c = 0; c < num_cells; ++c)
+  // Add 'outer_edges' as a property of the outer_cells.
+  int* oce = malloc(slist_size(outer_cell_edges)*sizeof(double));
+  mesh_tag_set_property(mesh->edge_tags, "outer_cells", "outer_edges", outer_cell_edges, &free);
+  int offset = 0;
+  for (slist_node_t* n = slist_front(outer_cell_edges); n != NULL;)
   {
-    int be = cell_bedges[c];
-    if (be >= 0)
+    // Read the number of edges for the cell.
+    int num_edges = (int)n->value;
+    n = n->next;
+    oce[offset++] = num_edges;
+    for (int e = 0; e < num_edges; ++e)
     {
-      v->cells[c].edges[v->cells[cell1].num_edges] = &(v->edges[be]);
-      ++(v->cells[c].num_edges);
-      v->edges[be].cell1 = &(v->cells[c]);
-      v->edges[be].cell2 = &(v->cells[be]);
+      oce[offset++] = (int)n->value;
+      n = n->next;
     }
   }
 
-  // Clean up our Triangle data structures.
+  // Clean up.
+  slist_free(outer_cell_edges);
+  avl_tree_free(outer_cells);
+  avl_tree_free(outer_edges);
   free(in.pointlist);
   free(delaunay.pointlist);
   free(delaunay.trianglelist);
@@ -242,20 +370,28 @@ planar_voronoi_t* planar_voronoi_graph(point_t* points, int num_points, sp_func_
   free(voro.edgelist);
   free(voro.normlist);
 
-  return v;
+  return mesh;
 }
 
-mesh_t* voronoi_extrusion(planar_voronoi_t* planar_graph, int num_layers, double length)
+mesh_t* voronoi_stack(mesh_t* plane, int num_planes, double z1, double z2)
 {
-  ASSERT(planar_graph != NULL);
-  ASSERT(num_extruded_cells > 0);
-  ASSERT(length > 0.0);
-  arbi_not_implemented("extrusion");
-
-  // Dispose of the planar graph.
-  planar_voronoi_free(planar_graph);
-
-  return NULL;
+  int num_points = 0; // FIXME
+  int num_ghost_points = 0;
+  int num_cells = num_planes * num_points;
+  int num_ghost_cells = num_planes * num_ghost_points;
+  int nodes_per_plane = 0;
+  int num_nodes = (num_planes + 1) * nodes_per_plane;
+  int horiz_edges_per_plane = 0;
+  int vert_edges_per_plane = 0;
+  int num_edges = num_planes * horiz_edges_per_plane + 
+                  (num_planes - 1) * vert_edges_per_plane;
+  int horiz_faces_per_plane = 0;
+  int vert_faces_per_plane = num_points;
+  int num_faces = num_planes * horiz_faces_per_plane + 
+                  (num_planes + 1) * vert_faces_per_plane;
+  mesh_t* mesh = mesh_new(num_cells, num_ghost_cells,
+                          num_faces, num_edges, num_nodes);
+  return mesh;
 }
 
 #ifdef __cplusplus
--- geometry/voronoi.h
+++ geometry/voronoi.h
@@ -9,26 +9,33 @@
 extern "C" {
 #endif
 
-// This type represents a planar Voronoi graph.
-//typedef struct planar_voronoi_t planar_voronoi_t;
-
-// This function creates a planar Voronoi graph given a set of Voronoi generators and an 
-// implicit function F(x,y) which is zero at the domain boundary.
-//planar_voronoi_t* planar_voronoi_graph(point_t* points, int num_points, sp_func_t* F);
-
-// This function creates a 3D Voronoi tessellation by extruding a planar Voronoi graph.
-//mesh_t* voronoi_extrusion(planar_voronoi_t* graph, int num_layers, double length);
-
 // This function creates a Voronoi tessellation of the given points in 
 // three-dimensional space, returning a fully-featured mesh.
+// The tessellation contains infinite cells at its boundary, and 
+// these cells are tagged as "outer_cells". The edges bounding 
+// these cells are semi-infinite, having one node each, and are 
+// tagged as "outer_edges". These tags can be used to intersect 
+// the mesh with a boundary function to form a closed domain.
 mesh_t* voronoi_tessellation(point_t* points, int num_points, 
                              point_t* ghost_points, int num_ghost_points);
+
+// This function creates a planar Voronoi tessellation, in which 
+// a single layer of cells is bounded by two planes: one above, 
+// and one below. The top plane is located at z = 0.5, and the 
+// bottom one is located at z = -0.5.
+mesh_t* voronoi_plane(point_t* points, int num_points, 
+                      point_t* ghost_points, int num_ghost_points);
 
 // This function intersects the given Voronoi tessellation (mesh) with the 
 // given signed distance function representing a domain boundary, adding the 
 // necessary faces and vertices. It is assumed that all generators fall within
 // the domain.
 void voronoi_intersect_with_boundary(mesh_t* mesh, sp_func_t* boundary);
+
+// This function takes a planar Voronoi mesh and generates a 
+// stack of these planes to form a new mesh. The bottom plane is 
+// z = z1 and the top is z = z2.
+mesh_t* voronoi_stack(mesh_t* plane, int num_planes, double z1, double z2);
 
 #ifdef __cplusplus
 }
