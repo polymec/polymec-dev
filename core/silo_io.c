1ae005a5794f78dd155b6e182b424cb1cd1ef23b
Jeffrey Johnson <jjphatt@gmail.com>
2012-09-05 23:16:01 -0700

    Simplified the I/O interface.
    Former-commit-id: 753c612c2f91b1c071866d19d3162f761320078a

--- core/silo_io.c
+++ core/silo_io.c
@@ -17,9 +17,9 @@ typedef struct
 {
 } silo_context;
 
-void* silo_create_file(const char* filename,
-                       const char* dirname,
-                       void* userData)
+static void* silo_create_file(const char* filename,
+                              const char* dirname,
+                              void* userData)
 {
   int driver = DB_HDF5;
   DBfile* file = DBCreate(filename, 0, DB_LOCAL, 0, driver);
@@ -28,10 +28,10 @@ void* silo_create_file(const char* filename,
   return (void*)file;
 }
 
-void* silo_open_file(const char* filename, 
-                     const char* dirname,
-                     io_mode_t mode, 
-                     void* userData)
+static void* silo_open_file(const char* filename, 
+                            const char* dirname,
+                            io_mode_t mode, 
+                            void* userData)
 {
   int driver = DB_HDF5;
   DBfile* file;
@@ -49,7 +49,7 @@ void* silo_open_file(const char* filename,
   return (void*)file;
 }
 
-void silo_close_file(void* context, void* file)
+static void silo_close_file(void* context, void* file)
 {
   // In this method, we do all the writing.
   silo_context* silo = (silo_context*)context;
@@ -57,44 +57,12 @@ void silo_close_file(void* context, void* file)
   DBClose(file);
 }
 
-static int silo_read_mesh(void* context, const char* dataset, mesh_t** mesh)
+static void silo_write_data(void* context, void* file, io_buffered_data_t* data)
 {
-  return 0;
 }
 
-static int silo_write_mesh(void* context, const char* dataset, mesh_t* mesh)
+static void silo_read_data(void* context, void* file, io_buffered_data_t* data)
 {
-  return 0;
-}
-
-static int silo_query_field(void* context, const char* dataset, const char* field_name, int* num_components, mesh_centering_t* centering)
-{
-  return 0;
-}
-
-static int silo_read_field(void* context, const char* dataset, const char* field_name, double* data)
-{
-  return 0;
-}
-
-static int silo_write_field(void* context, const char* dataset, const char* field_name, double* data, int num_components, mesh_centering_t centering)
-{
-  return 0;
-}
-
-static int silo_query_source_code(void* context, const char* dataset, const char* code_name, int* length)
-{
-  return 0;
-}
-
-static int silo_read_source_code(void* context, const char* dataset, const char* code_name, char* source_code)
-{
-  return 0;
-}
-
-static int silo_write_source_code(void* context, const char* dataset, const char* code_name, const char* source_code)
-{
-  return 0;
 }
 
 static void silo_dtor(void* context)
@@ -108,14 +76,8 @@ io_interface_t* silo_io_new()
   io_vtable vtable = {.create_file = &silo_create_file,
                       .open_file = &silo_open_file, 
                       .close_file = &silo_close_file,
-                      .read_mesh = &silo_read_mesh,
-                      .query_field = &silo_query_field,
-                      .read_field = &silo_read_field,
-                      .query_source_code = &silo_query_source_code,
-                      .read_source_code = &silo_read_source_code,
-                      .write_mesh = &silo_write_mesh,
-                      .write_field = &silo_write_field,
-                      .write_source_code = &silo_write_source_code,
+                      .read_data = &silo_read_data,
+                      .write_data = &silo_write_data,
                       .dtor = &silo_dtor};
   return io_interface_new(context, "SILO", vtable);
 }
