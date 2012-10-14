5cef7e2a514b0a7d35382e2a7f26c544bda7e8f2
Jeffrey Johnson <jjphatt@gmail.com>
2012-10-14 12:23:43 -0700

    Finished generic AVL tree implementation and test.
    Former-commit-id: 9913fdbd7ee4acb3c6168f921cdd4bdf924a2726

--- core/avl_tree.h
+++ core/avl_tree.h
@@ -1,21 +1,40 @@
 #ifndef ARBI_AVL_TREE_H
 #define ARBI_AVL_TREE_H
 
+#include <stdlib.h>
 #include "core/arbi.h"
 #include "core/comparators.h"
 #include "arena/proto.h"
 
-#ifdef MAX(x,y)
-#undef MAX
-#define MAX(x,y) ((x > y) ? x : y)
-#endif
+// An AVL tree is a balanced binary tree that can be used to implement other data structures.
+// One defines tree and node types using
+// DEFINE_AVL_TREE(tree_name, element, comparator, destructor)
+//
+// Interface for a type x_tree_t (with node type x_tree_node_t and datum x) defined with 
+// DEFINE_AVL_TREE(x_tree, x, x_comparator, x_destructor):
+// 
+// x_tree_t* x_tree_new() - Creates a new empty tree.
+// void x_tree_free(x_tree_t* tree) - Destroys the tree.
+// void x_tree_clear(x_tree_t* tree) - Empties the tree.
+// x_tree_node_t* x_tree_find(x_tree_t* tree, x datum) - Finds the node containing the datum.
+// void x_tree_insert(x_tree_t* tree, x datum) - Inserts a datum into the tree.
+// void x_tree_delete(x_tree_t* tree, x_tree_node_t* node) - Deletes the node from the tree.
+// void x_tree_node_visit(x_tree_node_t* node, x_tree_node_visitor visit, void*) - Visits the given node and its subtree.
+// int x_tree_size(x_tree_t* tree) - Returns the number of nodes in the tree. Computed, not stored.
 
-#define DEFINE_AVL_TREE(tree_name, tree_node_name, element, comparator, destructor) \
-typedef struct tree_node_name##_t tree_node_name##_t; \
-struct tree_node_name##_t \
+// This maximum function is used to keep AVL trees ordered.
+static int avl_tree_max(int x, int y)
+{
+  return ((x > y) ? x : y);
+}
+
+#define DEFINE_AVL_TREE(tree_name, element, comparator, destructor) \
+typedef struct tree_name##_node_t tree_name##_node_t; \
+typedef void (*tree_name##_node_visitor)(tree_name##_node_t*, void*); \
+struct tree_name##_node_t \
 { \
-  tree_node_name##_t* left; \
-  tree_node_name##_t* right; \
+  tree_name##_node_t* left; \
+  tree_name##_node_t* right; \
   int depth; \
   element attribute; \
 }; \
@@ -24,12 +43,12 @@ typedef struct tree_name##_t tree_name##_t; \
 typedef void (*tree_name##_dtor)(element); \
 struct tree_name##_t \
 { \
-  tree_node_name##_t* root; \
+  tree_name##_node_t* root; \
   ARENA* arena; \
   tree_name##_dtor dtor; \
 }; \
 \
-tree_name##_t* tree_name##_new() \
+static inline tree_name##_t* tree_name##_new() \
 { \
   tree_name##_t* tree = malloc(sizeof(tree_name##_t)); \
   tree->arena = NULL; \
@@ -38,7 +57,7 @@ tree_name##_t* tree_name##_new() \
   return tree; \
 } \
 \
-static inline void tree_name##_clear_node(tree_node_name##_t* node, tree_name##_dtor dtor) \
+static inline void tree_name##_clear_node(tree_name##_node_t* node, tree_name##_dtor dtor) \
 { \
   if (node != NULL) \
   { \
@@ -62,7 +81,7 @@ static inline void tree_name##_free(tree_name##_t* tree) \
   free(tree); \
 } \
 \
-static inline tree_node_name##_t* tree_name##_find_node(tree_node_name##_t* node, element datum) \
+static inline tree_name##_node_t* tree_name##_find_node(tree_name##_node_t* node, element datum) \
 { \
   if (node == NULL) \
     return NULL; \
@@ -75,57 +94,57 @@ static inline tree_node_name##_t* tree_name##_find_node(tree_node_name##_t* node
     return tree_name##_find_node(node->right, datum); \
 } \
 \
-static inline tree_node_name##_t* tree_name##_find(tree_name##_t* tree, element datum) \
+static inline tree_name##_node_t* tree_name##_find(tree_name##_t* tree, element datum) \
 { \
   return tree_name##_find_node(tree->root, datum); \
 } \
 \
-static inline int tree_node_name##_depth(tree_node_name##_t* node) \
+static inline int tree_name##_node_depth(tree_name##_node_t* node) \
 { \
   if (node == NULL) \
     return -1; \
   else return node->depth; \
 } \
 \
-static inline tree_node_name##_t* tree_node_name##_single_rotate_with_left(tree_node_name##_t* node) \
+static inline tree_name##_node_t* tree_name##_node_single_rotate_with_left(tree_name##_node_t* node) \
 { \
-  tree_node_name##_t* n = node->left; \
+  tree_name##_node_t* n = node->left; \
   ASSERT(n != NULL); \
   node->left = n->right; \
   n->right = node; \
-  node->depth = MAX(tree_node_name##_depth(node->left), tree_node_name##_depth(node->right)) + 1; \
-  n->depth = MAX(tree_node_name##_depth(n->left), node->depth) + 1; \
+  node->depth = avl_tree_max(tree_name##_node_depth(node->left), tree_name##_node_depth(node->right)) + 1; \
+  n->depth = avl_tree_max(tree_name##_node_depth(n->left), node->depth) + 1; \
   return n; \
 } \
 \
-static inline tree_node_name##_t* tree_node_name##_single_rotate_with_right(tree_node_name##_t* node) \
+static inline tree_name##_node_t* tree_name##_node_single_rotate_with_right(tree_name##_node_t* node) \
 { \
-  tree_node_name##_t* n = node->right; \
+  tree_name##_node_t* n = node->right; \
   ASSERT(n != NULL); \
   node->right = n->left; \
   n->left = node; \
-  node->depth = MAX(tree_node_name##_depth(node->left), tree_node_name##_depth(node->right)) + 1; \
-  n->depth = MAX(tree_node_name##_depth(n->right), node->depth) + 1; \
+  node->depth = avl_tree_max(tree_name##_node_depth(node->left), tree_name##_node_depth(node->right)) + 1; \
+  n->depth = avl_tree_max(tree_name##_node_depth(n->right), node->depth) + 1; \
   return n; \
 } \
 \
-static inline tree_node_name##_t* tree_node_name##double_rotate_with_left(tree_node_name##_t* node) \
+static inline tree_name##_node_t* tree_name##_node_double_rotate_with_left(tree_name##_node_t* node) \
 { \
-  node->left = tree_node_name##single_rotate_with_right(node->left); \
-  return tree_node_name##single_rotate_with_left(node); \
+  node->left = tree_name##_node_single_rotate_with_right(node->left); \
+  return tree_name##_node_single_rotate_with_left(node); \
 } \
 \
-static inline tree_node_name##_t* tree_node_name##double_rotate_with_right(tree_node_name##_t* node) \
+static inline tree_name##_node_t* tree_name##_node_double_rotate_with_right(tree_name##_node_t* node) \
 { \
-  node->right = tree_node_name##single_rotate_with_left(node->right); \
-  return tree_node_name##single_rotate_with_right(node); \
+  node->right = tree_name##_node_single_rotate_with_left(node->right); \
+  return tree_name##_node_single_rotate_with_right(node); \
 } \
 \
-static inline tree_node_name##_t* tree_name##_insert_node(tree_node_name##_t* node, element datum) \
+static inline tree_name##_node_t* tree_name##_insert_node(tree_name##_node_t* node, element datum) \
 { \
   if (node == NULL) \
   { \
-    node = malloc(sizeof(tree_node_name##_t)); \
+    node = malloc(sizeof(tree_name##_node_t)); \
     node->left = NULL; \
     node->right = NULL; \
     node->attribute = datum; \
@@ -138,28 +157,28 @@ static inline tree_node_name##_t* tree_name##_insert_node(tree_node_name##_t* no
     if (result < 0) \
     { \
       node->left = tree_name##_insert_node(node->left, datum); \
-      if ((tree_node_name##_depth(node->left) - tree_node_name##_depth(node->right)) == 2) \
+      if ((tree_name##_node_depth(node->left) - tree_name##_node_depth(node->right)) == 2) \
       { \
         int result2 = comparator(datum, node->left->attribute); \
         if (result2 < 0) \
-          node = tree_node_name##single_rotate_with_left(node); \
+          node = tree_name##_node_single_rotate_with_left(node); \
         else \
-          node = tree_node_name##double_rotate_with_left(node); \
+          node = tree_name##_node_double_rotate_with_left(node); \
       } \
     } \
     else if (result > 0) \
     { \
       node->right = tree_name##_insert_node(node->right, datum); \
-      if ((tree_node_name##_depth(node->right) - tree_node_name##_depth(node->left)) == 2) \
+      if ((tree_name##_node_depth(node->right) - tree_name##_node_depth(node->left)) == 2) \
       { \
         int result2 = comparator(datum, node->right->attribute); \
         if (result2 > 0) \
-          node = tree_node_name##single_rotate_with_right(node); \
+          node = tree_name##_node_single_rotate_with_right(node); \
         else \
-          node = tree_node_name##double_rotate_with_right(node); \
+          node = tree_name##_node_double_rotate_with_right(node); \
       } \
     } \
-    node->depth = MAX(tree_node_name##_depth(node->left), tree_node_name##_depth(node->right)) + 1; \
+    node->depth = avl_tree_max(tree_name##_node_depth(node->left), tree_name##_node_depth(node->right)) + 1; \
     return node; \
   } \
 } \
@@ -169,8 +188,8 @@ static inline void tree_name##_insert(tree_name##_t* tree, element datum) \
   tree->root = tree_name##_insert_node(tree->root, datum); \
 } \
 \
-static inline tree_node_name##_t* tree_name##_find_node_parent(tree_node_name##_t* root, \
-                                             tree_node_name##_t* node) \
+static inline tree_name##_node_t* tree_name##_find_node_parent(tree_name##_node_t* root, \
+                                             tree_name##_node_t* node) \
 { \
   ASSERT(node != NULL); \
   int result = comparator(node->attribute, root->attribute); \
@@ -190,14 +209,14 @@ static inline tree_node_name##_t* tree_name##_find_node_parent(tree_node_name##_
     if (root->right == node) \
       return root; \
     else \
-      return tree_name##_find_node(root->right, node); \
+      return tree_name##_find_node_parent(root->right, node); \
   } \
 } \
 \
-static inline void tree_name##_delete(tree_name##_t* tree, tree_node_name##_t* node) \
+static inline void tree_name##_delete(tree_name##_t* tree, tree_name##_node_t* node) \
 { \
   ASSERT(node != NULL); \
-  tree_node_name##_t* parent = tree_name##_find_node_parent(tree->root, node); \
+  tree_name##_node_t* parent = tree_name##_find_node_parent(tree->root, node); \
   if ((node->left == NULL) || (node->right == NULL)) \
   { \
     if ((node->left == NULL) && (node->right == NULL)) \
@@ -212,7 +231,7 @@ static inline void tree_name##_delete(tree_name##_t* tree, tree_node_name##_t* n
     } \
     else \
     { \
-      tree_node_name##_t* child = (node->left != NULL) ? node->left : node->right; \
+      tree_name##_node_t* child = (node->left != NULL) ? node->left : node->right; \
       if (parent != NULL) \
       { \
         if (parent->left == node) \
@@ -224,10 +243,10 @@ static inline void tree_name##_delete(tree_name##_t* tree, tree_node_name##_t* n
   } \
   else \
   { \
-    tree_node_name##_t* pred = node->left; \
-    tree_node_name##_t* pred_parent = node; \
-    tree_node_name##_t* succ = node->right; \
-    tree_node_name##_t* succ_parent = node; \
+    tree_name##_node_t* pred = node->left; \
+    tree_name##_node_t* pred_parent = node; \
+    tree_name##_node_t* succ = node->right; \
+    tree_name##_node_t* succ_parent = node; \
     while (pred->right != NULL) \
     { \
       pred_parent = pred; \
@@ -238,8 +257,8 @@ static inline void tree_name##_delete(tree_name##_t* tree, tree_node_name##_t* n
       succ_parent = succ; \
       succ = succ->left; \
     } \
-    tree_node_name##_t* replacement; \
-    tree_node_name##_t* replacement_parent; \
+    tree_name##_node_t* replacement; \
+    tree_name##_node_t* replacement_parent; \
     int imbalance = succ->depth - pred->depth; \
     if (imbalance < 0) \
     { \
@@ -257,30 +276,39 @@ static inline void tree_name##_delete(tree_name##_t* tree, tree_node_name##_t* n
     } \
     parent->left = replacement; \
   } \
-  if (dtor != NULL) \
-    dtor(node->attribute); \
+  if (tree->dtor != NULL) \
+    tree->dtor(node->attribute); \
   free(node); \
 } \
 \
-static inline void tree_node_name##_visit(tree_node_name##_t* node, tree_node_name##_visitor visit, element arg) \
+static inline void tree_name##_node_visit(tree_name##_node_t* node, tree_name##_node_visitor visit, void* arg) \
 { \
   if (node == NULL) return; \
   if (node->left != NULL) \
-    tree_node_name##_visit(node->left, visit, arg); \
+    tree_name##_node_visit(node->left, visit, arg); \
   visit(node, arg); \
   if (node->right != NULL) \
-    tree_node_name##_visit(node->right, visit, arg); \
+    tree_name##_node_visit(node->right, visit, arg); \
 } \
 \
-tree_node_name##_t* tree_name##_root(tree_name##_t* tree) \
+static inline void tree_name##_count_node(tree_name##_node_t* node, void* arg) \
 { \
-  return tree->root; \
+  int* count = (int*)arg; \
+  (*count)++; \
 } \
+\
+static inline int tree_name##_size(tree_name##_t* tree) \
+{ \
+  int count = 0; \
+  tree_name##_node_visit(tree->root, &tree_name##_count_node, (void*)&count); \
+  return count; \
+} \
+\
 
 
 // Define some avl_trees.
-DEFINE_AVL_TREE(int_tree, int_tree_node, int, int_cmp, NULL)
-DEFINE_AVL_TREE(double_tree, double_tree_node, double, double_cmp, NULL)
+DEFINE_AVL_TREE(int_tree, int, int_cmp, NULL)
+DEFINE_AVL_TREE(double_tree, double, double_cmp, NULL)
 
 
 // Old stuff.
---
+++ core/tests/test_avl_tree.c
@@ -0,0 +1,52 @@
+#include <stdarg.h>
+#include <stddef.h>
+#include <setjmp.h>
+#include <string.h>
+#include "cmockery.h"
+#include "core/avl_tree.h"
+
+int int_values[] = {0, 1, 2, 3, 4, 5};
+double double_values[] = {0., 1., 2., 3., 4., 5.};
+
+#define DEFINE_AVL_TREE_TEST(tree_name, element) \
+void test_##tree_name##_ctor(void** state) \
+{ \
+  tree_name##_t* t = tree_name##_new(); \
+  assert_int_equal(0, tree_name##_size(t)); \
+  assert_true(t->root == NULL); \
+  assert_true(t->arena == NULL); \
+  tree_name##_free(t); \
+} \
+\
+void test_##tree_name##_insert(void** state) \
+{ \
+  tree_name##_t* t = tree_name##_new(); \
+  for (int i = 0; i < 5; ++i) \
+    tree_name##_insert(t, element##_values[i]); \
+  assert_int_equal(5, tree_name##_size(t)); \
+  assert_true(t->root != NULL); \
+  tree_name##_clear(t); \
+  assert_int_equal(0, tree_name##_size(t)); \
+  assert_true(t->root == NULL); \
+  tree_name##_free(t); \
+} \
+\
+void test_##element##_tree(void** state) \
+{ \
+  test_##tree_name##_ctor(state); \
+  test_##tree_name##_insert(state); \
+}
+
+DEFINE_AVL_TREE_TEST(int_tree, int)
+DEFINE_AVL_TREE_TEST(double_tree, double)
+
+int main(int argc, char* argv[]) 
+{
+  arbi_init(argc, argv);
+  const UnitTest tests[] = 
+  {
+    unit_test(test_int_tree),
+    unit_test(test_double_tree)
+  };
+  return run_tests(tests);
+}
