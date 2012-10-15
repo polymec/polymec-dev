759742898164ad94a9bdd2d59440ad1aa94dec32
Jeffrey Johnson <jjphatt@gmail.com>
2012-10-14 20:05:20 -0700

    Interim commit for unordered_set. We don't strictly need this yet.
    Former-commit-id: bf25d9dac3b68cbdf5f94c3021b6a6400f2a1dc4

---
+++ core/hash_functions.h
@@ -0,0 +1,36 @@
+#ifndef ARBI_HASH_FUNCTIONS_H
+#define ARBI_HASH_FUNCTIONS_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// A solid string hash function by Dan Bernstein.
+static inline int djb2_hash(char* str)
+{
+  int hash = 5381;
+  int c;
+
+  while (c = *str++)
+    hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+
+  return hash;
+}
+
+// A slightly improved version of djb2 using xor.
+static inline int djb2_xor_hash(char* str)
+{
+  int hash = 5381;
+  int c;
+
+  while (c = *str++)
+    hash = ((hash << 5) + hash) ^ c;
+
+  return hash;
+}
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
