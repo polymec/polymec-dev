ce8aa102ff60588015d703abd76a0feaa7f26879
Jeffrey Johnson <jjphatt@gmail.com>
2012-10-20 15:28:22 -0700

    Unordered set is done.
    Former-commit-id: 97043fe12a5e914f2b9c4e94976e4eb8f6387add

---
+++ core/tests/test_unordered_set.c
@@ -0,0 +1,50 @@
+#include <stdarg.h>
+#include <stddef.h>
+#include <setjmp.h>
+#include <string.h>
+#include "cmockery.h"
+#include "core/unordered_set.h"
+
+int int_values[] = {0, 1, 2, 3, 4, 5};
+double double_values[] = {0., 1., 2., 3., 4., 5.};
+
+#define DEFINE_UNORDERED_SET_TEST(set_name, element) \
+void test_##set_name##_ctor(void** state) \
+{ \
+  set_name##_t* s = set_name##_new(); \
+  assert_int_equal(0, s->size); \
+  set_name##_free(s); \
+} \
+\
+void test_##set_name##_insert(void** state) \
+{ \
+  set_name##_t* s = set_name##_new(); \
+  for (int i = 0; i < 5; ++i) \
+    set_name##_insert(s, element##_values[i]); \
+  assert_int_equal(5, s->size); \
+  for (int i = 0; i < 5; ++i) \
+  { \
+    assert_true(set_name##_contains(s, element##_values[i])); \
+  } \
+  set_name##_clear(s); \
+  assert_int_equal(0, s->size); \
+  set_name##_free(s); \
+} \
+\
+void test_##element##_unordered_set(void** state) \
+{ \
+  test_##set_name##_ctor(state); \
+  test_##set_name##_insert(state); \
+}
+
+DEFINE_UNORDERED_SET_TEST(int_unordered_set, int)
+
+int main(int argc, char* argv[]) 
+{
+  arbi_init(argc, argv);
+  const UnitTest tests[] = 
+  {
+    unit_test(test_int_unordered_set)
+  };
+  return run_tests(tests);
+}
