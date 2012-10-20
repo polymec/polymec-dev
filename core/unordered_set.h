7cb674ed7124839b3579c23c58a524899cb2fda9
Jeffrey Johnson <jjphatt@gmail.com>
2012-10-20 12:34:52 -0700

    A real hash map and implementation for unordered set.
    Former-commit-id: 52ef0c988a4b8a45d48b1ca8ff26f971430d83e8

--- core/unordered_set.h
+++ core/unordered_set.h
@@ -1,107 +1,70 @@
 #ifndef ARBI_UNORDERED_SET_H
 #define ARBI_UNORDERED_SET_H
 
+#include "core/hash_map.h"
+
 // An unordered set is a container that stores unique values using a hash table.
-// One defines an unordered set and node types using
-// DEFINE_UNORDERED_SET(set_name, element, hash_function, destructor)
+// One defines an unordered set using
+// DEFINE_UNORDERED_SET(set_name, element, hash_func, equals_func)
 //
 // Interface for a type x_set_t (with node type x_set_node_t 
 // and datum x) defined with 
-// DEFINE_UNORDERED_SET(x_set, x, x_comparator, x_destructor):
+// DEFINE_UNORDERED_SET(x_set, x, x_hash, x_equals):
 // 
 // x_set_t* x_set_new() - Creates a new empty ordered set.
 // void x_set_free(x_set_t* set) - Destroys the set.
 // void x_set_clear(x_set_t* set) - Empties the set.
-// x_set_node_t* x_set_find(x_set_t* set, x datum) - Finds the node containing the datum.
 // bool x_set_contains(x_set_t* set, x datum) - Returns true if the set contains the datum, false otherwise.
 // void x_set_insert(x_set_t* set, x datum) - Inserts a datum into the set.
 // void x_set_delete(x_set_t* set, x datum) - Deletes the datum from the set.
 // void x_set_foreach(x_set_t* node, x_set_visitor visit, void*) - Executes visit on each set element.
 
-#define DEFINE_UNORDERED_SET(set_name, element, hash_function, destructor) \
-typedef int (*set_name##_hash_function)(element); \
-typedef struct set_name##_t set_name##_t; \
-typedef void (*set_name##_destructor)(element); \
+#define DEFINE_UNORDERED_SET(set_name, element, hash_func, equals_func) \
+DEFINE_HASH_MAP(set_name##_hash_map, element, bool, hash_func, equals_func) \
+typedef element set_name##_element_t; \
+typedef int (*set_name##_hash_func)(element); \
 typedef struct \
 { \
-  int hash; \
-  element value; \
-} set_name##_entry_t; \
-struct set_name##_t \
-{ \
-  set_name##_entry_t* entries; \
-  set_name##_hash_function hash; \
-  int size; \
-}; \
+  set_name##_hash_map map; \
+} set_name##_t; \
 \
 static inline set_name##_t* set_name##_new() \
 { \
   set_name##_t* set = malloc(sizeof(set_name##_t)); \
-  set->entries = NULL; \
-  set->hash = hash_function; \
-  set->size = 0; \
+  set->map = set_name##_hash_map_new(); \
   return set; \
 } \
 \
 static inline void set_name##_clear(set_name##_t* set) \
 { \
-  if (set->entries != NULL) \
-  { \
-    free(set->entries); \
-    set->entries = NULL; \
-  } \
-  set->size = 0; \
+  set_name##_hash_map_clear(set->map); \
 } \
 \
 static inline void set_name##_free(set_name##_t* set) \
 { \
-  set_name##_clear(set); \
+  set_name##_hash_map_free(set->map); \
   free(set); \
-} \
-\
-static inline set_name##_entry_t* set_name##_find(set_name##_t* set, set_name##_element_t datum) \
-{ \
-  int key_hash = set->hash(datum); \
-  for (int i = 0; i < set->size; ++i) \
-  { \
-  } \
-  return tree_name##_find(set->tree, datum); \
 } \
 \
 static inline bool set_name##_contains(set_name##_t* set, set_name##_element_t datum) \
 { \
-  return (set_name##_find(set, datum) != NULL); \
+  return set_name##_hash_map_contains(set, datum); \
 } \
 \
 static inline void set_name##_insert(set_name##_t* set, set_name##_element_t datum) \
 { \
-  tree_name##_insert(set->tree, datum); \
-  set->size = tree_name##_size(set->tree); \
+  set_name##_hash_map_set(set->map, datum, true); \
 } \
 \
 static inline void set_name##_delete(set_name##_t* set, set_name##_element_t datum) \
 { \
-  set_name##_node_t* node = set_name##_find(set, datum); \
-  if (node != NULL) \
-  { \
-    tree_name##_delete(set->tree, node); \
-    set->size = tree_name##_size(set->tree); \
-  } \
+  set_name##_hash_map_delete(set->map, datum); \
 } \
 \
-static inline void set_name##_foreach(set_name##_t* set, set_name##_visitor visit, void* arg) \
-{ \
-  tree_name##_node_visit(set->tree->root, visit, arg); \
-} \
-\
-
-#define DEFINE_ORDERED_SET(set_name, element, comparator, destructor) \
-DEFINE_AVL_TREE(set_name##_avl_tree, element, comparator, destructor) \
-DEFINE_ORDERED_SET_USING_TREE(set_name, set_name##_avl_tree)
 
 // Define some ordered_sets.
-DEFINE_ORDERED_SET_USING_AVL_TREE(int_ordered_set, int_avl_tree)
-DEFINE_ORDERED_SET_USING_AVL_TREE(double_ordered_set, double_avl_tree)
-DEFINE_ORDERED_SET_USING_AVL_TREE(string_ordered_set, string_avl_tree)
+DEFINE_UNORDERED_SET(int_unordered_set, int, int_hash, int_equals)
+DEFINE_UNORDERED_SET(double_unordered_set, double, double_hash, double_equals)
+DEFINE_UNORDERED_SET(string_unordered_set, char*, string_hash, double_hash)
 
 #endif
