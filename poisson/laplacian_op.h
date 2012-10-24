cdbcad1d9f26207c8da4e474628ccd544284e3bb
Jeffrey Johnson <jjphatt@gmail.com>
2012-10-23 21:45:36 -0700

    Implemented a simple laplacian operator.
    Former-commit-id: 1b9e94b29722d092a5494d00e735ccdae799550a

---
+++ poisson/laplacian_op.h
@@ -0,0 +1,17 @@
+#ifndef ARBI_LAPLACIAN_OP_H
+#define ARBI_LAPLACIAN_OP_H
+
+#include "core/lin_op.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// Returns a 2nd-order, finite-volume, cell-centered Laplacian operator.
+lin_op_t* laplacian_op_new(mesh_t* mesh);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
