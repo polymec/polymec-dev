e86dd5c41567981529f4cad812b01ca866638988
Jeffrey Johnson <jjphatt@gmail.com>
2012-11-06 12:16:44 -0800

    Added least squares fit shape functions.
    Former-commit-id: f3c2d30631057bfd2e233841b63dc15aff5571c0

--- core/least_squares.h
+++ core/least_squares.h
@@ -33,22 +33,22 @@ void multi_index_reset(multi_index_t* m);
 // a value given a Euclidean distance and a spacing parameter.
 typedef double (*ls_weighting_func_t)(double);
 
-// Returns the size of the polynomial basis of order p.
-int poly_basis_size(int p);
+// Returns the size of the least-squares polynomial basis of order p.
+int poly_ls_basis_size(int p);
 
 // Allocates sufficient storage for a moment matrix (or its transpose) for 
 // a least squares fit of polynomial order p. This storage must be freed with 
 // a call to free(). 
 // NOTE: This function does not compute the coefficients of the moment matrix. 
-double* allocate_poly_moment_matrix(int p);
+double* allocate_poly_ls_moment_matrix(int p);
 
 // Allocates sufficient storage for the right-hand-side vector for a least
 // squares fit of polynomial order p. This storage must be freed with a call to free(). 
 // NOTE: This function does not compute the coefficients of the vector. 
-double* allocate_poly_rhs_vector(int p);
+double* allocate_poly_ls_rhs_vector(int p);
 
 // Computes the pth-order polynomial basis vector for the given point.
-void compute_poly_basis_vector(int p, point_t* point, double* basis);
+void compute_poly_ls_basis_vector(int p, point_t* point, double* basis);
 
 // Computes the least squares system for a pth-order polynomial fit to a set 
 // of scattered point data, centered about the point x0. This computes the 
@@ -60,6 +60,20 @@ void compute_poly_ls_system(int p, point_t* x0, point_t* points, int num_points,
 // This computes the weighted least squares system for the weighting function W.
 void compute_weighted_poly_ls_system(int p, ls_weighting_func_t W, point_t* x0, point_t* points, int num_points, 
                                      double* data, double* moment_matrix, double* rhs);
+
+// This class represents a shape function basis for a polynomial least-squares 
+// fit. A shape function maps a set of data (associated a given set of points in 
+// space) to a value interpolated at a given point.
+// Objects of this type are garbage-collected.
+typedef struct poly_ls_shape_basis_t poly_ls_shape_basis_t;
+
+// Create a new shape function for a polynomial least-squares fit of order p, 
+// with an optional weighting function.
+poly_ls_shape_basis_t* poly_ls_shape_basis_new(int p);
+
+// Compute the values of the shape function basis at the point x0, 
+// given a set of points.
+void poly_ls_shape_basis_compute(poly_ls_shape_basis_t* N, point_t* x0, point_t* points, int num_points, double* values);
 
 #ifdef __cplusplus
 }
