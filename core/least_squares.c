ccc351b94421593323da26d0111d1da5bd160d13
Jeffrey Johnson <jjphatt@gmail.com>
2012-11-08 13:53:41 -0800

    Hopefully tying off least-squares stuff.
    Former-commit-id: d8cbf9073b020532e125fdabcac20575b183ea96

--- core/least_squares.c
+++ core/least_squares.c
@@ -135,22 +135,25 @@ int poly_ls_basis_size(int p)
   return multi_index_sizes[p];
 }
 
-double* allocate_poly_ls_basis_vector(int p)
-{
-  return malloc(sizeof(double)*multi_index_sizes[p]);
-}
-
-double* allocate_poly_ls_moment_matrix(int p)
-{
-  return malloc(sizeof(double)*multi_index_sizes[p]*multi_index_sizes[p]);
-}
-
 void compute_poly_ls_basis_vector(int p, point_t* point, double* basis)
 {
   multi_index_t* m = multi_index_new(p);
   int i = 0, x, y, z;
   while (multi_index_next(m, &x, &y, &z))
     basis[i++] = pow(point->x, x)*pow(point->y, y)*pow(point->z, z);
+  m = NULL;
+}
+
+void compute_poly_ls_basis_gradient(int p, point_t* point, vector_t* gradients)
+{
+  multi_index_t* m = multi_index_new(p);
+  int i = 0, x, y, z;
+  while (multi_index_next(m, &x, &y, &z))
+  {
+    gradients[i++].x = (x == 0) ? 0.0 : x*pow(point->x, x-1)*pow(point->y, y)*pow(point->z, z);
+    gradients[i++].y = (y == 0) ? 0.0 : pow(point->x, x)*y*pow(point->y, y-1)*pow(point->z, z);
+    gradients[i++].z = (z == 0) ? 0.0 : pow(point->x, x)*pow(point->y, y)*z*pow(point->z, z-1);
+  }
   m = NULL;
 }
 
@@ -220,7 +223,9 @@ void compute_weighted_poly_ls_system(int p, ls_weighting_func_t W, point_t* x0, 
       compute_poly_ls_basis_vector(p, &points[n], basis);
       d = points[n].x*points[n].x + points[n].y*points[n].y + points[n].z*points[n].z;
     }
-    double Wd = W(d);
+    double Wd;
+    vector_t gradWd;
+    W(d, &Wd, &gradWd);
     for (int i = 0; i < size; ++i)
     {
       for (int j = 0; j < size; ++j)
@@ -231,14 +236,24 @@ void compute_weighted_poly_ls_system(int p, ls_weighting_func_t W, point_t* x0, 
 }
 
 // Some LAPACK prototypes.
+
+// LU factorization.
 void dgetrf(int *N, int *NRHS, double *A, int *LDA, int *IPIV, int *INFO);
+
+// Solution of a linear system using an LU factorization.
 void dgetrs(char *TRANS, int *N, int *NRHS, double *A, 
             int *LDA, int *IPIV, double *B, int *LDB, int *INFO);
+
+// Matrix-vector multiplication: y := alpha*A*x + beta*y.
+void dgemv(const char *trans, int *m, int *n, double *alpha,
+           void *a, int *lda, void *x, int *incx,
+           double *beta, void *y, int *incy);
 
 // Shape function basis.
 struct poly_ls_shape_basis_t 
 {
   int p; // Order of basis.
+  ls_weighting_func_t weighting_func;
 };
 
 static void poly_ls_shape_basis_free(void* context, void* dummy)
@@ -257,7 +272,7 @@ poly_ls_shape_basis_t* poly_ls_shape_basis_new(int p)
   return N;
 }
 
-void poly_ls_shape_basis_compute(poly_ls_shape_basis_t* N, point_t* x0, point_t* points, int num_points, double* values)
+void poly_ls_shape_basis_compute(poly_ls_shape_basis_t* N, point_t* x0, point_t* points, int num_points, point_t* x, double* values)
 {
   int dim = poly_ls_basis_size(N->p);
 
@@ -266,15 +281,20 @@ void poly_ls_shape_basis_compute(poly_ls_shape_basis_t* N, point_t* x0, point_t*
   memset(A, 0, sizeof(double)*dim*dim);
   for (int n = 0; n < num_points; ++n)
   {
+    // Expand about x0.
     point_t y = {.x = points[n].x - x0->x, 
                  .y = points[n].y - x0->y,
                  .z = points[n].z - x0->z};
+    double d = y.x*y.x + y.y*y.y + y.z*y.z, Wn = 1.0;
+    vector_t gradWn = {.x = 0, .y = 0, .z = 0};
+    if (N->weighting_func != NULL)
+      N->weighting_func(d, &Wn, &gradWn);
     compute_poly_ls_basis_vector(N->p, &y, basis);
     memcpy(&P[dim*n], basis, dim*sizeof(double));
     for (int i = 0; i < dim; ++i)
     {
       for (int j = 0; j < dim; ++j)
-        A[dim*j+i] += basis[i]*basis[j];
+        A[dim*j+i] += basis[i]*Wn*basis[j];
     }
   }
 
@@ -283,15 +303,139 @@ void poly_ls_shape_basis_compute(poly_ls_shape_basis_t* N, point_t* x0, point_t*
   dgetrf(&dim, &dim, A, &lda, pivot, &info);
   ASSERT(info == 0);
 
-  // Now compute the values of the shape function basis at x0.
+  // Now compute the values of the shape function basis at x.
   char trans = 'N';
   int ldb = dim, one = 1;
   double Ainvb[dim];
+  memset(values, 0, sizeof(double)*num_points);
+  {
+    compute_poly_ls_basis_vector(N->p, x, basis);
+    for (int n = 0; n < num_points; ++n)
+    {
+      memcpy(Ainvb, &P[dim*n], dim*sizeof(double));
+      dgetrs(&trans, &dim, &one, A, &lda, pivot, Ainvb, &ldb, &info);
+      for (int i = 0; i < dim; ++i)
+        values[n] += basis[i]*Ainvb[i];
+    }
+  }
+}
+
+void poly_ls_shape_basis_compute_gradients(poly_ls_shape_basis_t* N, point_t* x0, point_t* points, int num_points, point_t* x, double* values, vector_t* gradients)
+{
+  int dim = poly_ls_basis_size(N->p);
+
+  // Compute the moment matrix and its gradient.
+  double A[dim*dim], basis[dim], P[dim*dim], W[num_points],
+         gradAx[dim*dim], gradAy[dim*dim], gradAz[dim*dim];
+  vector_t gradW[num_points];
+  memset(A, 0, sizeof(double)*dim*dim);
+  memset(gradAx, 0, sizeof(double)*dim*dim);
+  memset(gradAy, 0, sizeof(double)*dim*dim);
+  memset(gradAz, 0, sizeof(double)*dim*dim);
+  memset(gradW, 0, sizeof(vector_t)*num_points);
   for (int n = 0; n < num_points; ++n)
   {
-    memcpy(Ainvb, &P[dim*n], dim*sizeof(double));
-    dgetrs(&trans, &dim, &one, A, &lda, pivot, Ainvb, &ldb, &info);
-    values[n] = Ainvb[n];
+    // Expand about x0.
+    point_t y = {.x = points[n].x - x0->x, 
+                 .y = points[n].y - x0->y,
+                 .z = points[n].z - x0->z};
+
+    // Compute the weighting functions.
+    double d = y.x*y.x + y.y*y.y + y.z*y.z;
+    W[n] = 1.0;
+    if (N->weighting_func != NULL)
+      N->weighting_func(d, &W[n], &gradW[n]);
+
+    compute_poly_ls_basis_vector(N->p, &y, basis);
+    memcpy(&P[dim*n], basis, dim*sizeof(double));
+    for (int i = 0; i < dim; ++i)
+    {
+      for (int j = 0; j < dim; ++j)
+      {
+        A[dim*j+i] += basis[i]*W[n]*basis[j];
+        gradAx[dim*j+i] += basis[i]*gradW[n].x*basis[j];
+        gradAy[dim*j+i] += basis[i]*gradW[n].y*basis[j];
+        gradAz[dim*j+i] += basis[i]*gradW[n].z*basis[j];
+      }
+    }
+  }
+
+  // Factor the moment matrix.
+  int lda = dim, pivot[dim], info;
+  dgetrf(&dim, &dim, A, &lda, pivot, &info);
+  ASSERT(info == 0);
+
+  // Now compute the values and gradients of the shape function basis at x.
+  char trans = 'N';
+  int ldb = dim, one = 1;
+  double alpha = 1.0, beta = 0.0;
+  double Ainv_p[dim];
+  memset(values, 0, sizeof(double)*num_points);
+  memset(gradients, 0, sizeof(vector_t)*num_points);
+  {
+    compute_poly_ls_basis_vector(N->p, x, basis);
+    compute_poly_ls_basis_gradient(N->p, x, gradients);
+    for (int n = 0; n < num_points; ++n)
+    {
+      // Compute Ainv * p.
+      memcpy(Ainv_p, &P[dim*n], dim*sizeof(double));
+      dgetrs(&trans, &dim, &one, A, &lda, pivot, Ainv_p, &ldb, &info);
+
+      // First pass: pT_Ainv_p and dpT_Ainv_p.
+      double pT_Ainv_p = 0.0;
+      vector_t gradpT_Ainv_p = {.x = 0., .y = 0., .z = 0.};
+      for (int i = 0; i < dim; ++i)
+      {
+        pT_Ainv_p += basis[i]*Ainv_p[i];
+        gradpT_Ainv_p.x += gradients[i].x * Ainv_p[i];
+        gradpT_Ainv_p.y += gradients[i].y * Ainv_p[i];
+        gradpT_Ainv_p.z += gradients[i].z * Ainv_p[i];
+      }
+      values[n] = pT_Ainv_p;
+
+      // Second pass: Ainv_gradA_Ainv_p.
+      double Ainv_gradA_Ainv_px[dim],
+             Ainv_gradA_Ainv_py[dim],
+             Ainv_gradA_Ainv_pz[dim];
+      for (int i = 0; i < dim; ++i)
+      {
+        // gradA * Ainv_p -> Ainv_gradA_Ainv_p.
+        dgemv(&trans, &dim, &dim, &alpha, gradAx, &dim, Ainv_p, &one, &beta,
+              Ainv_gradA_Ainv_px, &one);
+        dgemv(&trans, &dim, &dim, &alpha, gradAy, &dim, Ainv_p, &one, &beta,
+              Ainv_gradA_Ainv_py, &one);
+        dgemv(&trans, &dim, &dim, &alpha, gradAz, &dim, Ainv_p, &one, &beta,
+              Ainv_gradA_Ainv_pz, &one);
+
+        // Solve A * X = gradA * Ainv_p; X -> Ainv_gradA_Ainv_p.
+        dgetrs(&trans, &dim, &one, A, &lda, pivot, Ainv_gradA_Ainv_px, &ldb, &info);
+        ASSERT(info == 0);
+        dgetrs(&trans, &dim, &one, A, &lda, pivot, Ainv_gradA_Ainv_py, &ldb, &info);
+        ASSERT(info == 0);
+        dgetrs(&trans, &dim, &one, A, &lda, pivot, Ainv_gradA_Ainv_pz, &ldb, &info);
+        ASSERT(info == 0);
+      }
+      
+      // Third pass: pT_Ainv_gradA_Ainv_p.
+      vector_t pT_Ainv_gradA_Ainv_p = {.x = 0., .y = 0., .z = 0.};
+      for (int i = 0; i < dim; ++i)
+      {
+        pT_Ainv_gradA_Ainv_p.x += basis[i] * Ainv_gradA_Ainv_px[i];
+        pT_Ainv_gradA_Ainv_p.y += basis[i] * Ainv_gradA_Ainv_py[i];
+        pT_Ainv_gradA_Ainv_p.y += basis[i] * Ainv_gradA_Ainv_pz[i];
+      }
+
+      // Third pass: compute the shape function gradients.
+      for (int i = 0; i < dim; ++i)
+      {
+        gradients[n].x += (gradpT_Ainv_p.x - pT_Ainv_gradA_Ainv_p.x) * W[n] + 
+                          pT_Ainv_p * gradW[n].x;
+        gradients[n].y += (gradpT_Ainv_p.y - pT_Ainv_gradA_Ainv_p.y) * W[n] + 
+                          pT_Ainv_p * gradW[n].y;
+        gradients[n].z += (gradpT_Ainv_p.z - pT_Ainv_gradA_Ainv_p.z) * W[n] + 
+                          pT_Ainv_p * gradW[n].z;
+      }
+    }
   }
 }
 
