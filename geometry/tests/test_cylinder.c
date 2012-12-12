c785a697c3848f4398c0d51885e766b96781cd0a
Jeffrey Johnson <jjphatt@gmail.com>
2012-12-11 23:19:54 -0800

    Factored out Octave script generation.
    Former-commit-id: 330ee1af6aa1d8e52750b15fa82e5b12104e0bb5

--- geometry/tests/test_cylinder.c
+++ geometry/tests/test_cylinder.c
@@ -4,6 +4,7 @@
 #include <string.h>
 #include "cmockery.h"
 #include "cylinder.h"
+#include "generate_octave_script_for_surface.h"
 
 void test_construct(void** state)
 {
@@ -25,55 +26,8 @@ void test_plot(void** state)
   vector_t oblique = {1.0, 1.0, 1.0};
   point_t origin = {0.0, 0.0, 0.0};
   sp_func_t* c = cylinder_new(&oblique, &origin, 0.5, OUTWARD_NORMAL);
-  FILE* fd = fopen("test_cylinder.m", "w");
-  fprintf(fd, "%% test_cylinder.m - A script for visualizing a cylinder.\n");
-  fprintf(fd, "%% Run with octave --persist test_cylinder.m\n\n");
-  int N = 20;
-  double h = 2.0/N;
-  point_t x;
-
-  // Write out X, Y, and Z arrays.
-  fprintf(fd, "X = [");
-  for (int i = 0; i < N; ++i)
-    fprintf(fd, "%g ", -1.0 + (i+0.5)*h);
-  fprintf(fd, "];\n");
-
-  fprintf(fd, "Y = [");
-  for (int i = 0; i < N; ++i)
-    fprintf(fd, "%g ", -1.0 + (i+0.5)*h);
-  fprintf(fd, "];\n");
-
-  fprintf(fd, "Z = [");
-  for (int i = 0; i < N; ++i)
-    fprintf(fd, "%g ", -1.0 + (i+0.5)*h);
-  fprintf(fd, "];\n");
-
-  fprintf(fd, "F = zeros(%d, %d, %d);\n", N, N, N);
-  for (int i = 0; i < N; ++i)
-  {
-    x.x = -1.0 + (i+0.5)*h;
-    for (int j = 0; j < N; ++j)
-    {
-      x.y = -1.0 + (j+0.5)*h;
-      for (int k = 0; k < N; ++k)
-      {
-        x.z = -1.0 + (k+0.5)*h;
-        double F;
-        sp_func_eval(c, &x, &F);
-        fprintf(fd, "F(%d, %d, %d) = %g;\n", i+1, j+1, k+1, F);
-      }
-    }
-  }
-
-  // Dump out the rest of the script for Octave.
-  static const char* instructions = 
-    "[XX, YY, ZZ] = meshgrid(X, Y, Z);\n"
-    "isosurface(XX, YY, ZZ, F, 0.0);\n"
-    "xlabel('x');\n"
-    "ylabel('y');\n"
-    "zlabel('z');\n";
-  fprintf(fd, "%s", instructions);
-  fclose(fd);
+  bbox_t bbox = {.x1 = -1.0, .x2 = 1.0, .y1 = -1.0, .y2 = 1.0, .z1 = -1.0, .z2 = 1.0};
+  generate_octave_script_for_surface(c, 20, &bbox, "test_cylinder.m");
 }
 
 int main(int argc, char* argv[]) 
