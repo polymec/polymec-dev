7da26158e2785fe9ddbb442dbccea63d2944609c
Jeffrey Johnson <jjphatt@gmail.com>
2012-12-15 08:43:33 -0800

    Consolodated brent solver with newton/picard solvers, added multi-dim picard iteration.
    Former-commit-id: 7bf0f401169ee07dacd40d4a266aa3163d85c4ca

--- core/newton.h
+++ core/newton.h
@@ -11,17 +11,27 @@ extern "C" {
 // (real-valued) function of a single variable given a context.
 typedef void (*nonlinear_function_t)(void*, double, double*, double*);
 
+// This defines a vector-valued function that computes its value and 
+// Jacobian given a context. 
+typedef void (*nonlinear_vector_function_t)(void*, double, double*, double*);
+
 // Solve the nonlinear equation F(x) = 0 using Newton-Raphson iteration on 
 // the interval [min, max] using the initial estimate x, with the specified 
 // maximum number of iterations and desired tolerance.
 // Returns true if the solution was achieved, false otherwise.
 bool newton_solve(nonlinear_function_t F, void* context, double* x, double min, double max, double tolerance, int max_iters); 
 
-// Solve the nonlinear equation F(x) = 0 using Picard iteration on 
-// the interval [min, max] using the initial estimate x, with the specified 
-// maximum number of iterations and desired tolerance.
+// Solve the nonlinear equation F(x) = 0 using Picard iteration 
+// using the initial estimate x, with the specified maximum number of 
+// iterations and desired tolerance.
 // Returns true if the solution was achieved, false otherwise.
-bool picard_solve(nonlinear_function_t F, void* context, double* x, double min, double max, double tolerance, int max_iters); 
+bool picard_solve(nonlinear_function_t F, void* context, double* x, double tolerance, int max_iters); 
+
+// Solve the nonlinear system F(x) = 0 using Picard iteration using the 
+// initial estimate vector x, with the specified maximum number of iterations 
+// and desired tolerance.
+// Returns true if the solution was achieved, false otherwise.
+bool picard_solve_system(int dim, nonlinear_vector_function_t F, void* context, double* x, double tolerance, int max_iters); 
 
 // Solve the nonlinear equation F(x) = 0 using a hybrid Newton-Raphson/Picard 
 // iteration on the interval [min, max] using the initial estimate x, with 
@@ -30,6 +40,12 @@ bool picard_solve(nonlinear_function_t F, void* context, double* x, double min, 
 // performs picard_iters iterations. This cycle is performed num_cycles times.
 // Returns true if the solution was achieved, false otherwise.
 bool newton_picard_solve(nonlinear_function_t F, void* context, double* x, double min, double max, double tolerance, int newton_iters, int picard_iters, int num_cycles); 
+
+// Solve the nonlinear equation F(x) = 0 using Brent's method on 
+// the interval [x1, x2] with the specified maximum number of iterations 
+// and desired tolerance. Returns the solution x. NOTE that the nonlinear
+// function F need not have a derivative for Brent's method.
+double brent_solve(nonlinear_function_t F, void* context, double x1, double x2, double tolerance, int max_iters, double* error); 
 
 #ifdef __cplusplus
 }
