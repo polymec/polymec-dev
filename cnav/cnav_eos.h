2d746d0eab09a1ebdbd92d85db515b09eb6bd275
Jeffrey Johnson <jjphatt@gmail.com>
2013-01-11 02:17:55 -0800

    Laying down some more structure.
    Former-commit-id: 3c9ab0be426e2b64e1a05c29f30745bf03233a26

--- cnav/cnav_eos.h
+++ cnav/cnav_eos.h
@@ -8,8 +8,16 @@ extern "C" {
 #endif
 
 // This type represents a multi-component equation of state for the 
-// compressible Navier-Stokes solver.
+// compressible Navier-Stokes solver. Objects of this type are 
+// garbage-collected.
 typedef struct cnav_eos_t cnav_eos_t;
+
+// Returns the name of the equation of state.
+char* cnav_eos_name(cnav_eos_t* eos);
+
+// Returns the number of species in the material described by this
+// equation of state.
+int cnav_eos_num_species(cnav_eos_t* eos);
 
 #ifdef __cplusplus
 }
