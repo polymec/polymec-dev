47a27f6bd676a7c9ed500820801e34cce007cafd
Jeffrey Johnson <jjphatt@gmail.com>
2013-01-12 23:18:47 -0800

    Starting on explicit RK integrators.
    Former-commit-id: be04fe36954873b66d767eee95d6bba4acd5864e

---
+++ integrators/rk_integrator.h
@@ -0,0 +1,29 @@
+#ifndef POLYMEC_RK_INTEGRATOR_H
+#define POLYMEC_RK_INTEGRATOR_H
+
+#include "core/integrator.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// A function signature for explicitly computing the derivative of a solution 
+// at the given time. Arguments are:
+// 1. A context object
+// 2. The time t
+// 3. The solution u 
+// 4. Storage for the derivative of the solution, du/dt.
+typedef void (*rk_compute_deriv)(void*, double, double*, double*);
+
+// Creates an explicit Runge-Kutta integrator of the given order.
+integrator_t* rk_integrator_new(int order,
+                                void* context, 
+                                rk_compute_deriv compute_deriv,
+                                integrator_dtor dtor);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
