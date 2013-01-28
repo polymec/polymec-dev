fb318fe0ca64034b43e1fe59b12e9bdb1f6f76b6
Jeffrey Johnson <jjphatt@gmail.com>
2013-01-27 18:20:42 -0800

    Added a test for the table data structure.
    Former-commit-id: 953efa05518bea26e68e09799ff6b9231d431c25

--- core/table_t.h
+++ core/table_t.h
@@ -16,7 +16,7 @@
 // x_table_t* x_table_copy(x_table_t* t) - Creates a (shallow) copy of t.
 // void x_table_free(x_table_t* table) - Destroys the table.
 // void x_table_clear(x_table_t* table) - Empties the table.
-// bool x_table_get_row(x_table_t* table, int row, int* num_cols, int** cols, x_table_value_t** values) - Retrieves the given row in the table, returning true if successful, false if not.
+// x_table_row_t* x_table_get_row(x_table_t* table, int row) - Returns the row in the given table (itself an unordered map).
 // x_table_value_t* x_table_get(x_table_t* table, int row, int col) - Returns the element at the given row/column, or NULL.
 // bool x_table_contains_row(x_table_t* table, int row) - Returns true if the table contains the given row, false if not.
 // bool x_table_contains(x_table_t* table, int row, int col) - Returns true if the table contains an element at row/column, false if not.
@@ -24,7 +24,6 @@
 // void x_table_insert_row_with_dtor(x_table_t* table, int row, int num_cols, int* cols, x_table_value_t* values, x_table_value_dtor dtor) - Inserts a row into the table with a destructor.
 // void x_table_insert(x_table_t* table, int row, int col, x_table_value_t value) - Inserts a value at the given row/column.
 // void x_table_insert_with_dtor(x_table_t* table, int row, int col, x_table_value_t value, x_table_value_dtor dtor) - Inserts a value at the given row/column with a destructor.
-// void x_table_copy_row(x_table_t* table, int src_row, int dest_row) - Copies the contents of src_row into dest_row.
 // void x_table_delete_row(x_table_t* table, int row) - Deletes the given row from the table.
 // void x_table_delete(x_table_t* table, int row, col) - Deletes the given element from the table.
 // bool x_table_next_row(x_table_t* map, int* pos, int* row, int* num_cols, int** cols, x_table_value_t** values) - Allows the traversal of the table rows.
@@ -32,45 +31,8 @@
 #define DEFINE_TABLE(table_name, value_type) \
 typedef value_type table_name##_value_t; \
 typedef void (*table_name##_value_dtor)(table_name##_value_t); \
-typedef struct \
-{ \
-  int num_cols; \
-  int* cols; \
-  table_name##_value_t* values; \
-  table_name##_value_dtor dtors; \
-} table_name##_row_t; \
-\
-static inline table_name##_row_t* table_name##_row_new(int num_cols, int* cols, table_name##_value_t* values, table_name##_value_dtor* dtors) \
-{ \
-  ASSERT(num_cols > 0); \
-  ASSERT(cols != NULL); \
-  ASSERT(values != NULL); \
-  table_name##_row_t* row = malloc(sizeof(table_name##_row_t)); \
-  row->num_cols = num_cols; \
-  row->cols = malloc(sizeof(int)*num_cols); \
-  for (int c = 0; c < num_cols; ++c) \
-  { \
-    row->cols[c] = cols[c]; \
-    row->values[c] = values[c]; \
-    row->dtors[c] = (dtors != NULL) ? dtors[c] : NULL; \
-  } \
-  return row; \
-} \
-\
-static inline void table_name##_row_free(table_name##_row_t* row) \
-{ \
-  for (int i = 0; i < row->num_cols; ++i) \
-  { \
-    if (row->dtors[i] != NULL) \
-      row->dtors[i](row->values[i]); \
-  } \
-  free(row->cols); \
-  free(row->values); \
-  free(row->dtors); \
-  free(row); \
-} \
-\
-DEFINE_UNORDERED_MAP(table_name##_map_t, int, table_name##_row_t*, int_hash, int_equals) \
+DEFINE_UNORDERED_MAP(table_name##_row, int, value_type, int_hash, int_equals) \
+DEFINE_UNORDERED_MAP(table_name##_map, int, table_name##_row_t*, int_hash, int_equals) \
 \
 typedef struct \
 { \
@@ -103,14 +65,9 @@ static inline void table_name##_free(table_name##_t* table) \
   free(table); \
 } \
 \
-static inline bool table_name##_get_row(table_name##_t* table, int row, int* num_cols, int** cols, table_name##_value_t** values) \
+static inline table_name##_row_t** table_name##_get_row(table_name##_t* table, int row) \
 { \
-  table_name##_row_t** row = table_name##_map_get(table->map, row); \
-  if (row == NULL) \
-    return false; \
-  *num_cols = *row->num_cols; \
-  *cols = *row->cols; \
-  *values = *row->values; \
+  return table_name##_map_get(table->map, row); \
 } \
 \
 static inline table_name##_value_t* table_name##_get(table_name##_t* table, int row, int col) \
@@ -119,12 +76,7 @@ static inline table_name##_value_t* table_name##_get(table_name##_t* table, int 
   if (row == NULL) \
     return NULL; \
   table_name##_row_t* r = *row; \
-  for (int c = 0; c < r->num_cols; ++c) \
-  { \
-    if (r->cols[c] == col) \
-      return &r->values[c]; \
-  } \
-  return NULL; \
+  return table_name##_row_get(r, col); \
 } \
 \
 static inline bool table_name##_contains_row(table_name##_t* table, int row) \
@@ -139,15 +91,11 @@ static inline bool table_name##_contains(table_name##_t* table, int row, int col
 \
 static inline void table_name##_insert_row_with_dtor(table_name##_t* table, int row, int num_cols, int* cols, table_name##_value_t* values, table_name##_value_dtor dtor) \
 { \
-  table_name##_value_dtor* dtors; \
-  if (dtor != NULL) \
-  { \
-    dtors = malloc(sizeof(table_name##_value_dtor)); \
-    for (int c = 0; c < num_cols; ++c) \
-      dtors[c] = dtor; \
-  } \
-  table_name##_row_t* r = table_name##_row_new(num_cols, cols, values, dtors); \
+  table_name##_row_t* r = table_name##_row_new(); \
+  for (int c = 0; c < num_cols; ++c) \
+    table_name##_row_insert_with_dtor(r, cols[c], values[c], dtor); \
   table_name##_map_insert_with_dtor(table->map, row, r, table_name##_row_free); \
+  table->num_rows = table->map->size; \
 } \
 \
 static inline void table_name##_insert_row(table_name##_t* table, int row, int num_cols, int* cols, table_name##_value_t* values) \
@@ -157,43 +105,44 @@ static inline void table_name##_insert_row(table_name##_t* table, int row, int n
 \
 static inline void table_name##_insert_with_dtor(table_name##_t* table, int row, int col, table_name##_value_t value, table_name##_value_dtor dtor) \
 { \
+  table_name##_row_t** row = table_name##_map_get(table->map, row); \
+  table_name##_row_t* r; \
+  if (row != NULL) \
+  { \
+    r = *row; \
+  } \
+  else \
+  { \
+    r = table_name##_row_new(); \
+    table_name##_map_insert_with_dtor(table->map, row, r, table_name##_row_free); \
+    table->num_rows = table->map->size; \
+  } \
+  table_name##_row_insert_with_dtor(r, col, value, dtor); \
 } \
+\
 static inline void table_name##_insert(table_name##_t* map, int row, int col, table_name##_value_t value) \
 { \
   table_name##_insert_with_dtor(map, row, col, value, NULL); \
 } \
 \
-static inline void table_name##_copy_row(table_name##_t* table, int src_row, int dest_row) \
-{ \
-  table_name##_row_t** row = table_name##_map_get(table->map, src_row); \
-  if (row == NULL) return; \
-  table_name##_row_t* r = *row; \
-  table_name##_insert_row(table, dest_row, r->num_cols, r->cols, r->values); \
-} \
-\
 static inline void table_name##_delete_row(table_name##_t* table, int row) \
 { \
   table_name##_map_delete(table->map, row); \
+  table->num_rows = table->map->size; \
 } \
 \
 static inline void table_name##_delete(table_name##_t* table, int row, int col) \
 { \
-  table_name##_row_t** row = table_name##_map_get(table->map, src_row); \
-  if (row == NULL) return; \
-  table_name##_row_delete_col(*row, col); \
+  table_name##_row_t** r = table_name##_map_get(table->map, row); \
+  if (r == NULL) return; \
+  table_name##_row_delete(*r, col); \
+  if (*r->size == 0) \
+    table_name##_delete_row(table, row); \
 } \
 \
-static inline bool table_name##_next_row(table_name##_t* table, int* pos, int* row, int* num_cols, int** cols, table_name##_value_t** values) \
+static inline bool table_name##_next_row(table_name##_t* table, int* pos, int row, table_name##_row_t** row_data) \
 { \
-  table_name##_row_t* r; \
-  bool result = table_name##_map_next(table->map, pos, row, &r); \
-  if (result) \
-  { \
-    *num_cols = r->num_cols; \
-    *cols = r->cols; \
-    *values = r->values; \
-  } \
-  return result; \
+  return table_name##_map_next(table->map, pos, row, row_data); \
 } \
 \
 static inline table_name##_t* table_name##_copy(table_name##_t* table) \
---
+++ core/tests/test_table.c
@@ -0,0 +1,59 @@
+#include <stdarg.h>
+#include <stddef.h>
+#include <setjmp.h>
+#include <string.h>
+#include "cmockery.h"
+#include "core/table.h"
+
+int rows[] = {0, 1, 2, 4, 8, 16};
+int cols[] = {0, 10, 20, 40, 80, 160};
+int int_values[] = {0, 1, 2, 3, 4, 5};
+double double_values[] = {0., 1., 2., 3., 4., 5.};
+
+#define DEFINE_TABLE_TEST(table_name, element) \
+void test_##table_name##_ctor(void** state) \
+{ \
+  table_name##_t* t = table_name##_new(); \
+  assert_int_equal(0, t->num_rows); \
+  table_name##_free(t); \
+} \
+\
+void test_##table_name##_insert(void** state) \
+{ \
+  table_name##_t* t = table_name##_new(); \
+  for (int i = 0; i < 5; ++i) \
+    table_name##_insert(t, rows[i], cols[i], element##_values[i]); \
+  assert_int_equal(5, t->num_rows); \
+  for (int i = 0; i < 5; ++i) \
+    table_name##_insert(t, rows[i], cols[i], element##_values[i]); \
+  assert_int_equal(5, t->size); \
+  for (int i = 0; i < 5; ++i) \
+  { \
+    assert_true(table_name##_contains_row(t, rows[i])); \
+    assert_true(table_name##_contains(t, rows[i], cols[i])); \
+    assert_true(*table_name##_get(t, rows[i], cols[i]) == element##_values[i]); \
+  } \
+  table_name##_clear(m); \
+  assert_int_equal(0, t->num_rows); \
+  table_name##_free(t); \
+} \
+\
+void test_##element##_table(void** state) \
+{ \
+  test_##table_name##_ctor(state); \
+  test_##table_name##_insert(state); \
+}
+
+DEFINE_TABLE_TEST(int_table, int)
+DEFINE_TABLE_TEST(double_table, double)
+
+int main(int argc, char* argv[]) 
+{
+  polymec_init(argc, argv);
+  const UnitTest tests[] = 
+  {
+    unit_test(test_int_table),
+    unit_test(test_double_table)
+  };
+  return run_tests(tests);
+}
