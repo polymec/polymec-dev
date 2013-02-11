f6f16df3ff78a62e27c40e76f95e71dc76350212
Jeffrey Johnson <jjphatt@gmail.com>
2013-02-11 12:45:03 -0800

    Some work on surface mesh supporting functions.
    Former-commit-id: 6c542137974c86ee11be5f4338ef192fa17c796c

--- core/surface_mesh.c
+++ core/surface_mesh.c
@@ -1,4 +1,5 @@
 #include "core/surface_mesh.h"
+#include "core/edit_surface_mesh.h"
 #include "core/mesh_storage.h"
 
 #ifdef __cplusplus
@@ -50,6 +51,130 @@ surface_mesh_t* surface_mesh_new_with_arena(ARENA* arena, int num_faces, int num
   s->storage->edge_capacity = edge_cap;
   s->storage->face_capacity = face_cap;
   s->storage->cell_capacity = 0;
+
+  return s;
+}
+
+surface_mesh_t* surface_mesh_from_bbox(bbox_t* bbox)
+{
+  ARENA* a = arena_open(&arena_defaults, 0);
+  surface_mesh_t* s = surface_mesh_from_bbox_with_arena(a, bbox);
+  s->close_arena = true;
+  return s;
+}
+
+surface_mesh_t* surface_mesh_from_bbox_with_arena(ARENA* arena, bbox_t* bbox)
+{
+  // The bounding box has six faces, each of which is rectangular. We split 
+  // each rectangle into 2 triangles.
+  int num_faces = 2*6, num_edges = 12+4, num_nodes = 8;
+  surface_mesh_t* s = surface_mesh_new_with_arena(arena, num_faces, num_edges, num_nodes);
+
+  // For this surface mesh, we use the usual finite-element-inspired node 
+  // ordering for a hexahedron.
+
+  // Edges.
+  s->edges[0].node1 = &s->nodes[0];
+  s->edges[0].node2 = &s->nodes[4];
+
+  s->edges[1].node1 = &s->nodes[4];
+  s->edges[1].node2 = &s->nodes[7];
+
+  s->edges[2].node1 = &s->nodes[3];
+  s->edges[2].node2 = &s->nodes[7];
+
+  s->edges[3].node1 = &s->nodes[0];
+  s->edges[3].node2 = &s->nodes[3];
+
+  s->edges[4].node1 = &s->nodes[1];
+  s->edges[4].node2 = &s->nodes[5];
+
+  s->edges[5].node1 = &s->nodes[5];
+  s->edges[5].node2 = &s->nodes[6];
+
+  s->edges[6].node1 = &s->nodes[2];
+  s->edges[6].node2 = &s->nodes[6];
+
+  s->edges[7].node1 = &s->nodes[1];
+  s->edges[7].node2 = &s->nodes[2];
+
+  s->edges[8].node1 = &s->nodes[0];
+  s->edges[8].node2 = &s->nodes[1];
+
+  s->edges[9].node1 = &s->nodes[4];
+  s->edges[9].node2 = &s->nodes[5];
+
+  s->edges[10].node1 = &s->nodes[2];
+  s->edges[10].node2 = &s->nodes[3];
+
+  s->edges[11].node1 = &s->nodes[6];
+  s->edges[11].node2 = &s->nodes[7];
+
+  s->edges[12].node1 = &s->nodes[0];
+  s->edges[12].node2 = &s->nodes[7];
+
+  s->edges[13].node1 = &s->nodes[1];
+  s->edges[13].node2 = &s->nodes[4];
+
+  s->edges[14].node1 = &s->nodes[2];
+  s->edges[14].node2 = &s->nodes[5];
+
+  s->edges[15].node1 = &s->nodes[3];
+  s->edges[15].node2 = &s->nodes[6];
+
+  // Triangles 0 and 1 (-x).
+  surface_mesh_add_edge_to_face(s, &s->edges[0], &s->faces[0]);
+  surface_mesh_add_edge_to_face(s, &s->edges[1], &s->faces[0]);
+  surface_mesh_add_edge_to_face(s, &s->edges[12], &s->faces[0]);
+
+  surface_mesh_add_edge_to_face(s, &s->edges[2], &s->faces[1]);
+  surface_mesh_add_edge_to_face(s, &s->edges[3], &s->faces[1]);
+  surface_mesh_add_edge_to_face(s, &s->edges[12], &s->faces[1]);
+
+  // Triangles 2 and 3 (+x).
+  surface_mesh_add_edge_to_face(s, &s->edges[6], &s->faces[2]);
+  surface_mesh_add_edge_to_face(s, &s->edges[5], &s->faces[2]);
+  surface_mesh_add_edge_to_face(s, &s->edges[14], &s->faces[2]);
+
+  surface_mesh_add_edge_to_face(s, &s->edges[7], &s->faces[3]);
+  surface_mesh_add_edge_to_face(s, &s->edges[4], &s->faces[3]);
+  surface_mesh_add_edge_to_face(s, &s->edges[14], &s->faces[3]);
+
+  // Triangles 4 and 5 (-y). // FIXME
+  surface_mesh_add_edge_to_face(s, &s->edges[6], &s->faces[4]);
+  surface_mesh_add_edge_to_face(s, &s->edges[5], &s->faces[4]);
+  surface_mesh_add_edge_to_face(s, &s->edges[14], &s->faces[4]);
+
+  surface_mesh_add_edge_to_face(s, &s->edges[7], &s->faces[5]);
+  surface_mesh_add_edge_to_face(s, &s->edges[4], &s->faces[5]);
+  surface_mesh_add_edge_to_face(s, &s->edges[14], &s->faces[5]);
+
+  // Triangles 6 and 7 (+y). // FIXME
+  surface_mesh_add_edge_to_face(s, &s->edges[6], &s->faces[6]);
+  surface_mesh_add_edge_to_face(s, &s->edges[5], &s->faces[6]);
+  surface_mesh_add_edge_to_face(s, &s->edges[14], &s->faces[6]);
+
+  surface_mesh_add_edge_to_face(s, &s->edges[7], &s->faces[7]);
+  surface_mesh_add_edge_to_face(s, &s->edges[4], &s->faces[7]);
+  surface_mesh_add_edge_to_face(s, &s->edges[14], &s->faces[7]);
+
+  // Triangles 8 and 9 (-z). // FIXME
+  surface_mesh_add_edge_to_face(s, &s->edges[6], &s->faces[8]);
+  surface_mesh_add_edge_to_face(s, &s->edges[5], &s->faces[8]);
+  surface_mesh_add_edge_to_face(s, &s->edges[14], &s->faces[8]);
+
+  surface_mesh_add_edge_to_face(s, &s->edges[7], &s->faces[9]);
+  surface_mesh_add_edge_to_face(s, &s->edges[4], &s->faces[9]);
+  surface_mesh_add_edge_to_face(s, &s->edges[14], &s->faces[9]);
+
+  // Triangles 10 and 11 (+z). // FIXME
+  surface_mesh_add_edge_to_face(s, &s->edges[6], &s->faces[10]);
+  surface_mesh_add_edge_to_face(s, &s->edges[5], &s->faces[10]);
+  surface_mesh_add_edge_to_face(s, &s->edges[14], &s->faces[10]);
+
+  surface_mesh_add_edge_to_face(s, &s->edges[7], &s->faces[11]);
+  surface_mesh_add_edge_to_face(s, &s->edges[4], &s->faces[11]);
+  surface_mesh_add_edge_to_face(s, &s->edges[14], &s->faces[11]);
 
   return s;
 }
