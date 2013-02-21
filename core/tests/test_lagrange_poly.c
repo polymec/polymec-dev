3324845a0b27856aa2d67e1d04bcc33d214b7774
Jeffrey Johnson <jjphatt@gmail.com>
2013-02-21 15:36:49 -0800

    Finished Lagrange polynomial test.
    Former-commit-id: 49e179fc41face7192d0a2bb8cacb335be46301e

--- core/tests/test_lagrange_poly.c
+++ core/tests/test_lagrange_poly.c
@@ -37,23 +37,53 @@ void test_L1(void** state)
   assert_true(fabs(D2two - 0.0) < 1e-14);
 }
 
-#if 0
 void test_L2(void** state)
 {
-  static double points[] = {0.0, 0.5, 1.0};
-  static double values[] = {1.0, 2.0};
-  lagrange_poly_t* L1 = lagrange_poly_new(1);
-  lagrange_poly_set_points(L1, points);
+  // This is taken from 
+  // http://en.wikipedia.org/wiki/Lagrange_polynomial#Example_3
+  static double points[] = {1.0, 2.0, 3.0};
+  static double values[] = {1.0, 8.0, 27.0};
+  lagrange_poly_t* L2 = lagrange_poly_new(2);
+  lagrange_poly_set_points(L2, points);
 
   // Test values.
-  double one = lagrange_poly_value(L1, 0.0, values);
+  double one = lagrange_poly_value(L2, 1.0, values);
   assert_true(fabs(one - 1.0) < 1e-14);
-  double three_halves = lagrange_poly_value(L1, 0.5, values);
-  assert_true(fabs(three_halves - 1.5) < 1e-14);
-  double two = lagrange_poly_value(L1, 1.0, values);
-  assert_true(fabs(two - 2.0) < 1e-14);
+  double eight = lagrange_poly_value(L2, 2.0, values);
+  assert_true(fabs(eight - 8.0) < 1e-14);
+  double twenty_seven = lagrange_poly_value(L2, 3.0, values);
+  assert_true(fabs(twenty_seven - 27.0) < 1e-14);
+
+  // Non-interpolated values match the interpolation polynomial
+  // L2(x) = 6x^2 - 11x + 6.
+  double xs[3] = {1.5, 2.5, 3.5};
+  for (int i = 0; i < 3; ++i)
+  {
+    double val = lagrange_poly_value(L2, xs[i], values);
+    double L2_val = 6.0*xs[i]*xs[i] - 11.0*xs[i] + 6.0;
+    assert_true(fabs(val - L2_val) < 1e-14);
+  }
 }
-#endif
+
+void test_L4(void** state)
+{
+  // This is taken from 
+  // http://en.wikipedia.org/wiki/Lagrange_polynomial#Example_1
+  static double points[] = {-1.5, -0.75, 0.0, 0.75, 1.5};
+  static double values[] = {-14.1014, -0.931596, 0.0, 0.931596, 14.1014};
+  lagrange_poly_t* L4 = lagrange_poly_new(4);
+  lagrange_poly_set_points(L4, points);
+
+  // Non-interpolated values match the interpolation polynomial
+  // L4(x) = 4.834848*x^3 - 1.477474x
+  double xs[5] = {1.5, 2.5, 3.5, 4.5, 5.5};
+  for (int i = 0; i < 5; ++i)
+  {
+    double val = lagrange_poly_value(L4, xs[i], values);
+    double L4_val = 4.834848*xs[i]*xs[i]*xs[i] - 1.477474*xs[i];
+    assert_true(fabs(val - L4_val) < 1e-4);
+  }
+}
 
 int main(int argc, char* argv[]) 
 {
@@ -61,10 +91,9 @@ int main(int argc, char* argv[])
 
   const UnitTest tests[] = 
   {
-    unit_test(test_L1)
-//    unit_test(test_L2), 
-//    unit_test(test_L3),
-//    unit_test(test_L4)
+    unit_test(test_L1),
+    unit_test(test_L2), 
+    unit_test(test_L4)
   };
   return run_tests(tests);
 }
