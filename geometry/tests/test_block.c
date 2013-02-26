49057a7222b07963b9847b6ffba5d2e9a6723e76
Jeffrey Johnson <jjphatt@gmail.com>
2013-02-26 15:55:25 -0800

    Adding TODO.
    Former-commit-id: 07f772a2f1b28739a3d99fa34ac9385dea1c2c6d

--- geometry/tests/test_block.c
+++ geometry/tests/test_block.c
@@ -10,7 +10,8 @@ void test_linear_block(void** state)
   block_t* block = block_new(1);
   assert_int_equal(1, block_order(block));
   assert_int_equal(8, block_num_points(block));
-  point_t xs[] = {};
+  point_t xs[] = {{.x = 0.0, .y = 0.0, .z = 0.0},
+                  {.x = };
   point_t xi, x;
 
   block_map(block, xs, &xi, &x);
@@ -49,10 +50,10 @@ void test_block_tags(void** state)
 
   bool result = block_next_tag(block, 0, &pos, &tag);
   assert_true(result);
-  assert_true(!strcmp(tag, "left"));
+  assert_true(!strcmp(tag, "left") || !strcmp(tag, "left2"));
   result = block_next_tag(block, 0, &pos, &tag);
   assert_true(result);
-  assert_true(!strcmp(tag, "left2"));
+  assert_true(!strcmp(tag, "left2") || !strcmp(tag, "left"));
   result = block_next_tag(block, 0, &pos, &tag);
   assert_false(result);
 
