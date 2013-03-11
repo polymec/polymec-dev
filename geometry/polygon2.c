8cb22466a805fb69d95f1042e6e09e0034a7bb76
Jeffrey Johnson <jjphatt@gmail.com>
2013-03-11 13:55:05 -0700

    Implemented 2D polygon clipping algoritm.
    Former-commit-id: 40284dbc793045e7d1dac468e98e53af7d7b0fd9

--- geometry/polygon2.c
+++ geometry/polygon2.c
@@ -1,5 +1,6 @@
 #include <gc/gc.h>
 #include "geometry/polygon2.h"
+#include "core/slist.h"
 
 #ifdef __cplusplus
 extern "C" {
@@ -18,9 +19,15 @@ static void polygon2_free(void* ctx, void* dummy)
   free(poly->vertices);
 }
 
-static inline double triangle_area(point2_t* x1, point2_t* x2, point2_t* x3)
+static void polygon2_compute_area(polygon2_t* poly)
 {
-  return 0.5 * ((x2->x - x1->x) * (x3->y - x1->y) - (x3->x - x1->x) * (x2->y - x1->y));
+  // Compute the area using the fan algorithm.
+  poly->area = 0.0;
+  for (int j = 1; j < poly->num_vertices - 1; ++j)
+  {
+    // Form a triangle from vertex 0, vertex j, and vertex j+1.
+    poly->area += triangle_area(&poly->vertices[0], &poly->vertices[j], &poly->vertices[j+1]);
+  }
 }
 
 polygon2_t* polygon2_new(point2_t* vertices, int num_vertices)
@@ -31,15 +38,7 @@ polygon2_t* polygon2_new(point2_t* vertices, int num_vertices)
   poly->vertices = malloc(sizeof(point2_t)*num_vertices);
   memcpy(poly->vertices, vertices, sizeof(point2_t)*num_vertices);
   poly->num_vertices = num_vertices;
-
-  // Compute the area using the fan algorithm.
-  poly->area = 0.0;
-  for (int j = 1; j < num_vertices - 1; ++j)
-  {
-    // Form a triangle from vertex 0, vertex j, and vertex j+1.
-    poly->area += triangle_area(&vertices[0], &vertices[j], &vertices[j+1]);
-  }
-
+  polygon2_compute_area(poly);
   GC_register_finalizer(poly, polygon2_free, poly, NULL, NULL);
   return poly;
 }
@@ -128,15 +127,101 @@ polygon2_t* polygon2_clone(polygon2_t* poly)
   return polygon2_new(poly->vertices, poly->num_vertices);
 }
 
+static char parallel_int(point2_t* a, point2_t* b, point2_t* c, 
+                         point2_t* d, point2_t* p)
+{
+  if (!point2s_are_colinear(a, b, c))
+    return '0';
+  if (point2_is_between(c, a, b))
+  {
+    point2_copy(p, c);
+    return 'e';
+  }
+  if (point2_is_between(d, a, b))
+  {
+    point2_copy(p, d);
+    return 'e';
+  }
+  if (point2_is_between(a, c, d))
+  {
+    point2_copy(p, a);
+    return 'e';
+  }
+  if (point2_is_between(b, c, d))
+  {
+    point2_copy(p, b);
+    return 'e';
+  }
+  return '0';
+}
+
+static char seg_set_int(point2_t* a, point2_t* b, point2_t* c, 
+                        point2_t* d, point2_t* p)
+{
+  char code;
+
+  double denom = a->x * (d->y - c->y) + 
+                 b->x * (c->y - d->y) + 
+                 d->x * (b->y - a->y) + 
+                 c->x * (a->y - b->y);
+
+  // If denom is zero, segments are parallel.
+  if (denom == 0.0)
+    return parallel_int(a, b, c, d, p);
+
+  double num = a->x * (d->y - c->y) + 
+               c->x * (a->y - d->y) + 
+               d->x * (c->y - a->y);
+  if ((num == 0.0) || (num == denom)) 
+    code = 'v';
+  double s = num / denom;
+
+  num = -(a->x * (c->y - b->y) + 
+          b->x * (a->y - c->y) + 
+          c->x * (b->y - a->y));
+  if ((num == 0.0) || (num == denom))
+    code = 'v';
+  double t = num / denom;
+
+  if ((0.0 < s) && (s < 1.0) &&
+      (0.0 < t) && (t < 1.0))
+    code = '1';
+  else if ((0.0 > s) || (s > 1.0) || 
+           (0.0 > t) || (t > 1.0))
+    code = '0';
+
+  p->x = a->x + s * (b->x - a->x);
+  p->y = a->y + s * (b->y - a->y);
+
+  return code;
+}
+
 typedef enum
 {
   UNKNOWN,
-  IN,
-  OUT
+  PIN,
+  QIN
 } polygon2_inout_t;
 
-static int advance(int a, int* aa, int n, bool inside, point2_t* v)
+static polygon2_inout_t in_out(point2_t* p, polygon2_inout_t inflag, int aHB, int bHA, double_slist_t* xlist, double_slist_t* ylist)
 {
+  double_slist_append(xlist, p->x);
+  double_slist_append(ylist, p->y);
+  if (aHB > 0)
+    return PIN;
+  else if (bHA > 0)
+    return QIN;
+  else 
+    return inflag;
+}
+
+static int advance(int a, int* aa, int n, bool inside, point2_t* v, double_slist_t* xlist, double_slist_t* ylist)
+{
+  if (inside)
+  {
+    double_slist_append(xlist, v->x);
+    double_slist_append(ylist, v->y);
+  }
   (*aa)++;
   return (a+1) % n;
 }
@@ -149,31 +234,84 @@ void polygon2_clip(polygon2_t* poly, polygon2_t* other)
   int a = 0, b = 0, aa = 0, ba = 0;
   int n = poly->num_vertices, m = other->num_vertices;
   polygon2_inout_t inflag = UNKNOWN;
-  bool first_point = true;
   static point2_t origin = {.x = 0.0, .y = 0.0};
+  point2_t p0; // First point.
+
+  // We'll store the coordinates of the vertices of the 
+  // clipped polygon here.
+  double_slist_t* xlist = double_slist_new();
+  double_slist_t* ylist = double_slist_new();
+
   do
   {
     // Computations of key variables.
     int a1 = (a + n - 1) % n;
     int b1 = (b + m - 1) % m;
 
+    point2_t* Pa = &poly->vertices[a];
+    point2_t* Pa1 = &poly->vertices[a1];
+    point2_t* Qb = &other->vertices[b];
+    point2_t* Qb1 = &other->vertices[b1];
+
     point2_t A, B;
-    A.x = poly->vertices[a1].x - poly->vertices[a].x;
-    A.y = poly->vertices[a1].y - poly->vertices[a].y;
-    B.x = poly->vertices[b1].x - poly->vertices[b].x;
-    B.y = poly->vertices[b1].y - poly->vertices[b].y;
+    A.x = Pa1->x - Pa->x;
+    A.y = Pa1->y - Pa->y;
+    B.x = Qb1->x - Qb->x;
+    B.y = Qb1->y - Qb->y;
 
     int cross = SIGN(triangle_area(&origin, &A, &B));
-    int aHB   = SIGN(triangle_area(&other->vertices[b1], &other->vertices[b], &poly->vertices[a]));
-    int bHA   = SIGN(triangle_area(&poly->vertices[a1], &poly->vertices[a], &other->vertices[b]));
+    int aHB   = SIGN(triangle_area(Qb1, Qb, Pa));
+    int bHA   = SIGN(triangle_area(Pa1, Pa, Qb));
 
     // If A and B intersect, update inflag.
-    // FIXME
+    point2_t p;
+    char code = seg_set_int(Pa1, Pa, Qb1, Qb, &p);
+    if ((code == '1') || (code == 'v')) 
+    {
+      if ((inflag == UNKNOWN) && (double_slist_empty(xlist)))
+      {
+        aa = ba = 0;
+        p0.x = p.x;
+        p0.y = p.y;
+        double_slist_append(xlist, p0.x);
+        double_slist_append(ylist, p0.y);
+      }
+      inflag = in_out(&p, inflag, aHB, bHA, xlist, ylist);
+    }
 
     // Advance rules.
-    // FIXME
+    else if (cross >= 0)
+    {
+      if (bHA > 0)
+        a = advance(a, &aa, n, (inflag == PIN), Pa, xlist, ylist);
+      else
+        b = advance(b, &ba, m, (inflag == QIN), Qb, xlist, ylist);
+    }
+    else // if (cross < 0)
+    {
+      if (aHB > 0)
+        b = advance(b, &ba, m, (inflag == QIN), Qb, xlist, ylist);
+      else
+        a = advance(a, &aa, n, (inflag == PIN), Pa, xlist, ylist);
+    }
   }
   while (((aa < n) || (ba < m)) && (aa < 2*n) && (ba < 2*m));
+
+  // Replace this polygon with its clipped version.
+  ASSERT(xlist->size == ylist->size);
+  if (xlist->size > poly->num_vertices)
+    realloc(poly->vertices, sizeof(point2_t)*xlist->size);
+  poly->num_vertices = xlist->size;
+  for (int i = 0; i < xlist->size; ++i)
+  {
+    poly->vertices[i].x = double_slist_pop(xlist);
+    poly->vertices[i].y = double_slist_pop(ylist);
+  }
+  polygon2_compute_area(poly);
+
+  // Clean up.
+  double_slist_free(xlist);
+  double_slist_free(ylist);
 }
 
 #ifdef __cplusplus
