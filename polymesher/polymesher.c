52c7aa7f90d2e22e55f893a2d2bbd95f5b555a67
Jeffrey Johnson <jjphatt@gmail.com>
2013-04-17 13:36:47 -0700

    Added a simple stub for generating meshes.
    Former-commit-id: 7edf949cf8edc14c244f5ff78da65b604f47ad7a

---
+++ polymesher/polymesher.c
@@ -0,0 +1,66 @@
+#include "core/polymec.h"
+#include "core/options.h"
+#include "core/interpreter.h"
+#include "geometry/interpreter_register_geometry_functions.h"
+//#include "polymesher/mesher_functions.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+static void mesher_usage(FILE* stream)
+{
+  fprintf(stream, "polymesher: usage:\n");
+  fprintf(stream, "polymesher [file] [args]\n\n");
+  fprintf(stream, "Here, [file] is a file specifying instructions\n");
+  fprintf(stream, "for generating a mesh.\n\n");
+  exit(-1);
+}
+
+static void interpreter_register_mesher_functions(interpreter_t* interpreter)
+{
+  // FIXME
+}
+
+int main(int argc, char** argv)
+{
+  // Start everything up.
+  polymec_init(argc, argv);
+
+  // Parse options on the command line.
+  options_t* opts = options_parse(argc, argv);
+  if (opts == NULL)
+    mesher_usage(stderr);
+
+  // Extract the input file and arguments. Note that we use "command" here 
+  // to get the input file, since it comes first.
+  char* input = options_command(opts);
+  if (!strcmp(input, "help") || (input == NULL))
+    mesher_usage(stderr);
+
+  // Check to see whether the given file exists.
+  FILE* fp = fopen(input, "r");
+  if (fp == NULL)
+  {
+    fprintf(stderr, "polymesher: Input file not found: %s\n", input);
+    return -1;
+  }
+  fclose(fp);
+
+  // Set up an interpreter for parsing the input file.
+  interpreter_t* interp = interpreter_new(NULL);
+  interpreter_register_geometry_functions(interp);
+  interpreter_register_mesher_functions(interp);
+
+  // Parse it!
+  interpreter_parse_file(interp, input);
+
+  // Clean up.
+  interpreter_free(interp);
+
+  return 0;
+}
+
+#ifdef __cplusplus
+}
+#endif
