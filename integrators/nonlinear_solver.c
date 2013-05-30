4eea413696a374f9f184fd318191d22e990f3b26
Jeffrey Johnson <jjphatt@gmail.com>
2013-05-29 22:53:49 -0700

    Some more work on the nonlinear solver.
    Former-commit-id: e98db8729bd9f6e66ef917c7a5f3c9c3f30b9cce

--- integrators/nonlinear_solver.c
+++ integrators/nonlinear_solver.c
@@ -1,4 +1,6 @@
+#include <float.h>
 #include "integrators/nonlinear_solver.h"
+#include "core/hypre_helpers.h"
 
 struct nonlinear_function_t 
 {
@@ -13,7 +15,22 @@ struct nonlinear_solver_t
   nonlinear_function_t* F;
   adj_graph_t* graph;
   int num_sites;
-  double* XX; // Work array
+  double* XX; // Work array.
+
+  // Linear system and solver.
+  HYPRE_IJMatrix A;
+  HYPRE_IJVector x, b;
+  HYPRE_Solver solver;
+
+  // Flag is set to true if the linear system above is initialized.
+  bool initialized;
+
+  // Index space.
+  index_space_t* index_space;
+
+  // Jacobian and residual coefficients.
+  double_table_t* Jij;
+  double* R;
 };
 
 nonlinear_function_t* nonlinear_function_new(const char* name, 
@@ -80,13 +97,28 @@ nonlinear_solver_t* nonlinear_solver_new(nonlinear_function_t* F,
   solver->num_sites = adj_graph_num_vertices(graph);
   int N = nonlinear_function_num_comps(F);
   solver->XX = malloc(sizeof(double) * solver->num_sites * N);
+  solver->index_space = index_space_new_from_low_and_high(adj_graph_comm(graph),
+                                                          adj_graph_first_vertex(graph),
+                                                          adj_graph_last_vertex(graph));
+  solver->initialized = false;
   return solver;
 }
 
 
 void nonlinear_solver_free(nonlinear_solver_t* solver)
 {
+  if (solver->initialized)
+  {
+    HYPRE_ParCSRHybridDestroy(solver->solver);
+    HYPRE_IJMatrixDestroy(solver->A);
+    HYPRE_IJVectorDestroy(solver->x);
+    HYPRE_IJVectorDestroy(solver->b);
+    double_table_free(solver->Jij);
+    free(solver->R);
+  }
+
   nonlinear_function_free(solver->F);
+  solver->index_space = NULL;
   adj_graph_free(solver->graph);
   free(solver->XX);
   free(solver);
@@ -177,5 +209,92 @@ void nonlinear_solver_compute_jacobian(nonlinear_solver_t* solver,
       }
     }
   }
+}
+
+static void initialize(nonlinear_solver_t* solver)
+{
+  if (!solver->initialized)
+  {
+    solver->A = HYPRE_IJMatrixNew(solver->index_space);
+    solver->x = HYPRE_IJVectorNew(solver->index_space);
+    solver->b = HYPRE_IJVectorNew(solver->index_space);
+    HYPRE_ParCSRHybridCreate(&solver->solver);
+    HYPRE_ParCSRHybridSetSolverType(solver->solver, 2);
+    HYPRE_ParCSRHybridSetKDim(solver->solver, 5);
+    solver->Jij = double_table_new();
+    int N = nonlinear_function_num_comps(solver->F);
+    solver->R = malloc(sizeof(double) * N * adj_graph_num_vertices(solver->graph));
+    solver->initialized = true;
+  }
+}
+
+static inline void solve(nonlinear_solver_t* solver, HYPRE_IJMatrix A, HYPRE_IJVector b, HYPRE_IJVector x)
+{
+//HYPRE_IJMatrixPrint(solver->A, "A");
+//HYPRE_IJVectorPrint(solver->b, "b");
+  HYPRE_ParCSRMatrix Aobj;
+  int err = HYPRE_IJMatrixGetObject(solver->A, (void**)&Aobj);
+  ASSERT(err == 0);
+  ASSERT(Aobj != NULL);
+  HYPRE_ParVector xobj, bobj;
+  err = HYPRE_IJVectorGetObject(solver->x, (void**)&xobj);
+  ASSERT(err == 0);
+  ASSERT(xobj != NULL);
+  err = HYPRE_IJVectorGetObject(solver->b, (void**)&bobj);
+  ASSERT(err == 0);
+  ASSERT(bobj != NULL);
+
+  err = HYPRE_ParCSRHybridSetup(solver->solver, Aobj, bobj, xobj);
+  ASSERT(err == 0);
+  err = HYPRE_ParCSRHybridSolve(solver->solver, Aobj, bobj, xobj);
+  if (err == HYPRE_ERROR_CONV)
+    polymec_error("nonlinear_solver: Solve did not converge.");
+
+  HYPRE_ClearAllErrors();
+}
+
+void nonlinear_solver_step(nonlinear_solver_t* solver,
+                           double* t, double* x)
+{
+  // Make sure the solver is initialized.
+  initialize(solver);
+
+  static double epsilon = FLT_EPSILON * FLT_MIN; // Smallest possible float.
+
+  // Compute the components of the Jacobian and the residual at each site.
+  int N = solver->index_space->high - solver->index_space->low;
+  int num_comps = nonlinear_function_num_comps(solver->F);
+  for (int i = 0; i < N; ++i)
+  {
+    nonlinear_solver_compute_jacobian(solver, i, x, epsilon, solver->Jij);
+    nonlinear_solver_compute_residual(solver, i, x, solver->R);
+  }
+
+  // Set up the linear system.
+  HYPRE_IJMatrixSetValuesFromTable(solver->A, solver->index_space, solver->Jij);
+//HYPRE_IJMatrixPrint(solver->A, "L");
+  HYPRE_IJVectorSetValuesFromArray(solver->b, solver->index_space, solver->R);
+
+  // Now solve the linear system for the increment.
+  solve(solver, solver->A, solver->b, solver->x);
+
+  // Copy the solution to XX.
+  HYPRE_IJVectorGetValuesToArray(solver->x, solver->index_space, solver->XX);
+
+  // Add the increment to x.
+  for (int i = 0; i < N*num_comps; ++i)
+    x[i] += solver->XX[i];
+}
+
+void nonlinear_solver_integrate(nonlinear_solver_t* solver,
+                                double t1, double* x1,
+                                double t2, double* x2)
+{
+  double t = t1;
+  int N = solver->index_space->high - solver->index_space->low;
+  int num_comps = nonlinear_function_num_comps(solver->F);
+  memcpy(x2, x1, sizeof(double) * N * num_comps);
+  while (t < t2)
+    nonlinear_solver_step(solver, &t, x2);
 }
 
