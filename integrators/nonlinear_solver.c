d8940fd59f101ed5b0a65b63e1c2ad7ef289d43e
Jeffrey Johnson <jjphatt@gmail.com>
2013-05-30 10:31:11 -0700

    Added a nonlinear timestepper class.
    Former-commit-id: 431c253cddba43bc02af35a4fdd2203344b571fc

--- integrators/nonlinear_solver.c
+++ integrators/nonlinear_solver.c
@@ -10,9 +10,24 @@ struct nonlinear_function_t
   nonlinear_function_vtable vtable;
 };
 
+struct nonlinear_timestepper_t
+{
+  void* context;
+  char* name;
+  int max_history_length, history_length;
+  double* dt_history;
+  double* error_history;
+  int* iteration_history;
+  double last_unsuccessful_dt;
+  int num_failures;
+  char* explanation;
+  nonlinear_timestepper_vtable vtable;
+};
+
 struct nonlinear_solver_t 
 {
   nonlinear_function_t* F;
+  nonlinear_timestepper_t* timestepper;
   adj_graph_t* graph;
   int num_sites;
   double* XX; // Work array.
@@ -43,6 +58,7 @@ nonlinear_function_t* nonlinear_function_new(const char* name,
 
   nonlinear_function_t* F = malloc(sizeof(nonlinear_function_t));
   F->name = strdup(name);
+  F->context = context;
   F->num_comps = num_comps;
   F->vtable = vtable;
 
@@ -62,13 +78,11 @@ char* nonlinear_function_name(nonlinear_function_t* F)
   return F->name;
 }
 
-// Returns the context object for this nonlinear function.
 void* nonlinear_function_context(nonlinear_function_t* F)
 {
   return F->context;
 }
 
-// Returns the number of components per site in this nonlinear function.
 int nonlinear_function_num_comps(nonlinear_function_t* F)
 {
   return F->num_comps;
@@ -76,23 +90,145 @@ int nonlinear_function_num_comps(nonlinear_function_t* F)
 
 void nonlinear_function_eval(nonlinear_function_t* F,
                              int site,
+                             double t,
+                             double dt,
                              double* X,
                              double* R)
 {
   ASSERT(site >= 0);
+  ASSERT(dt > 0.0);
   ASSERT(X != NULL);
   ASSERT(R != NULL);
-  F->vtable.eval_residual(F->context, F->num_comps, site, X, R);
+  F->vtable.eval_residual(F->context, F->num_comps, site, t, dt, X, R);
+}
+
+nonlinear_timestepper_t* nonlinear_timestepper_new(const char* name, 
+                                                   void* context,
+                                                   int max_history_length,
+                                                   nonlinear_timestepper_vtable vtable)
+{
+  ASSERT(history_length >= 1);
+  ASSERT(vtable->compute_dt != NULL);
+  ASSERT(vtable->recompute_J != NULL);
+
+  nonlinear_timestepper_t* timestepper = malloc(sizeof(nonlinear_timestepper_t));
+  timestepper->name = strdup(name);
+  timestepper->context = context;
+  timestepper->history_length = 0;
+  timestepper->max_history_length = max_history_length;
+  timestepper->dt_history = malloc(sizeof(double) * max_history_length);
+  timestepper->error_history = malloc(sizeof(double) * max_history_length);
+  timestepper->iteration_history = malloc(sizeof(int) * max_history_length);
+  timestepper->last_unsuccessful_dt = FLT_MAX;
+  timestepper->num_failures = 0;
+  timestepper->vtable = vtable;
+  timestepper->explanation = malloc(sizeof(char) * 2048);
+  
+  return timestepper;
+}
+
+void nonlinear_timestepper_free(nonlinear_timestepper_t* timestepper)
+{
+  if ((timestepper->context != NULL) && (timestepper->vtable.dtor != NULL))
+    timestepper->vtable.dtor(timestepper->context);
+  free(timestepper->dt_history);
+  free(timestepper->error_history);
+  free(timestepper->iteration_history);
+  free(timestepper->explanation);
+  free(timestepper->name);
+}
+
+char* nonlinear_timestepper_name(nonlinear_timestepper_t* timestepper)
+{
+  return timestepper->name;
+}
+
+int nonlinear_timestepper_max_history_length(nonlinear_timestepper_t* timestepper)
+{
+  return timestepper->max_history_length;
+}
+
+void nonlinear_timestepper_clear_history(nonlinear_timestepper_t* timestepper)
+{
+  timestepper->history_length = 0;
+}
+
+double nonlinear_timestepper_step_size(nonlinear_timestepper_t* timestepper,
+                                       char** explanation)
+{
+  ASSERT(explanation != NULL);
+  double dt = timestepper->vtable.compute_dt(timestepper->context, 
+                                             timestepper->last_unsuccessful_dt,
+                                             timestepper->num_failures,
+                                             timestepper->dt_history + timestepper->history_length,
+                                             timestepper->error_history + timestepper->history_length,
+                                             timestepper->iteration_history + timestepper->history_length,
+                                             timestepper->history_length,
+                                             timestepper->explanation);
+  explanation = &timestepper->explanation;
+  return dt;
+}
+
+bool nonlinear_timestepper_recompute_jacobian(nonlinear_timestepper_t* timestepper)
+{
+  return timestepper->vtable.recompute_J(timestepper->context, 
+                                         timestepper->dt_history + timestepper->history_length,
+                                         timestepper->error_history + timestepper->history_length,
+                                         timestepper->iteration_history + timestepper->history_length,
+                                         timestepper->history_length);
+}
+
+void nonlinear_timestepper_converged(nonlinear_timestepper_t* timestepper,
+                                     double step_size,
+                                     double error_norm,
+                                     int num_iterations)
+{
+  ASSERT(step_size > 0.0);
+  ASSERT(error_norm >= 0.0);
+  ASSERT(num_iterations > 0);
+
+  if (timestepper->history_length == (timestepper->max_history_length - 1))
+  {
+    // We shift the history downward.
+    for (int i = 1; i < timestepper->max_history_length; ++i)
+    {
+      timestepper->dt_history[i-1] = timestepper->dt_history[i];
+      timestepper->error_history[i-1] = timestepper->error_history[i];
+      timestepper->iteration_history[i-1] = timestepper->iteration_history[i];
+    }
+  }
+
+  // Append the latest information to the history.
+  timestepper->dt_history[timestepper->history_length] = step_size;
+  timestepper->error_history[timestepper->history_length] = error_norm;
+  timestepper->iteration_history[timestepper->history_length] = num_iterations;
+
+  if (timestepper->history_length < (timestepper->max_history_length - 1))
+    ++timestepper->history_length;
+
+  // Reset the failure counter.
+  timestepper->num_failures = 0;
+}
+
+void nonlinear_timestepper_failed(nonlinear_timestepper_t* timestepper,
+                                  double step_size)
+{
+  // Record the time step size and increment the failure counter.
+  timestepper->last_unsuccessful_dt = step_size;
+  ++timestepper->num_failures;
 }
 
 nonlinear_solver_t* nonlinear_solver_new(nonlinear_function_t* F,
+                                         nonlinear_timestepper_t* timestepper,
                                          adj_graph_t* graph)
 {
   ASSERT(F != NULL);
-  ASSERT(num_sites > 0);
+  ASSERT(timestepper != NULL);
+  ASSERT(graph != NULL);
 
   nonlinear_solver_t* solver = malloc(sizeof(nonlinear_solver_t));
   solver->F = F;
+  solver->timestepper = timestepper;
   solver->graph = graph;
   solver->num_sites = adj_graph_num_vertices(graph);
   int N = nonlinear_function_num_comps(F);
@@ -117,6 +253,7 @@ void nonlinear_solver_free(nonlinear_solver_t* solver)
     free(solver->R);
   }
 
+  nonlinear_timestepper_free(solver->timestepper);
   nonlinear_function_free(solver->F);
   solver->index_space = NULL;
   adj_graph_free(solver->graph);
@@ -131,24 +268,30 @@ nonlinear_function_t* nonlinear_solver_function(nonlinear_solver_t* solver)
 
 void nonlinear_solver_compute_residual(nonlinear_solver_t* solver,
                                        int site,
+                                       double t, 
+                                       double dt,
                                        double* X,
                                        double* R)
 {
   ASSERT(site >= 0);
   ASSERT(site < solver->num_sites);
+  ASSERT(dt > 0.0);
   ASSERT(X != NULL);
   ASSERT(R != NULL);
-  nonlinear_function_eval(solver->F, site, X, R);
+  nonlinear_function_eval(solver->F, site, t, dt, X, R);
 }
 
 void nonlinear_solver_compute_jacobian(nonlinear_solver_t* solver,
                                        int site,
+                                       double t,
+                                       double dt,
                                        double* X,
                                        double delta,
                                        double_table_t* Jij)
 {
   ASSERT(site >= 0);
   ASSERT(site < solver->num_sites);
+  ASSERT(dt > 0.0);
   ASSERT(X != NULL);
   ASSERT(delta > 0.0);
   ASSERT(Jij != NULL);
@@ -156,7 +299,7 @@ void nonlinear_solver_compute_jacobian(nonlinear_solver_t* solver,
   // Compute the residual in the reference state.
   int N = nonlinear_function_num_comps(solver->F);
   double R0[N];
-  nonlinear_function_eval(solver->F, site, X, R0);
+  nonlinear_function_eval(solver->F, site, t, dt, X, R0);
 
   // Increment the various solution coefficients to compute the components 
   // of the Jacobian. We only need to increment those coefficients that 
@@ -187,7 +330,7 @@ void nonlinear_solver_compute_jacobian(nonlinear_solver_t* solver,
 
     // Compute the residual for the incremented solution vector.
     double R[N];
-    nonlinear_function_eval(solver->F, site, solver->XX, R);
+    nonlinear_function_eval(solver->F, site, t, dt, solver->XX, R);
 
     // Stash the resulting partial derivatives in Jij.
     for (int j = 0; j < N; ++j)
@@ -261,14 +404,23 @@ void nonlinear_solver_step(nonlinear_solver_t* solver,
 
   static double epsilon = FLT_EPSILON * FLT_MIN; // Smallest possible float.
 
-  // Compute the components of the Jacobian and the residual at each site.
+  // Choose a new timestep.
+  char* explanation;
+  double dt = nonlinear_timestepper_step_size(solver->timestepper, &explanation);
+  log_detail("nonlinear_solver_step: Chose dt = %g (%s)", dt, explanation);
+
+  // Compute the components of the Jacobian and the residual at each site 
+  // as needed.
   int N = solver->index_space->high - solver->index_space->low;
   int num_comps = nonlinear_function_num_comps(solver->F);
-  for (int i = 0; i < N; ++i)
+  if (nonlinear_timestepper_recompute_jacobian(solver->timestepper))
   {
-    nonlinear_solver_compute_jacobian(solver, i, x, epsilon, solver->Jij);
-    nonlinear_solver_compute_residual(solver, i, x, solver->R);
+    log_detail("nonlinear_solver_step: Recomputing Jacobian.");
+    for (int i = 0; i < N; ++i)
+      nonlinear_solver_compute_jacobian(solver, i, *t, dt, x, epsilon, solver->Jij);
   }
+  for (int i = 0; i < N; ++i)
+    nonlinear_solver_compute_residual(solver, i, *t, dt, x, solver->R);
 
   // Set up the linear system.
   HYPRE_IJMatrixSetValuesFromTable(solver->A, solver->index_space, solver->Jij);
@@ -277,6 +429,7 @@ void nonlinear_solver_step(nonlinear_solver_t* solver,
 
   // Now solve the linear system for the increment.
   solve(solver, solver->A, solver->b, solver->x);
+  // FIXME: Measure of success?
 
   // Copy the solution to XX.
   HYPRE_IJVectorGetValuesToArray(solver->x, solver->index_space, solver->XX);
@@ -284,6 +437,9 @@ void nonlinear_solver_step(nonlinear_solver_t* solver,
   // Add the increment to x.
   for (int i = 0; i < N*num_comps; ++i)
     x[i] += solver->XX[i];
+
+  // Update the time.
+  *t += dt;
 }
 
 void nonlinear_solver_integrate(nonlinear_solver_t* solver,
