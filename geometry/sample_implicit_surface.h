7fdf99335ccc95c92df9ff3e992893a5edc608f9
Jeffrey Johnson <jjphatt@gmail.com>
2013-06-20 11:12:18 -0700

    A bit more work on the sampling algorithm.
    Former-commit-id: 4f8884ba826ef79993575aa68ded81ee7b183b4a

--- geometry/sample_implicit_surface.h
+++ geometry/sample_implicit_surface.h
@@ -7,12 +7,14 @@
 // this function computes and returns a set of points that sample the 
 // given (closed) implicit surface. The surface diameter is an estimate of 
 // the characteristic size of the surface, which is used to estimate a stopping 
-// condition for the sampling. The given initial point is used to seed
-// the sampling process. NOTE: if surface_density is NULL, a uniform density 
-// is assumed.
+// condition for the sampling. The algorithm terminates when it reaches the 
+// desired surface density or the maximum number of sampling points. 
+// The given initial point is used to seed the sampling process. 
+// NOTE: if surface_density is NULL, a uniform density is assumed.
 point_t* sample_implicit_surface(sp_func_t* surface, 
                                  sp_func_t* surface_density,
                                  double surface_diameter,
+                                 int max_num_sample_points,
                                  point_t* initial_point,
                                  int* num_sample_points);
 
