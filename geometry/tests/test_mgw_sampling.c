b4d35a38db41bf841c3d3a942057f0fa6139edaf
Jeffrey Johnson <jjphatt@gmail.com>
2013-06-27 10:10:59 -0700

    Some progress on M-G-W sampling.
    Former-commit-id: 8b4ccdd13d5e5b243b16e1c380ad4a62a6d192db

---
+++ geometry/tests/test_mgw_sampling.c
@@ -0,0 +1,85 @@
+#include <stdarg.h>
+#include <stddef.h>
+#include <setjmp.h>
+#include <string.h>
+#include "cmockery.h"
+#include "geometry/mgw_sampling.h"
+#include "geometry/rect_prism.h"
+#include "geometry/cylinder.h"
+#include "geometry/sphere.h"
+
+static void plot_points(point_t* points, 
+                        int num_points, 
+                        const char* filename)
+{
+  int rank;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  if (rank != 0) return;
+
+  FILE* fd = fopen(filename, "w");
+  fprintf(fd, "# x y z\n");
+  for (int i = 0; i < num_points; ++i)
+    fprintf(fd, "%g %g %g\n", points[i].x, points[i].y, points[i].z);
+  fclose(fd);
+}
+
+void test_sample_bbox_uniformly(void** state)
+{
+  unsigned int seed = 1;
+  srandom(seed);
+
+  point_t x0 = {.x = 0.0, .y = 0.0, .z = 0.0};
+  bbox_t bbox = {.x1 = -0.5, .x2 = 0.5, 
+                 .y1 = -0.5, .y2 = 0.5,
+                 .z1 = -0.5, .z2 = 0.5};
+  sp_func_t* box = rect_prism_new_from_bbox(&bbox);
+  int N = 1000;
+  point_t* points = mgw_sampling(box, &bbox, N);
+  plot_points(points, N, "uniformly_sampled_box.gnuplot");
+  free(points);
+}
+
+void test_sample_cylinder_uniformly(void** state)
+{
+  unsigned int seed = 1;
+  srandom(seed);
+
+  point_t x0 = {.x = 0.0, .y = 0.0, .z = 0.0};
+  sp_func_t* cylinder = cylinder_new(&x0, 0.5, INWARD_NORMAL);
+  bbox_t bbox = {.x1 = -0.5, .x2 = 0.5, 
+                 .y1 = -0.5, .y2 = 0.5, 
+                 .z1 = -0.5, .z2 = 0.5};
+  int N = 1000;
+  point_t* points = mgw_sampling(cylinder, &bbox, N);
+  plot_points(points, N, "uniformly_sampled_cylinder.gnuplot");
+  free(points);
+}
+
+void test_sample_sphere_uniformly(void** state)
+{
+  unsigned int seed = 1;
+  srandom(seed);
+
+  point_t x0 = {.x = 0.0, .y = 0.0, .z = 0.0};
+  sp_func_t* sphere = sphere_new(&x0, 0.5, INWARD_NORMAL);
+  bbox_t bbox = {.x1 = -0.5, .x2 = 0.5, 
+                 .y1 = -0.5, .y2 = 0.5, 
+                 .z1 = -0.5, .z2 = 0.5};
+  int N = 1000;
+  point_t* points = mgw_sampling(sphere, &bbox, N);
+  plot_points(points, N, "uniformly_sampled_sphere.gnuplot");
+  free(points);
+}
+
+int main(int argc, char* argv[]) 
+{
+  polymec_init(argc, argv);
+  set_log_level(LOG_DEBUG);
+  const UnitTest tests[] = 
+  {
+    unit_test(test_sample_bbox_uniformly),
+    unit_test(test_sample_cylinder_uniformly),
+    unit_test(test_sample_sphere_uniformly)
+  };
+  return run_tests(tests);
+}
