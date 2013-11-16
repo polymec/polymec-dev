95e6b2feea5e91a2f8e6a953d7d6ddc813d825b1
Jeffrey Johnson <jjphatt@gmail.com>
2013-11-16 15:51:04 -0800

    First real stab at inserting Jv values into Jacobian.

--- integrators/supermatrix_factory.c
+++ integrators/supermatrix_factory.c
@@ -50,7 +50,7 @@ supermatrix_factory_t* supermatrix_factory_from_sys_func(adj_graph_t* graph,
   N_Vector prototype = N_VNew(comm, num_vertices);
 //  int num_colors = adj_graph_coloring_num_colors(factory->coloring);
 //  factory->work = N_VCloneVectorArray(num_colors, prototype);
-  factory->work = N_VCloneVectorArray(2, prototype);
+  factory->work = N_VCloneVectorArray(3, prototype);
   N_VDestroy(prototype);
   return factory;
 }
@@ -73,7 +73,7 @@ supermatrix_factory_t* supermatrix_factory_from_rhs(adj_graph_t* graph,
   N_Vector prototype = N_VNew(comm, num_vertices);
 //  int num_colors = adj_graph_coloring_num_colors(factory->coloring);
 //  factory->work = N_VCloneVectorArray(num_colors, prototype);
-  factory->work = N_VCloneVectorArray(2, prototype);
+  factory->work = N_VCloneVectorArray(3, prototype);
   N_VDestroy(prototype);
   return factory;
 }
@@ -82,7 +82,7 @@ void supermatrix_factory_free(supermatrix_factory_t* factory)
 {
 //  int num_colors = adj_graph_coloring_num_colors(factory->coloring);
 //  N_VDestroyVectorArray(factory->work, num_colors);
-  N_VDestroyVectorArray(factory->work, 2);
+  N_VDestroyVectorArray(factory->work, 3);
   adj_graph_coloring_free(factory->coloring);
   free(factory);
 }
@@ -141,16 +141,64 @@ static void finite_diff_F_Jv(KINSysFn F, void* context, N_Vector u, N_Vector v, 
   // F(u) -> Jv.
   F(u, Jv, context); 
 
-  // u + eps*v -> work[0].
+  // u + eps*v -> work[1].
   for (int i = 0; i < NV_LOCLENGTH(u); ++i)
-    NV_Ith(work[0], i) = NV_Ith(u, i) + eps*NV_Ith(v, i);
+    NV_Ith(work[1], i) = NV_Ith(u, i) + eps*NV_Ith(v, i);
 
-  // F(u + eps*v) -> work[1].
-  F(work[0], work[1], context);
+  // F(u + eps*v) -> work[2].
+  F(work[1], work[2], context);
 
   // (F(u + eps*v) - F(u)) / eps -> Jv
   for (int i = 0; i < NV_LOCLENGTH(u); ++i)
-    NV_Ith(Jv, i) = (NV_Ith(work[1], i) - NV_Ith(Jv, i)) / eps;
+    NV_Ith(Jv, i) = (NV_Ith(work[2], i) - NV_Ith(Jv, i)) / eps;
+}
+
+// Column index comparison function for bsearch within supermatrix NRformat.
+static int ind_comp(const void* l, const void* r)
+{
+  int li = *((int*)l), ri = *((int*)r);
+  if (li < ri)
+    return -1;
+  else if (li > ri)
+    return 1;
+  else
+    return 0;
+}
+
+static void insert_Jv_into_matrix(adj_graph_t* graph, 
+                                  adj_graph_coloring_t* coloring, 
+                                  int color, 
+                                  N_Vector Jv, 
+                                  SuperMatrix* J)
+{
+  int N = NV_LOCLENGTH(Jv);
+  NRformat* Jdata = J->Store;
+  double* Jij = Jdata->nzval;
+  for (int i = 0; i < N; ++i)
+  {
+    if (NV_Ith(Jv, i) != 0.0)
+    {
+      int pos = 0, j;
+      while (adj_graph_next_edge(graph, i, &pos, &j))
+      {
+        if (i == j)
+        {
+          // Diagonal value.
+          Jij[Jdata->rowptr[i]] = NV_Ith(Jv, i);
+        }
+        else if (adj_graph_coloring_has_vertex(coloring, color, j))
+        {
+          // Off-diagonal value.
+          int row_index = Jdata->rowptr[i];
+          size_t num_cols = Jdata->colind[Jdata->rowptr[i+1]] - Jdata->colind[row_index];
+          int* entry = (int*)bsearch(&j, &Jdata->colind[row_index+1], 
+              num_cols - 1, sizeof(int), ind_comp);
+          ASSERT(entry != NULL);
+          Jij[*entry] = NV_Ith(Jv, i);
+        }
+      }
+    }
+  }
 }
 
 static void compute_F_jacobian(KINSysFn F, 
@@ -162,8 +210,26 @@ static void compute_F_jacobian(KINSysFn F,
                                SuperMatrix* J)
 {
   // We compute the system Jacobian using the method described in 
-  // Gebremedhin, et al (2005).
+  // Curtis, Powell, and Reed.
+  int N = NV_LOCLENGTH(u);
+  N_Vector Jv = N_VClone(u);
+  int num_colors = adj_graph_coloring_num_colors(coloring);
+  for (int c = 0; c < num_colors; ++c)
+  {
+    // We construct d, the binary vector corresponding to this color, in work[0].
+    memset(work[0], 0, sizeof(double) * N);
+    int pos = 0, i;
+    while (adj_graph_coloring_next_vertex(coloring, c, &pos, &i))
+      NV_Ith(work[0], i) = 1.0;
 
+    // Now evaluate the matrix-vector product.
+    memset(NV_DATA(Jv), 0, sizeof(double) * N);
+    finite_diff_F_Jv(F, context, u, work[0], work, Jv);
+
+    // Copy the components of Jv into their proper locations.
+    insert_Jv_into_matrix(graph, coloring, c, Jv, J);
+  }
+  N_VDestroy(Jv);
 }
 
 // Here's our finite difference implementation of the RHS Jacobian 
@@ -175,16 +241,16 @@ static void finite_diff_rhs_Jv(CVRhsFn rhs, void* context, N_Vector u, double t,
   // rhs(u, t) -> Jv.
   rhs(t, u, Jv, context); 
 
-  // u + eps*v -> work[0].
+  // u + eps*v -> work[1].
   for (int i = 0; i < NV_LOCLENGTH(u); ++i)
-    NV_Ith(work[0], i) = NV_Ith(u, i) + eps*NV_Ith(v, i);
+    NV_Ith(work[1], i) = NV_Ith(u, i) + eps*NV_Ith(v, i);
 
-  // F(u + eps*v, t) -> work[1].
-  rhs(t, work[0], work[1], context);
+  // F(u + eps*v, t) -> work[2].
+  rhs(t, work[1], work[2], context);
 
   // (F(u + eps*v) - F(u)) / eps -> Jv
   for (int i = 0; i < NV_LOCLENGTH(u); ++i)
-    NV_Ith(Jv, i) = (NV_Ith(work[1], i) - NV_Ith(Jv, i)) / eps;
+    NV_Ith(Jv, i) = (NV_Ith(work[2], i) - NV_Ith(Jv, i)) / eps;
 }
 
 static void compute_rhs_jacobian(CVRhsFn rhs, 
@@ -196,6 +262,27 @@ static void compute_rhs_jacobian(CVRhsFn rhs,
                                  N_Vector* work,
                                  SuperMatrix* J)
 {
+  // We compute the system Jacobian using the method described in 
+  // Curtis, Powell, and Reed.
+  int N = NV_LOCLENGTH(u);
+  N_Vector Jv = N_VClone(u);
+  int num_colors = adj_graph_coloring_num_colors(coloring);
+  for (int c = 0; c < num_colors; ++c)
+  {
+    // We construct d, the binary vector corresponding to this color, in work[0].
+    memset(work[0], 0, sizeof(double) * N);
+    int pos = 0, i;
+    while (adj_graph_coloring_next_vertex(coloring, c, &pos, &i))
+      NV_Ith(work[0], i) = 1.0;
+
+    // Now evaluate the matrix-vector product.
+    memset(NV_DATA(Jv), 0, sizeof(double) * N);
+    finite_diff_rhs_Jv(rhs, context, u, t, work[0], work, Jv);
+
+    // Copy the components of Jv into their proper locations.
+    insert_Jv_into_matrix(graph, coloring, c, Jv, J);
+  }
+  N_VDestroy(Jv);
 }
 
 void supermatrix_factory_update_jacobian(supermatrix_factory_t* factory, N_Vector u, double t, SuperMatrix* J)
