d9463c0a6d97527cc0e49f71fe2f7873ffa230db
Jeffrey Johnson <jjphatt@gmail.com>
2013-11-20 22:35:38 -0800

    Changed integrator -> time_integrator.

--- integrators/time_integrator.c
+++ integrators/time_integrator.c
@@ -14,33 +14,33 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "integrators/integrator.h"
+#include "integrators/time_integrator.h"
 #include "core/sundials_helpers.h"
 #include "cvode/cvode_spils.h"
 #include "cvode/cvode_spgmr.h"
 #include "cvode/cvode_spbcgs.h"
 
-struct integrator_t 
+struct time_integrator_t 
 {
   char* name;
   void* context;
   void (*dtor)(void*);
   adj_graph_t* graph;
   int order;
-  integrator_solver_type_t solver_type;
+  time_integrator_solver_type_t solver_type;
 };
 
-integrator_t* integrator_new(const char* name, 
-                             void* context,
-                             CVRhsFn rhs,
-                             void (*dtor)(void*),
-                             adj_graph_t* graph,
-                             int order,
-                             integrator_solver_type_t solver_type)
+time_integrator_t* time_integrator_new(const char* name, 
+                                       void* context,
+                                       CVRhsFn rhs,
+                                       void (*dtor)(void*),
+                                       adj_graph_t* graph,
+                                       int order,
+                                       time_integrator_solver_type_t solver_type)
 {
   ASSERT(rhs != NULL);
   ASSERT(order > 0);
-  integrator_t* integ = malloc(sizeof(integrator_t));
+  time_integrator_t* integ = malloc(sizeof(time_integrator_t));
   integ->name = string_dup(name);
   integ->context = context;
   integ->graph = graph;
@@ -49,7 +49,7 @@ integrator_t* integrator_new(const char* name,
   return integ;
 }
 
-void integrator_free(integrator_t* integrator)
+void time_integrator_free(time_integrator_t* integrator)
 {
   if ((integrator->context != NULL) && (integrator->dtor != NULL))
     integrator->dtor(integrator->context);
@@ -57,22 +57,22 @@ void integrator_free(integrator_t* integrator)
   free(integrator);
 }
 
-char* integrator_name(integrator_t* integrator)
+char* time_integrator_name(time_integrator_t* integrator)
 {
   return integrator->name;
 }
 
-void* integrator_context(integrator_t* integrator)
+void* time_integrator_context(time_integrator_t* integrator)
 {
   return integrator->context;
 }
 
-int integrator_order(integrator_t* integrator)
+int time_integrator_order(time_integrator_t* integrator)
 {
   return integrator->order;
 }
 
-void integrator_step(integrator_t* integrator, double t1, double t2, double* X)
+void time_integrator_step(time_integrator_t* integrator, double t1, double t2, double* X)
 {
   ASSERT(t2 > t1);
   // FIXME
--- integrators/time_integrator.h
+++ integrators/time_integrator.h
@@ -14,8 +14,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#ifndef POLYMEC_INTEGRATOR_H
-#define POLYMEC_INTEGRATOR_H
+#ifndef POLYMEC_TIME_INTEGRATOR_H
+#define POLYMEC_TIME_INTEGRATOR_H
 
 #include "core/polymec.h"
 #include "core/adj_graph.h"
@@ -26,38 +26,38 @@ typedef enum
 {
   GMRES,
   BICGSTAB
-} integrator_solver_type_t;
+} time_integrator_solver_type_t;
 
 // This class provides an abstract interface for integrating systems of 
 // nonlinear differential equations. 
-typedef struct integrator_t integrator_t;
+typedef struct time_integrator_t time_integrator_t;
 
-// Creates an integrator with the given name, context, and virtual table.
+// Creates a time integrator with the given name, context, and virtual table.
 // Also given are some metadata, such as the order of the method, whether 
 // it is explicit, implicit, or hybrid. The integrator will integrate 
 // a system of N differential equations.
-integrator_t* integrator_new(const char* name, 
-                             void* context,
-                             CVRhsFn rhs,
-                             void (*dtor)(void*),
-                             adj_graph_t* graph,
-                             int order,
-                             integrator_solver_type_t solver_type);
+time_integrator_t* time_integrator_new(const char* name, 
+                                       void* context,
+                                       CVRhsFn rhs,
+                                       void (*dtor)(void*),
+                                       adj_graph_t* graph,
+                                       int order,
+                                       time_integrator_solver_type_t solver_type);
 
-// Frees an integrator.
-void integrator_free(integrator_t* integrator);
+// Frees a time integrator.
+void time_integrator_free(time_integrator_t* integrator);
 
 // Returns the name of the integrator (internally stored).
-char* integrator_name(integrator_t* integrator);
+char* time_integrator_name(time_integrator_t* integrator);
 
 // Returns the context object for this integrator.
-void* integrator_context(integrator_t* integrator);
+void* time_integrator_context(time_integrator_t* integrator);
 
 // Returns the order of the integration method.
-int integrator_order(integrator_t* integrator);
+int time_integrator_order(time_integrator_t* integrator);
 
 // Integrates the given solution X in place from time t1 to t2.
-void integrator_step(integrator_t* integrator, double t1, double t2, double* X);
+void time_integrator_step(time_integrator_t* integrator, double t1, double t2, double* X);
 
 #endif
 
