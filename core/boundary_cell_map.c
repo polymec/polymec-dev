b3b5da9afdb07fd4990c4f9e601d940dfba497e2
Jeffrey Johnson <jjphatt@gmail.com>
2013-12-13 10:50:01 -0800

    Switched license from Apache -> BSD.

--- core/boundary_cell_map.c
+++ core/boundary_cell_map.c
@@ -1,18 +1,27 @@
-// Copyright 2012-2013 Jeffrey Johnson.
+// Copyright (c) 2012-2013, Jeffrey N. Johnson
+// All rights reserved.
 // 
-// This file is part of Polymec, and is licensed under the Apache License, 
-// Version 2.0 (the "License"); you may not use this file except in 
-// compliance with the License. You may may find the text of the license in 
-// the LICENSE file at the top-level source directory, or obtain a copy of 
-// it at
+// Redistribution and use in source and binary forms, with or without 
+// modification, are permitted provided that the following conditions are met:
 // 
-// http://www.apache.org/licenses/LICENSE-2.0
+// 1. Redistributions of source code must retain the above copyright notice, this 
+// list of conditions and the following disclaimer.
 // 
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// 2. Redistributions in binary form must reproduce the above copyright notice, 
+// this list of conditions and the following disclaimer in the documentation 
+// and/or other materials provided with the distribution.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE 
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE 
+// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL 
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR 
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER 
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, 
+// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 
 #include "core/boundary_cell_map.h"
 
