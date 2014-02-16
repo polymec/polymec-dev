bf5e9d8fe1ccce2ab4824bd011340b10b9db4a02
Jeffrey Johnson <jjphatt@gmail.com>
2014-02-16 09:33:50 -0800

    Adding DAE integrator.

---
+++ integrators/dae_integrator.c
@@ -0,0 +1,427 @@
+// Copyright (c) 2012-2014, Jeffrey N. Johnson
+// All rights reserved.
+// 
+// Redistribution and use in source and binary forms, with or without 
+// modification, are permitted provided that the following conditions are met:
+// 
+// 1. Redistributions of source cdae must retain the above copyright notice, this 
+// list of conditions and the following disclaimer.
+// 
+// 2. Redistributions in binary form must reproduce the above copyright notice, 
+// this list of conditions and the following disclaimer in the documentation 
+// and/or other materials provided with the distribution.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE 
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE 
+// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL 
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR 
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER 
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, 
+// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+#include "core/sundials_helpers.h"
+#include "integrators/dae_integrator.h"
+
+#include "ida/ida.h"
+#include "ida/ida_spils.h"
+#include "ida/ida_spgmr.h"
+#include "ida/ida_spbcgs.h"
+#include "ida/ida_sptfqmr.h"
+
+// Types of linear solvers.
+typedef enum 
+{
+  GMRES,
+  BICGSTAB,
+  TFQMR
+} solver_type_t;
+
+struct dae_integrator_t 
+{
+  char* name;
+  void* context;
+  dae_integrator_vtable vtable;
+  int order;
+  MPI_Comm comm;
+  solver_type_t solver_type;
+  bool initialized;
+
+  int N; // dimension of system.
+
+  // IDA data structures.
+  void* ida;
+  N_Vector x, x_dot;
+  real_t current_time;
+  int max_krylov_dim;
+  char* status_message; // status of most recent integration.
+  real_t max_dt, stop_time;
+
+  // Error weight function.
+  dae_integrator_error_weight_func compute_weights;
+
+  // Preconditioning stuff.
+  preconditioner_t* precond;
+  preconditioner_matrix_t* precond_mat;
+};
+
+// This function wraps around the user-supplied right hand side.
+static int evaluate_residual(real_t t, N_Vector x, N_Vector x_dot, 
+                             N_Vector F, void* context)
+{
+  dae_integrator_t* integ = context;
+  real_t* xx = NV_DATA(x);
+  real_t* xxd = NV_DATA(x_dot);
+  real_t* Fx = NV_DATA(F);
+
+  // Do parallel communication.
+  if (integ->vtable.communicate != NULL)
+    integ->vtable.communicate(integ->context, t, xx, xxd);
+
+  // Evaluate the residual.
+  return integ->vtable.residual(integ->context, t, xx, xxd, Fx);
+}
+
+// This function sets up the preconditioner data within the integrator.
+static int set_up_preconditioner(real_t t, N_Vector x, N_Vector x_dot, N_Vector F,
+                                 real_t cj, void* context,
+                                 N_Vector work1, N_Vector work2, N_Vector work3)
+{
+  dae_integrator_t* integ = context;
+  preconditioner_compute_jacobian(integ->precond, t, NV_DATA(x), integ->precond_mat);
+//  preconditioner_matrix_scale_and_shift(integ->precond_mat, -gamma);
+  return 0;
+}
+
+// This function solves the preconditioner equation. On input, the vector r 
+// contains the right-hand side of the preconditioner system, and on output 
+// it contains the solution to the system.
+static int solve_preconditioner_system(real_t t, N_Vector x, N_Vector x_dot, 
+                                       N_Vector F, N_Vector r, N_Vector z, 
+                                       real_t cj, real_t delta, 
+                                       void* context, N_Vector work)
+{
+  dae_integrator_t* integ = context;
+  
+  // FIXME: Apply scaling if needed.
+  // Copy the contents of the RHS to the output vector.
+  memcpy(NV_DATA(z), NV_DATA(r), sizeof(real_t) * integ->N);
+
+  // Solve it.
+  if (preconditioner_solve(integ->precond, integ->precond_mat, NV_DATA(z)))
+    return 0;
+  else 
+    return 1; // recoverable error.
+}
+
+static dae_integrator_t* dae_integrator_new(const char* name, 
+                                            void* context,
+                                            MPI_Comm comm,
+                                            int N,
+                                            dae_integrator_vtable vtable,
+                                            int order,
+                                            solver_type_t solver_type,
+                                            int max_krylov_dim)
+{
+  ASSERT(N > 0);
+  ASSERT(order > 0);
+  ASSERT(order <= 5);
+  ASSERT(vtable.residual != NULL);
+  ASSERT(max_krylov_dim >= 3);
+
+  dae_integrator_t* integ = malloc(sizeof(dae_integrator_t));
+  integ->name = string_dup(name);
+  integ->context = context;
+  integ->comm = comm;
+  integ->vtable = vtable;
+  integ->order = order;
+  integ->solver_type = solver_type;
+  integ->current_time = 0.0;
+  integ->N = N;
+  integ->max_krylov_dim = max_krylov_dim;
+  integ->initialized = false;
+  integ->max_dt = FLT_MAX;
+  integ->status_message = NULL;
+
+  // Set up KINSol and accessories.
+  integ->x = N_VNew(comm, N);
+  integ->x_dot = N_VNew(comm, N);
+  integ->ida = IDACreate();
+  IDASetMaxOrd(integ->ida, integ->order);
+  IDASetUserData(integ->ida, integ);
+  IDAInit(integ->ida, evaluate_residual, integ->current_time, integ->x, integ->x_dot);
+
+  // Select the particular type of Krylov method for the underlying linear solves.
+  if (solver_type == GMRES)
+    IDASpgmr(integ->ida, max_krylov_dim); 
+  else if (solver_type == BICGSTAB)
+    IDASpbcg(integ->ida, max_krylov_dim);
+  else
+    IDASptfqmr(integ->ida, max_krylov_dim);
+
+  // We use modified Gram-Schmidt orthogonalization.
+  IDASpilsSetGSType(integ->ida, MODIFIED_GS);
+
+  IDASpilsSetPreconditioner(integ->ida, set_up_preconditioner,
+                           solve_preconditioner_system);
+
+  integ->precond = NULL;
+  integ->precond_mat = NULL;
+
+  // Set some default tolerances:
+  // relative error of 1e-4 means errors are controlled to 0.01%.
+  // absolute error is set to 1 because it's completely problem dependent.
+  dae_integrator_set_tolerances(integ, 1e-4, 1.0);
+
+  // Set up a maximum number of steps to take during the integration.
+//  IDASetMaxNumSteps(integ->ida, 500); // default is 500.
+
+  return integ;
+}
+
+dae_integrator_t* gmres_dae_integrator_new(const char* name,
+                                           void* context,
+                                           MPI_Comm comm,
+                                           int N,
+                                           dae_integrator_vtable vtable,
+                                           int order,
+                                           int max_krylov_dim)
+{
+  return dae_integrator_new(name, context, comm, N, vtable, order, GMRES, 
+                            max_krylov_dim);
+}
+
+dae_integrator_t* bicgstab_dae_integrator_new(const char* name,
+                                              void* context,
+                                              MPI_Comm comm,
+                                              int N,
+                                              dae_integrator_vtable vtable,
+                                              int order,
+                                              int max_krylov_dim)
+{
+  return dae_integrator_new(name, context, comm, N, vtable, order, BICGSTAB, 
+                            max_krylov_dim);
+}
+
+dae_integrator_t* tfqmr_dae_integrator_new(const char* name,
+                                           void* context,
+                                           MPI_Comm comm,
+                                           int N,
+                                           dae_integrator_vtable vtable,
+                                           int order,
+                                           int max_krylov_dim)
+{
+  return dae_integrator_new(name, context, comm, N, vtable, order, TFQMR, 
+                            max_krylov_dim);
+}
+
+void dae_integrator_free(dae_integrator_t* integ)
+{
+  // Kill the preconditioner stuff.
+  if (integ->precond != NULL)
+    preconditioner_free(integ->precond);
+  if (integ->precond_mat != NULL)
+    preconditioner_matrix_free(integ->precond_mat);
+
+  // Kill the IDA stuff.
+  N_VDestroy(integ->x_dot);
+  N_VDestroy(integ->x);
+  IDAFree(&integ->ida);
+
+  // Kill the rest.
+  if (integ->status_message != NULL)
+    free(integ->status_message);
+  if ((integ->context != NULL) && (integ->vtable.dtor != NULL))
+    integ->vtable.dtor(integ->context);
+  free(integ->name);
+  free(integ);
+}
+
+char* dae_integrator_name(dae_integrator_t* integ)
+{
+  return integ->name;
+}
+
+void* dae_integrator_context(dae_integrator_t* integ)
+{
+  return integ->context;
+}
+
+int dae_integrator_order(dae_integrator_t* integ)
+{
+  return integ->order;
+}
+
+void dae_integrator_set_preconditioner(dae_integrator_t* integrator,
+                                       preconditioner_t* precond)
+{
+  integrator->precond = precond;
+  if (integrator->precond_mat != NULL)
+    preconditioner_matrix_free(integrator->precond_mat);
+  integrator->precond_mat = preconditioner_matrix(precond);
+}
+
+preconditioner_matrix_t* dae_integrator_preconditioner_matrix(dae_integrator_t* integrator)
+{
+  return integrator->precond_mat;
+}
+
+void dae_integrator_set_tolerances(dae_integrator_t* integrator,
+                                    real_t relative_tol, real_t absolute_tol)
+{
+  ASSERT(relative_tol > 0.0);
+  ASSERT(absolute_tol > 0.0);
+
+  // Clear any existing error weight function.
+  integrator->compute_weights = NULL;
+
+  // Set the tolerances.
+  IDASStolerances(integrator->ida, relative_tol, absolute_tol);
+}
+
+// Error weight adaptor function.
+static int compute_error_weights(N_Vector y, N_Vector ewt, void* context)
+{
+  dae_integrator_t* integ = context;
+  integ->compute_weights(integ->context, NV_DATA(y), NV_DATA(ewt));
+  return 0;
+}
+
+void dae_integrator_set_error_weight_function(dae_integrator_t* integrator,
+                                               dae_integrator_error_weight_func compute_weights)
+{
+  ASSERT(compute_weights != NULL);
+  integrator->compute_weights = compute_weights;
+  IDAWFtolerances(integrator->ida, compute_error_weights);
+}
+
+void dae_integrator_eval_residual(dae_integrator_t* integ, real_t t, real_t* X, real_t* X_dot, real_t* F)
+{
+  integ->vtable.residual(integ->context, t, X, X_dot, F);
+}
+
+void dae_integrator_set_max_dt(dae_integrator_t* integ, real_t max_dt)
+{
+  ASSERT(max_dt > 0);
+  integ->max_dt = max_dt;
+  IDASetMaxStep(integ->ida, max_dt);
+}
+
+void dae_integrator_set_stop_time(dae_integrator_t* integ, real_t stop_time)
+{
+  integ->stop_time = stop_time;
+  IDASetStopTime(integ->ida, stop_time);
+}
+
+bool dae_integrator_step(dae_integrator_t* integ, real_t* t, real_t* X)
+{
+  // Copy in the solution.
+  memcpy(NV_DATA(integ->x), X, sizeof(real_t) * integ->N); 
+
+  if (!integ->initialized)
+  {
+    integ->current_time = *t;
+    IDAReInit(integ->ida, integ->current_time, integ->x, integ->x_dot);
+    integ->initialized = true;
+  }
+  else if (fabs(integ->current_time - *t) > 1e-14)
+  {
+    // Reset the integrator if t != current_time.
+    integ->current_time = *t;
+    IDAReInit(integ->ida, integ->current_time, integ->x, integ->x_dot);
+  }
+
+  // Integrate.
+  real_t t2 = *t + integ->max_dt;
+  int status = IDASolve(integ->ida, t2, &integ->current_time, integ->x, integ->x_dot, IDA_ONE_STEP);
+  
+  // Clear the present status.
+  if (integ->status_message != NULL)
+  {
+    free(integ->status_message);
+    integ->status_message = NULL;
+  }
+
+  // Did it work?
+  if ((status == IDA_SUCCESS) || (status == IDA_TSTOP_RETURN))
+  {
+    // Copy out the solution.
+    *t = integ->current_time;
+    memcpy(X, NV_DATA(integ->x), sizeof(real_t) * integ->N); 
+    return true;
+  }
+  else
+  {
+    if (status == IDA_TOO_MUCH_WORK)
+    {
+      char err[1024];
+      snprintf(err, 1024, "Integrator stopped at t = %g after maximum number of steps.", integ->current_time);
+      integ->status_message = string_dup(err);
+    }
+    else if (status == IDA_TOO_MUCH_ACC)
+      integ->status_message = string_dup("Integrator could not achieve desired level of accuracy.");
+    else if (status == IDA_ERR_FAIL)
+      integ->status_message = string_dup("Integrator encountered too many error test failures.");
+    else if (status == IDA_CONV_FAIL)
+      integ->status_message = string_dup("Integrator encountered too many convergence test failures.");
+    else if (status == IDA_LINIT_FAIL)
+      integ->status_message = string_dup("Integrator's linear solver failed to initialize.");
+    else if (status == IDA_LSETUP_FAIL)
+      integ->status_message = string_dup("Integrator's linear solver setup failed.");
+    else if (status == IDA_LSOLVE_FAIL)
+      integ->status_message = string_dup("Integrator's linear solver failed.");
+    else if (status == IDA_CONSTR_FAIL)
+      integ->status_message = string_dup("Integrator's inequality constraints were violated unrecoverably.");
+    else if (status == IDA_RES_FAIL)
+      integ->status_message = string_dup("Integrator's residual function failed unrecoverably.");
+    else if (status == IDA_REP_RES_ERR)
+      integ->status_message = string_dup("Integrator encountered too many recoverable RHS failures.");
+    else if (status == IDA_RTFUNC_FAIL)
+      integ->status_message = string_dup("Integrator encountered a failure in the rootfinding function.");
+    return false;
+  }
+}
+
+void dae_integrator_get_diagnostics(dae_integrator_t* integrator, 
+                                    dae_integrator_diagnostics_t* diagnostics)
+{
+  diagnostics->status_message = integrator->status_message; // borrowed!
+  IDAGetNumSteps(integrator->ida, &diagnostics->num_steps);
+  IDAGetLastOrder(integrator->ida, &diagnostics->order_of_last_step);
+  IDAGetActualInitStep(integrator->ida, &diagnostics->initial_step_size);
+  IDAGetLastStep(integrator->ida, &diagnostics->last_step_size);
+  IDAGetNumResEvals(integrator->ida, &diagnostics->num_residual_evaluations);
+  IDAGetNumLinSolvSetups(integrator->ida, &diagnostics->num_linear_solve_setups);
+  IDAGetNumErrTestFails(integrator->ida, &diagnostics->num_error_test_failures);
+  IDAGetNumNonlinSolvIters(integrator->ida, &diagnostics->num_nonlinear_solve_iterations);
+  IDAGetNumNonlinSolvConvFails(integrator->ida, &diagnostics->num_nonlinear_solve_convergence_failures);
+  IDASpilsGetNumLinIters(integrator->ida, &diagnostics->num_linear_solve_iterations);
+  IDASpilsGetNumPrecEvals(integrator->ida, &diagnostics->num_preconditioner_evaluations);
+  IDASpilsGetNumPrecSolves(integrator->ida, &diagnostics->num_preconditioner_solves);
+  IDASpilsGetNumConvFails(integrator->ida, &diagnostics->num_linear_solve_convergence_failures);
+}
+
+void dae_integrator_diagnostics_fprintf(dae_integrator_diagnostics_t* diagnostics, 
+                                        FILE* stream)
+{
+  fprintf(stream, "dae integrator diagnostics:\n");
+  if (diagnostics->status_message != NULL)
+    fprintf(stream, "  Status: %s\n", diagnostics->status_message);
+  fprintf(stream, "  Num steps: %d\n", (int)diagnostics->num_steps);
+  fprintf(stream, "  Num order reductions for stability: %d\n", (int)diagnostics->num_order_reductions);
+  fprintf(stream, "  Order of last step: %d\n", diagnostics->order_of_last_step);
+  fprintf(stream, "  Initial step size: %g\n", diagnostics->initial_step_size);
+  fprintf(stream, "  Last step size: %g\n", diagnostics->last_step_size);
+  fprintf(stream, "  Num residual evaluations: %d\n", (int)diagnostics->num_residual_evaluations);
+  fprintf(stream, "  Num linear solve setups: %d\n", (int)diagnostics->num_linear_solve_setups);
+  fprintf(stream, "  Num linear solve convergence failures: %d\n", (int)diagnostics->num_linear_solve_convergence_failures);
+  fprintf(stream, "  Num error test failures: %d\n", (int)diagnostics->num_error_test_failures);
+  fprintf(stream, "  Num nonlinear solve iterations: %d\n", (int)diagnostics->num_nonlinear_solve_iterations);
+  fprintf(stream, "  Num nonlinear solve convergence failures: %d\n", (int)diagnostics->num_nonlinear_solve_convergence_failures);
+  fprintf(stream, "  Num preconditioner evaluations: %d\n", (int)diagnostics->num_preconditioner_evaluations);
+  fprintf(stream, "  Num preconditioner solves: %d\n", (int)diagnostics->num_preconditioner_solves);
+}
+
+
--- integrators/dae_integrator.h
+++ integrators/dae_integrator.h
@@ -4,7 +4,7 @@
 // Redistribution and use in source and binary forms, with or without 
 // modification, are permitted provided that the following conditions are met:
 // 
-// 1. Redistributions of source code must retain the above copyright notice, this 
+// 1. Redistributions of source cdae must retain the above copyright notice, this 
 // list of conditions and the following disclaimer.
 // 
 // 2. Redistributions in binary form must reproduce the above copyright notice, 
@@ -22,141 +22,144 @@
 // OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-#ifndef POLYMEC_ODE_INTEGRATOR_H
-#define POLYMEC_ODE_INTEGRATOR_H
+#ifndef POLYMEC_DAE_INTEGRATOR_H
+#define POLYMEC_DAE_INTEGRATOR_H
 
 #include "core/polymec.h"
 #include "core/adj_graph.h"
 #include "integrators/preconditioner.h"
 
-// This function evaluates the RHS of a system of ODEs.
-typedef int (*ode_integrator_rhs_func)(void* context, real_t t, real_t* x, real_t* x_dot);
+// This function evaluates the residual of a system of differential-algebraic 
+// equations (DAEs).
+typedef int (*dae_integrator_residual_func)(void* context, real_t t, real_t* x, real_t* x_dot, real_t* F);
 
 // This function performs any parallel communication that is needed to 
 // put values in place for the evaluation of the residual function.
-typedef void (*ode_integrator_communication_func)(void* context, real_t t, real_t* x);
+typedef void (*dae_integrator_communication_func)(void* context, real_t t, real_t* x, real_t* x_dot);
 
 // This function destroys the state (context).
-typedef void (*ode_integrator_dtor)(void* context);
+typedef void (*dae_integrator_dtor)(void* context);
 
 // This function evaluates error weights for use in the WRMS error norm.
-typedef void (*ode_integrator_error_weight_func)(void* context, real_t* y, real_t* weights);
+typedef void (*dae_integrator_error_weight_func)(void* context, real_t* y, real_t* weights);
 
 // This virtual table determines the behavior of the time integrator.
 typedef struct
 {
-  // This function evaluates the right hand side of a coupled system of 
-  // nonlinear ordinary different equations at time t with solution x, 
-  // storing it in x_dot. It should return 0 on success, 1 for a 
-  // recoverable error, -1 for a fatal error.
-  ode_integrator_rhs_func rhs;
+  // This function evaluates the residual function for a system of 
+  // DAEs at time t with solution x and time derivative x_dot, storing it 
+  // in F. It should return 0 on success, 1 for a recoverable error, -1 
+  // for a fatal error.
+  dae_integrator_residual_func residual;
 
   // Perform inter-process communication.
-  ode_integrator_communication_func communicate;
+  dae_integrator_communication_func communicate;
 
   // This (optional) function destroys the state (context) when the time integrator 
   // is destroyed.
-  ode_integrator_dtor dtor;
+  dae_integrator_dtor dtor;
 
-} ode_integrator_vtable;
+} dae_integrator_vtable;
 
 // This class provides an abstract interface for integrating systems of 
 // nonlinear differential equations. 
-typedef struct ode_integrator_t ode_integrator_t;
+typedef struct dae_integrator_t dae_integrator_t;
 
 // Creates an integrator that uses a GMRES Krylov method with a given 
 // maximum subspace dimension of max_krylov_dim. No restarts are used.
 // N is the dimension of the system.
-ode_integrator_t* gmres_ode_integrator_new(const char* name, 
+dae_integrator_t* gmres_dae_integrator_new(const char* name, 
                                            void* context,
                                            MPI_Comm comm,
                                            int N,
-                                           ode_integrator_vtable vtable,
+                                           dae_integrator_vtable vtable,
                                            int order,
                                            int max_krylov_dim);
 
 // Creates an integrator that uses a stabilized bi-conjugate gradient Krylov 
 // method with a given maximum subspace dimension of max_krylov_dim.
 // N is the dimension of the system.
-ode_integrator_t* bicgs_ode_integrator_new(const char* name, 
+dae_integrator_t* bicgs_dae_integrator_new(const char* name, 
                                            void* context,
                                            MPI_Comm comm,
                                            int N,
-                                           ode_integrator_vtable vtable,
+                                           dae_integrator_vtable vtable,
                                            int order,
                                            int max_krylov_dim);
 
 // Creates an integrator that uses a transpose-free quasi-minimum residual 
 // Krylov method with a given maximum subspace dimension of max_krylov_dim.
 // N is the dimension of the system.
-ode_integrator_t* tfqmr_ode_integrator_new(const char* name, 
+dae_integrator_t* tfqmr_dae_integrator_new(const char* name, 
                                            void* context,
                                            MPI_Comm comm,
                                            int N,
-                                           ode_integrator_vtable vtable,
+                                           dae_integrator_vtable vtable,
                                            int order,
                                            int max_krylov_dim);
 
 // Frees a time integrator.
-void ode_integrator_free(ode_integrator_t* integrator);
+void dae_integrator_free(dae_integrator_t* integrator);
 
 // Returns the name of the integrator (internally stored).
-char* ode_integrator_name(ode_integrator_t* integrator);
+char* dae_integrator_name(dae_integrator_t* integrator);
 
 // Returns the context object for this integrator.
-void* ode_integrator_context(ode_integrator_t* integrator);
+void* dae_integrator_context(dae_integrator_t* integrator);
 
 // Returns the order of the integration method.
-int ode_integrator_order(ode_integrator_t* integrator);
+int dae_integrator_order(dae_integrator_t* integrator);
 
 // Sets the preconditioner to use to help solve the equations.
-void ode_integrator_set_preconditioner(ode_integrator_t* integrator,
+void dae_integrator_set_preconditioner(dae_integrator_t* integrator,
                                        preconditioner_t* precond);
 
 // Returns the current preconditioner matrix for the integrator, or 
 // NULL if there is no such matrix.
-preconditioner_matrix_t* ode_integrator_preconditioner_matrix(ode_integrator_t* integrator);
+preconditioner_matrix_t* dae_integrator_preconditioner_matrix(dae_integrator_t* integrator);
 
 // Sets whether to use a stability limit detection algorithm to improve 
 // robustness on particularly stiff problems (2-10% overhead, depending 
 // on the problem).
-void ode_integrator_set_stability_limit_detection(ode_integrator_t* integrator,
+void dae_integrator_set_stability_limit_detection(dae_integrator_t* integrator,
                                                   bool use_detection);
 
 // Sets the relative and absolute tolerances for integrated quantities.
-void ode_integrator_set_tolerances(ode_integrator_t* integrator,
+void dae_integrator_set_tolerances(dae_integrator_t* integrator,
                                    real_t relative_tol, real_t absolute_tol);
 
 // Sets the error weight function for evaluating the WRMS norm that is used 
 // as a proxy for the quality of the solution. This may be used in lieu of 
 // relative and absolute tolerances.
-void ode_integrator_set_error_weight_function(ode_integrator_t* integrator,
-                                              ode_integrator_error_weight_func compute_weights);                               
+void dae_integrator_set_error_weight_function(dae_integrator_t* integrator,
+                                              dae_integrator_error_weight_func compute_weights);                               
 
 // Evaluates the right-hand side of the system at the given time and with the 
 // given solution X, placing the results in rhs.
-void ode_integrator_eval_rhs(ode_integrator_t* integ, real_t t, real_t* X, real_t* rhs);
+void dae_integrator_eval_rhs(dae_integrator_t* integ, real_t t, real_t* X, real_t* rhs);
 
 // Sets the maximum time step size for the next integration step.
-void ode_integrator_set_max_dt(ode_integrator_t* integ, real_t max_dt);
+void dae_integrator_set_max_dt(dae_integrator_t* integ, real_t max_dt);
 
 // Sets the time past which the integrator will not step.
-void ode_integrator_set_stop_time(ode_integrator_t* integ, real_t stop_time);
+void dae_integrator_set_stop_time(dae_integrator_t* integ, real_t stop_time);
 
 // Integrates the given solution X in place, taking a single step starting at 
 // time *t and storing the new time in *t as well. Returns true if the step 
 // succeeded, false if it failed for some reason. If a step fails, both t 
 // and X remain unchanged.
-bool ode_integrator_step(ode_integrator_t* integrator, real_t* t, real_t* X);
+bool dae_integrator_step(dae_integrator_t* integrator, real_t* t, real_t* X);
 
 // Diagnostics for the time integrator.
 typedef struct
 {
   char* status_message; // borrowed pointer from integrator: do not free.
   long int num_steps;
+  long int num_order_reductions;
   int order_of_last_step;
+  real_t initial_step_size;
   real_t last_step_size;
-  long int num_rhs_evaluations;
+  long int num_residual_evaluations;
   long int num_linear_solve_setups;
   long int num_linear_solve_iterations;
   long int num_linear_solve_convergence_failures;
@@ -165,14 +168,14 @@ typedef struct
   long int num_nonlinear_solve_convergence_failures;
   long int num_preconditioner_evaluations;
   long int num_preconditioner_solves;
-} ode_integrator_diagnostics_t;
+} dae_integrator_diagnostics_t;
 
 // Retrieve diagnostics for the time integrator.
-void ode_integrator_get_diagnostics(ode_integrator_t* integrator, 
-                                    ode_integrator_diagnostics_t* diagnostics);
+void dae_integrator_get_diagnostics(dae_integrator_t* integrator, 
+                                    dae_integrator_diagnostics_t* diagnostics);
 
 // Writes time integrator diagnostics to the given file.
-void ode_integrator_diagnostics_fprintf(ode_integrator_diagnostics_t* diagnostics, 
+void dae_integrator_diagnostics_fprintf(dae_integrator_diagnostics_t* diagnostics, 
                                         FILE* stream);
 
 #endif
