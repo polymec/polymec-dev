8b2ae03e06647f8e5c7eca88338059c905c07b61
Jeffrey Johnson <jjphatt@gmail.com>
2014-03-17 11:30:31 -0700

    More work on tetrahedral primitives.

--- geometry/tetrahedron.c
+++ geometry/tetrahedron.c
@@ -26,6 +26,10 @@
 #include "core/polymec.h"
 #include "geometry/tetrahedron.h"
 
+// Jonathan Shewchuk's geometric predicates, which are implemented in 
+// double-precision arithmetic.
+extern double orient3d(double* pa, double* pb, double* pc, double* pd);
+
 struct tetrahedron_t 
 {
   point_t vertices[4];
@@ -55,14 +59,24 @@ void tetrahedron_set_vertices(tetrahedron_t* t,
   t->vertices[3] = *v4;
 }
 
+// This adapts Shewchuk's orient3d to the vertices (a, b, c, d) of a tetrahedron.
+static inline real_t tet_orient3d(point_t* a, point_t* b, point_t* c, point_t* d)
+{
+#if POLYMEC_HAVE_DOUBLE_PRECISION
+  return orient3d((double*)a, (double*)b, (double*)c, (double*)d);
+#else
+  double da[3], db[3], dc[3], dd[3];
+  da[0] = (double)a->x; da[1] = (double)a->y; da[2] = (double)a->z;
+  db[0] = (double)b->x; db[1] = (double)b->y; db[2] = (double)b->z;
+  dc[0] = (double)c->x; dc[1] = (double)c->y; dc[2] = (double)c->z;
+  dd[0] = (double)d->x; dd[1] = (double)d->y; dd[2] = (double)d->z;
+  return (real_t)orient3d(da, db, dc, dd)/6.0;
+#endif
+}
+
 real_t tetrahedron_volume(tetrahedron_t* t)
 {
-  vector_t u1, u2, u3, u2xu3;
-  point_displacement(&t->vertices[0], &t->vertices[1], &u1);
-  point_displacement(&t->vertices[0], &t->vertices[2], &u2);
-  point_displacement(&t->vertices[0], &t->vertices[3], &u3);
-  vector_cross(&u2, &u3, &u2xu3);
-  return fabs(vector_dot(&u1, &u2xu3))/6.0;
+  return tet_orient3d(&t->vertices[0], &t->vertices[1], &t->vertices[2], &t->vertices[3])/6.0;
 }
 
 void tetrahedron_compute_centroid(tetrahedron_t* t, point_t* centroid)
@@ -74,11 +88,46 @@ void tetrahedron_compute_centroid(tetrahedron_t* t, point_t* centroid)
 
 void tetrahedron_compute_circumcenter(tetrahedron_t* t, point_t* circumcenter)
 {
+  // Vector differences ad = a - d, bd = b - d, cd = c - d.
+  vector_t ad = {.x = t->vertices[0].x - t->vertices[3].x,
+                 .y = t->vertices[0].y - t->vertices[3].y,
+                 .z = t->vertices[0].z - t->vertices[3].z};
+  vector_t bd = {.x = t->vertices[1].x - t->vertices[3].x,
+                 .y = t->vertices[1].y - t->vertices[3].y,
+                 .z = t->vertices[1].z - t->vertices[3].z};
+  vector_t cd = {.x = t->vertices[2].x - t->vertices[3].x,
+                 .y = t->vertices[2].y - t->vertices[3].y,
+                 .z = t->vertices[2].z - t->vertices[3].z};
+
+  // Square magnitudes of differences.
+  real_t ad2 = vector_dot(&ad, &ad), bd2 = vector_dot(&bd, &bd), cd2 = vector_dot(&cd, &cd);
+
+  // Compute the circumcenter using robust geometric predicates
+  // (section 3.11 of Lecture Notes on Geometric Robustness,
+  //  Jonathan Shewchuk, April 15, 2013).
+  real_t denom = 2.0 * tet_orient3d(&t->vertices[0], &t->vertices[1], &t->vertices[2], &t->vertices[3]);
+  circumcenter->x = t->vertices[3].x + (ad2*bd.x*cd.x + bd2*cd.x*ad.x + cd2*ad.x*bd.x) / denom;
+  circumcenter->y = t->vertices[3].y + (ad2*bd.y*cd.y + bd2*cd.y*ad.y + cd2*ad.y*bd.y) / denom;
+  circumcenter->z = t->vertices[3].z + (ad2*bd.z*cd.z + bd2*cd.z*ad.z + cd2*ad.z*bd.z) / denom;
 }
 
 bool tetrahedron_contains_point(tetrahedron_t* t, point_t* x)
 {
-  return false;
+  // Taken from Newsgroups: comp.graphics,comp.graphics.algorithms
+  // From: herron@cs.washington.edu (Gary Herron)
+  // Subject: Re: point within a tetrahedron
+  // Date: Wed, 23 Feb 94 21:52:45 GMT
+  int sign1 = SIGN(tet_orient3d(&t->vertices[0], &t->vertices[1], &t->vertices[2], &t->vertices[3]));
+  if (sign1 == 0) return false;
+  int sign2 = SIGN(tet_orient3d(x, &t->vertices[1], &t->vertices[2], &t->vertices[3]));
+  if ((sign2 == 0) || (sign1*sign2 < 0)) return false;
+  int sign3 = SIGN(tet_orient3d(&t->vertices[0], x, &t->vertices[2], &t->vertices[3]));
+  if ((sign3 == 0) || (sign1*sign3 < 0)) return false;
+  int sign4 = SIGN(tet_orient3d(&t->vertices[0], &t->vertices[1], x, &t->vertices[3]));
+  if ((sign4 == 0) || (sign1*sign4 < 0)) return false;
+  int sign5 = SIGN(tet_orient3d(&t->vertices[0], &t->vertices[1], &t->vertices[2], x));
+  if ((sign5 == 0) || (sign1*sign4 < 0)) return false;
+  return true;
 }
 
 void tetrahedron_compute_nearest_point(tetrahedron_t* t, point_t* x, point_t* y)
