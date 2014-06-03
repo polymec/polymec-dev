0b67fbae43f20ee8a466ccc664a58b86aaa8c7de
Jeffrey Johnson <jjphatt@gmail.com>
2014-06-03 00:31:29 -0700

    Scotch now builds correctly.

--- core/polymec.c
+++ core/polymec.c
@@ -159,6 +159,25 @@ static noreturn void default_error_handler(const char* message)
   exit(-1);
 }
 
+#ifdef POLYMEC_HAVE_MPI
+// Here are the error handlers for the Scotch partitioning library.
+void SCOTCH_errorPrint(const char* const errstr, ...)
+{
+  va_list argp;
+  va_start(argp, errstr);
+  polymec_error(errstr, argp);
+  va_end(argp);
+}
+
+void SCOTCH_errorPrintW(const char* const errstr, ...)
+{
+  va_list argp;
+  va_start(argp, errstr);
+  polymec_warn(errstr, argp);
+  va_end(argp);
+}
+#endif
+
 void polymec_abort(const char* message, ...)
 {
   // Extract the variadic arguments and splat them into a string.
