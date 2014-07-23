1372f9831d7a2aa9751a1aa43c201afe7643fd5f
Jeffrey Johnson <jnjohnson@lbl.gov>
2014-07-23 08:08:09 -0700

    Added some more unit tests. Fixed an oversight with MPI unit tests.

---
+++ geometry/tests/test_partition_mesh.c
@@ -0,0 +1,68 @@
+// Copyright (c) 2012-2014, Jeffrey N. Johnson
+// All rights reserved.
+// 
+// Redistribution and use in source and binary forms, with or without 
+// modification, are permitted provided that the following conditions are met:
+// 
+// 1. Redistributions of source code must retain the above copyright notice, this 
+// list of conditions and the following disclaimer.
+// 
+// 2. Redistributions in binary form must reproduce the above copyright notice, 
+// this list of conditions and the following disclaimer in the documentation 
+// and/or other materials provided with the distribution.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE 
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE 
+// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL 
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR 
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER 
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, 
+// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+#include <stdarg.h>
+#include <stddef.h>
+#include <setjmp.h>
+#include <string.h>
+#include "cmockery.h"
+#include "core/silo_file.h"
+#include "geometry/create_uniform_mesh.h"
+#include "geometry/repartition.h"
+
+void test_partition_uniform_mesh(void** state)
+{
+  // Create a 2x1x1 uniform mesh.
+  int nx = 4, ny = 1, nz = 1;
+  bbox_t bbox = {.x1 = 0.0, .x2 = 1.0, .y1 = 0.0, .y2 = 1.0, .z1 = 0.0, .z2 = 1.0};
+  mesh_t* mesh = create_uniform_mesh(MPI_COMM_SELF, nx, ny, nz, &bbox);
+
+  // Repartition it.
+  exchanger_t* distributor = partition_mesh(&mesh, NULL, 0.05);
+  exchanger_free(distributor);
+
+  // Plot it.
+  int rank;
+  MPI_Comm_rank(mesh->comm, &rank);
+  double ones[mesh->num_cells];
+  for (int c = 0; c < mesh->num_cells; ++c)
+    ones[c] = 1.0*rank;
+  silo_file_t* silo = silo_file_new(mesh->comm, "uniform_mesh_partition", "uniform_mesh_partition", 1, 0, 0, 0.0);
+  silo_file_write_mesh(silo, "mesh", mesh);
+  silo_file_write_scalar_cell_field(silo, "rank", "mesh", ones);
+  silo_file_close(silo);
+
+  // Clean up.
+  mesh_free(mesh);
+}
+
+int main(int argc, char* argv[]) 
+{
+  polymec_init(argc, argv);
+  const UnitTest tests[] = 
+  {
+    unit_test(test_partition_uniform_mesh)
+  };
+  return run_tests(tests);
+}
