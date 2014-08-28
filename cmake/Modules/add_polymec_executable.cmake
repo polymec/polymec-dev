e96d02804a0579c8184fb29ec2f2892f58e1e061
Jeffrey Johnson <jnjohnson@lbl.gov>
2014-08-27 22:34:04 -0700

    Polymec executables now link with C, not C++.

--- cmake/Modules/add_polymec_executable.cmake
+++ cmake/Modules/add_polymec_executable.cmake
@@ -1,7 +1,7 @@
-# This function adds a (serial) unit test executable to be built using cmockery.
+# This function adds an executable program that links against polymec.
 function(add_polymec_executable exe)
   add_executable(${exe} ${ARGN})
-  set_target_properties(${exe} PROPERTIES OUTPUT_NAME ${exe} LINKER_LANGUAGE CXX)
+#  set_target_properties(${exe} PROPERTIES OUTPUT_NAME ${exe} LINKER_LANGUAGE CXX)
   target_link_libraries(${exe} ${POLYMEC_LIBS})
 endfunction(add_polymec_executable)
 
