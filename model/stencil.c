a72cfb5ccc39dabf3e95c9a4270b0ec5925cdf6e
Jeffrey Johnson <jnjohnson@lbl.gov>
2014-09-10 22:53:14 -0700

    Added a "stencil" class for representing non-trivial stencils.

---
+++ model/stencil.c
@@ -0,0 +1,52 @@
+// Copyright (c) 2012-2014, Jeffrey N. Johnson
+// All rights reserved.
+// 
+// Redistribution and use in source and binary forms, with or without 
+// modification, are permitted provided that the following conditions are met:
+// 
+// 1. Redistributions of source code must retain the above copyright notice, this 
+// list of conditions and the following disclaimer.
+// 
+// 2. Redistributions in binary form must reproduce the above copyright notice, 
+// this list of conditions and the following disclaimer in the documentation 
+// and/or other materials provided with the distribution.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE 
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE 
+// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL 
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR 
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER 
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, 
+// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+#include "model/stencil.h"
+#include "model/stencil.h"
+
+stencil_t* stencil_new(const char* name, int num_indices, 
+                       int* offsets, int* indices, real_t* weights,
+                       exchanger_t* ex)
+{
+  ASSERT(num_indices > 0);
+  stencil_t* s = polymec_malloc(sizeof(stencil_t));
+  s->name = string_dup(name);
+  s->num_indices = num_indices;
+  s->offsets = offsets;
+  s->indices = indices;
+  s->weights = weights;
+  s->ex = ex;
+  return s;
+}
+
+void stencil_free(stencil_t* stencil)
+{
+  polymec_free(stencil->name);
+  polymec_free(stencil->offsets);
+  polymec_free(stencil->indices);
+  polymec_free(stencil->weights);
+  exchanger_free(stencil->ex);
+  polymec_free(stencil);
+}
+
---
+++ model/stencil.h
@@ -0,0 +1,75 @@
+// Copyright (c) 2012-2014, Jeffrey N. Johnson
+// All rights reserved.
+// 
+// Redistribution and use in source and binary forms, with or without 
+// modification, are permitted provided that the following conditions are met:
+// 
+// 1. Redistributions of source code must retain the above copyright notice, this 
+// list of conditions and the following disclaimer.
+// 
+// 2. Redistributions in binary form must reproduce the above copyright notice, 
+// this list of conditions and the following disclaimer in the documentation 
+// and/or other materials provided with the distribution.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE 
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE 
+// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL 
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR 
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER 
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, 
+// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+#ifndef POLYMEC_STENCIL_H
+#define POLYMEC_STENCIL_H
+
+#include "core/polymec.h"
+#include "core/exchanger.h"
+
+// A stencil is a set of indices and weights associated with a stencil for 
+// some spatial discretization. Stencils can be constructed for any set of 
+// indices representing a spatial domain, and the underlying indices and 
+// weights are stored in compressed arrays for efficiency.
+typedef struct 
+{
+  char* name;
+  int num_indices;
+  int* offsets;
+  int* indices;
+  real_t* weights;
+  exchanger_t* ex; // Used to perform exchanges to fill all stencils on a domain.
+} stencil_t;
+
+// Creates a stencil object using compressed arrays for (1) the offsets of 
+// the different stencils for each index, (2) the indices of each stencil 
+// therein, and (3) the weights associated with the indices in each stencil.
+// Returns a newly-allocated stencil. These arrays must be allocated using 
+// polymec_malloc, and are consumed by the stencil. Likewise, the stencil's
+// exchanger is consumed by the stencil and is used for its exchanges.
+stencil_t* stencil_new(const char* name, int num_indices, 
+                       int* offsets, int* indices, real_t* weights,
+                       exchanger_t* ex);
+
+// Destroys the given stencil object.
+void stencil_free(stencil_t* stencil);
+
+// Traverses the stencil for a given index i, returning true if the traversal
+// has more indices remaining and false if it has completed. The pos pointer 
+// must be set to zero to reset the traversal. The j and weight pointers will 
+// store the next stencil index and weight, respectively.
+static inline bool stencil_next(stencil_t* stencil, int i, int* pos,
+                                int* j, real_t* weight)
+{
+  ASSERT(i < stencil->num_indices);
+  if (*pos >= stencil->offsets[i+1])
+    return false;
+  int k = stencil->offsets[i+*pos];
+  *j = stencil->indices[k];
+  *weight = stencil->weights[k];
+  ++(*pos);
+  return true;
+}
+
+#endif
