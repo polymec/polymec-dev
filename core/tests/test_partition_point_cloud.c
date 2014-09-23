337c4ec42de27857b4915423a9b318d2cdf0a77f
Jeffrey Johnson <jnjohnson@lbl.gov>
2014-09-23 13:37:18 -0700

    Adding a unit test for partitioning point clouds.

---
+++ core/tests/test_partition_point_cloud.c
@@ -0,0 +1,97 @@
+// Copyright (c) 2012-2014, Jeffrey N. Johnson
+// All rights reserved.
+// 
+// Redistribution and use in source and binary forms, with or without 
+// modification, are permitted provided that the following conditions are met:
+// 
+// 1. Redistributions of source code must retain the above copyright notice, this 
+// list of conditions and the following disclaimer.
+// 
+// 2. Redistributions in binary form must reproduce the above copyright notice, 
+// this list of conditions and the following disclaimer in the documentation 
+// and/or other materials provided with the distribution.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE 
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE 
+// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL 
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR 
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER 
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, 
+// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+#include <stdarg.h>
+#include <stddef.h>
+#include <setjmp.h>
+#include <string.h>
+#include "cmockery.h"
+#include "core/silo_file.h"
+#include "core/repartition.h"
+
+void test_partition_linear_cloud(void** state)
+{
+  // Create a 100x1x1 uniform point cloud.
+  int N = 100;
+  real_t dx = 1.0/N;
+  point_cloud_t* cloud = point_cloud_new(MPI_COMM_SELF, N);
+  for (int i = 0; i < N; ++i)
+    cloud->points[i].x = (0.5+i)*dx;
+
+  // Partition it.
+  MPI_Comm comm = MPI_COMM_WORLD;
+  exchanger_t* distributor = partition_point_cloud(&cloud, comm, NULL, 0.05);
+  exchanger_free(distributor);
+
+  // Check the number of points on each domain.
+  int rank, nprocs;
+  MPI_Comm_rank(comm, &rank);
+  MPI_Comm_size(comm, &nprocs);
+  if (rank < (nprocs-1))
+    assert_int_equal(N/nprocs, cloud->num_points);
+  else
+    assert_int_equal(N - rank*(N/nprocs), cloud->num_points);
+
+  // Now check data.
+  int first_point = rank*(N/nprocs);
+  for (int i = 0; i < cloud->num_points; ++i)
+  {
+    int j = first_point + i;
+    real_t x = cloud->points[i].x;
+    real_t y = cloud->points[i].y;
+    real_t z = cloud->points[i].z;
+    assert_true(fabs(x - (0.5+j)*dx) < 1e-6);
+    assert_true(fabs(y) < 1e-6);
+    assert_true(fabs(z) < 1e-6);
+  }
+
+  // Plot it.
+  double p[cloud->num_points];
+  for (int i = 0; i < cloud->num_points; ++i)
+    p[i] = 1.0*rank;
+  silo_file_t* silo = silo_file_new(comm, "linear_cloud_partition", "linear_cloud_partition", 1, 0, 0, 0.0);
+  silo_file_write_point_cloud(silo, "cloud", cloud);
+  silo_file_write_scalar_point_field(silo, "rank", "cloud", p);
+  silo_file_close(silo);
+
+  // Clean up.
+  point_cloud_free(cloud);
+
+  // Superficially check that the file is okay.
+  int num_files, num_procs;
+  assert_true(silo_file_query("linear_cloud_partition", "linear_cloud_partition",
+                              &num_files, &num_procs, NULL));
+  assert_int_equal(1, num_files);
+  assert_int_equal(nprocs, num_procs);
+}
+
+int main(int argc, char* argv[]) 
+{
+  polymec_init(argc, argv);
+  const UnitTest tests[] = 
+  {
+    unit_test(test_partition_linear_cloud)
+  };
+  return run_tests(tests);
+}
