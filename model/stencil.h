6e6998efe74fbd21062f4835a4b3ced0b4317a4a
Jeffrey Johnson <jnjohnson@lbl.gov>
2014-09-23 19:11:59 -0700

    Added stencil_num_indices().

--- model/stencil.h
+++ model/stencil.h
@@ -73,6 +73,12 @@ int stencil_start_exchange(stencil_t* stencil, void* data, int stride, int tag, 
 // this stencil. This method has the same signature as exchanger_finish_exchange().
 void stencil_finish_exchange(stencil_t* stencil, int token);
 
+// Returns the number of indices for which the stencil has data.
+static inline int stencil_num_indices(stencil_t* stencil)
+{
+  return stencil->num_indices;
+}
+
 // Returns the number of indices in the stencil for the given index i.
 static inline int stencil_size(stencil_t* stencil, int i)
 {
