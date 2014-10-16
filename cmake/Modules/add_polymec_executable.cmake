3086024707287c6ad77ff0875f78158e15a34b7f
Jeffrey Johnson <jnjohnson@lbl.gov>
2014-10-15 17:02:38 -0700

    Changed POLYMEC_LIBS -> POLYMEC_LIBRARIES everywhere.

--- cmake/Modules/add_polymec_executable.cmake
+++ cmake/Modules/add_polymec_executable.cmake
@@ -2,6 +2,6 @@
 function(add_polymec_executable exe)
   add_executable(${exe} ${ARGN})
 #  set_target_properties(${exe} PROPERTIES OUTPUT_NAME ${exe} LINKER_LANGUAGE CXX)
-  target_link_libraries(${exe} ${POLYMEC_LIBS})
+  target_link_libraries(${exe} ${POLYMEC_LIBRARIES})
 endfunction(add_polymec_executable)
 
