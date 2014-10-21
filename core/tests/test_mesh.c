876840140c1b6f0c11f331ef5b2c2d83ae6ad833
Jeffrey Johnson <jnjohnson@lbl.gov>
2014-10-20 18:44:08 -0700

    Fixed flakiness of mesh unit test.

--- core/tests/test_mesh.c
+++ core/tests/test_mesh.c
@@ -28,11 +28,12 @@
 #include <string.h>
 #include "cmockery.h"
 #include "core/mesh.h"
+#include "geometry/create_uniform_mesh.h"
 
 void test_single_cell_mesh_no_topo(void** state)
 {
   // Create a single hexahedron without topology.
-  mesh_t* mesh = mesh_new_with_cell_type(MPI_COMM_WORLD, 1, 0, 6, 8, 6, 4);
+  mesh_t* mesh = mesh_new_with_cell_type(MPI_COMM_SELF, 1, 0, 6, 8, 6, 4);
   assert_int_equal(1, mesh->num_cells);
   assert_int_equal(0, mesh->num_ghost_cells);
   assert_int_equal(6, mesh->num_faces);
@@ -43,8 +44,11 @@ void test_single_cell_mesh_no_topo(void** state)
 
 void test_single_cell_mesh_serialization(void** state)
 {
-  // Create a single hexahedron without topology.
-  mesh_t* mesh1 = mesh_new_with_cell_type(MPI_COMM_WORLD, 1, 0, 6, 8, 6, 4);
+  // Create a single hexahedron.
+  bbox_t bbox = {.x1 = 0.0, .x2 = 1.0, .y1 = 0.0, .y2 = 1.0, .z1 = 0.0, .z2 = 1.0};
+  mesh_t* mesh1 = create_uniform_mesh(MPI_COMM_SELF, 1, 1, 1, &bbox);
+
+  // Now serialize!
   serializer_t* s = mesh_serializer();
   byte_array_t* buffer = byte_array_new();
   size_t offset = 0;
@@ -52,6 +56,7 @@ void test_single_cell_mesh_serialization(void** state)
   offset = 0;
   mesh_t* mesh2 = serializer_read(s, buffer, &offset);
   byte_array_free(buffer);
+
   assert_int_equal(1, mesh2->num_cells);
   assert_int_equal(0, mesh2->num_ghost_cells);
   assert_int_equal(6, mesh2->num_faces);
