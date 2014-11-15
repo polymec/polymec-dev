2217252cc3b6f9b27928a0f75ead18d412b87b35
Jeffrey Johnson <jnjohnson@lbl.gov>
2014-11-14 19:50:27 -0800

    Added a missing header file.

--- model/interpreter_register_model_functions.h
+++ model/interpreter_register_model_functions.h
@@ -22,36 +22,13 @@
 // OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-#include "model/interpreter_register_model_functions.h"
-#include "model/periodic_bc.h"
+#ifndef POLYMEC_INTERPRETER_REGISTER_MODEL_FUNCTIONS_H
+#define POLYMEC_INTERPRETER_REGISTER_MODEL_FUNCTIONS_H
 
-// Lua stuff.
-#include "lua.h"
-#include "lualib.h"
-#include "lauxlib.h"
+#include "core/interpreter.h"
 
-// Creates a periodic boundary condition from a pair of tags.
-static int periodic_bc(lua_State* lua)
-{
-  // Check the argument.
-  int num_args = lua_gettop(lua);
-  if (num_args != 2)
-    return luaL_error(lua, "Arguments must be 2 boundary mesh (face) tags.");
+// This function registers model-related helper functions for the
+// interpreter.
+void interpreter_register_model_functions(interpreter_t* interp);
 
-  for (int i = 1; i <= 3; ++i)
-  {
-    if (!lua_isstring(lua, i))
-      return luaL_error(lua, "Argument %d must be a face tag.", i);
-  }
-
-  const char* tag1 = lua_tostring(lua, 1);
-  const char* tag2 = lua_tostring(lua, 2);
-  periodic_bc_t* bc = periodic_bc_new(tag1, tag2);
-  lua_pushuserdefined(lua, bc, NULL);
-  return 1;
-}
-
-void interpreter_register_model_functions(interpreter_t* interp)
-{
-  interpreter_register_function(interp, "periodic_bc", periodic_bc, NULL);
-}
+#endif
