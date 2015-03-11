f37ed4a8e8816bd388db97177f219f6bbcde8ece
Jeffrey Johnson <jjphatt@gmail.com>
2015-03-11 09:04:27 -0700

    Corrected a "spelling" error.

--- integrators/tests/test_newton_solver.c
+++ integrators/tests/test_newton_solver.c
@@ -22,35 +22,35 @@ extern real_t* foodweb_initial_conditions();
 
 void test_block_jacobi_precond_foodweb_ctor(void** state)
 {
-  newton_solver_t* integ = block_jacobi_precond_foodweb_solver_new();
-  newton_solver_free(integ);
+  newton_solver_t* newton = block_jacobi_precond_foodweb_solver_new();
+  newton_solver_free(newton);
 }
 
 void test_lu_precond_foodweb_ctor(void** state)
 {
-  newton_solver_t* integ = lu_precond_foodweb_solver_new();
-  newton_solver_free(integ);
+  newton_solver_t* newton = lu_precond_foodweb_solver_new();
+  newton_solver_free(newton);
 }
 
 void test_ilu_precond_foodweb_ctor(void** state)
 {
-  newton_solver_t* integ = ilu_precond_foodweb_solver_new();
-  newton_solver_free(integ);
+  newton_solver_t* newton = ilu_precond_foodweb_solver_new();
+  newton_solver_free(newton);
 }
 
-void test_foodweb_solve(void** state, newton_solver_t* integ)
+void test_foodweb_solve(void** state, newton_solver_t* newton)
 {
   // Set up the problem.
-  newton_solver_set_tolerances(integ, 1e-7, 1e-13);
+  newton_solver_set_tolerances(newton, 1e-7, 1e-13);
   real_t* cc = foodweb_initial_conditions();
 
   // Solve it.
   int num_iters;
-  bool solved = newton_solver_solve(integ, 0.0, cc, &num_iters);
+  bool solved = newton_solver_solve(newton, 0.0, cc, &num_iters);
   if (!solved)
   {
     newton_solver_diagnostics_t diagnostics;
-    newton_solver_get_diagnostics(integ, &diagnostics);
+    newton_solver_get_diagnostics(newton, &diagnostics);
     newton_solver_diagnostics_fprintf(&diagnostics, stdout);
   }
   assert_true(solved);
@@ -60,34 +60,34 @@ void test_foodweb_solve(void** state, newton_solver_t* integ)
   // Evaluate the 2-norm of the residual.
   int num_eq = 6*8*8;
   real_t F[num_eq];
-  newton_solver_eval_residual(integ, 0.0, cc, F);
+  newton_solver_eval_residual(newton, 0.0, cc, F);
   real_t L2 = l2_norm(F, num_eq);
   log_info("||F||_L2 = %g\n", L2);
   assert_true(L2 < sqrt(1e-7));
 
-  newton_solver_free(integ);
+  newton_solver_free(newton);
   free(cc);
 }
 
 void test_block_jacobi_precond_foodweb_solve(void** state)
 {
   // Set up the problem.
-  newton_solver_t* integ = block_jacobi_precond_foodweb_solver_new();
-  test_foodweb_solve(state, integ);
+  newton_solver_t* newton = block_jacobi_precond_foodweb_solver_new();
+  test_foodweb_solve(state, newton);
 }
 
 void test_lu_precond_foodweb_solve(void** state)
 {
   // Set up the problem.
-  newton_solver_t* integ = lu_precond_foodweb_solver_new();
-  test_foodweb_solve(state, integ);
+  newton_solver_t* newton = lu_precond_foodweb_solver_new();
+  test_foodweb_solve(state, newton);
 }
 
 void test_ilu_precond_foodweb_solve(void** state)
 {
   // Set up the problem.
-  newton_solver_t* integ = ilu_precond_foodweb_solver_new();
-  test_foodweb_solve(state, integ);
+  newton_solver_t* newton = ilu_precond_foodweb_solver_new();
+  test_foodweb_solve(state, newton);
 }
 
 int main(int argc, char* argv[]) 
