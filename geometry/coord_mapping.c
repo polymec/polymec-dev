a32c0fe3cb39ab79f70cfd1ec508ec82c410df87
Jeffrey Johnson <jjphatt@gmail.com>
2015-10-14 11:50:43 -0700

    Renamed mapping -> coord_mapping, added map_vector method.

---
+++ geometry/coord_mapping.c
@@ -0,0 +1,63 @@
+// Copyright (c) 2012-2015, Jeffrey N. Johnson
+// All rights reserved.
+// 
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#include <gc/gc.h>
+#include "geometry/coord_mapping.h"
+
+struct coord_mapping_t 
+{
+  char* name;
+  void* context;
+  coord_mapping_vtable vtable;
+};
+
+static void coord_mapping_free(void* ctx, void* dummy)
+{
+  coord_mapping_t* mapping = (coord_mapping_t*)ctx;
+  if (mapping->vtable.dtor)
+    free(mapping->context);
+  free(mapping->name);
+}
+
+coord_mapping_t* coord_mapping_new(const char* name, void* context, coord_mapping_vtable vtable)
+{
+  ASSERT(context != NULL);
+  ASSERT(vtable.map != NULL);
+  ASSERT(vtable.jacobian != NULL);
+  coord_mapping_t* m = GC_MALLOC(sizeof(coord_mapping_t));
+  m->name = string_dup(name);
+  m->context = context;
+  m->vtable = vtable;
+  GC_register_finalizer(m, coord_mapping_free, m, NULL, NULL);
+  return m;
+}
+
+const char* coord_mapping_name(coord_mapping_t* mapping)
+{
+  return mapping->name;
+}
+
+void* coord_mapping_context(coord_mapping_t* mapping)
+{
+  return mapping->context;
+}
+
+void coord_mapping_map(coord_mapping_t* mapping, point_t* x, point_t* y)
+{
+  mapping->vtable.map(mapping->context, x, y);
+}
+
+void coord_mapping_map_vector(coord_mapping_t* mapping, point_t* x, vector_t* v, vector_t* v1)
+{
+  mapping->vtable.map_vector(mapping->context, x, v, v1);
+}
+
+void coord_mapping_compute_jacobian(coord_mapping_t* mapping, point_t* x, real_t* J)
+{
+  mapping->vtable.jacobian(mapping->context, x, J);
+}
+
---
+++ geometry/coord_mapping.h
@@ -0,0 +1,65 @@
+// Copyright (c) 2012-2015, Jeffrey N. Johnson
+// All rights reserved.
+// 
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#ifndef POLYMEC_COORD_MAPPING_H
+#define POLYMEC_COORD_MAPPING_H
+
+#include "core/polymec.h"
+#include "core/point.h"
+
+// A coordinate mapping (coord_mapping) is a function that maps a point to 
+// another point -- the domain and the range of this function are points in 
+// 3-dimensional space. Accordingly, this function cannot be homogeneous, and 
+// its derivative is a 3x3 matrix called the Jacobian matrix. These specific 
+// features argue for a class that is more specific than just "a 3-component 
+// sp_func." Objects of this type are garbage-collected.
+typedef struct coord_mapping_t coord_mapping_t;
+
+// A function pointer type for evaluating the function at a point.
+typedef void (*coord_mapping_map_func)(void*, point_t*, point_t*);
+
+// A function pointer type for transforming a vector the Jacobian at a point.
+typedef void (*coord_mapping_map_vector_func)(void*, point_t*, vector_t*, vector_t*);
+
+// A function pointer type for evaluating the (3x3) Jacobian at a point.
+// The Jacobian is stored in column-major order in the (final) array argument.
+typedef void (*coord_mapping_jacobian_func)(void*, point_t*, real_t*);
+
+// A destructor for any given context object.
+typedef void (*coord_mapping_dtor)(void*);
+
+// This virtual table must be implemented by any coord_mapping.
+typedef struct 
+{
+  coord_mapping_map_func              map;
+  coord_mapping_map_vector_func       map_vector;
+  coord_mapping_jacobian_func         jacobian;
+  coord_mapping_dtor                  dtor;
+} coord_mapping_vtable;
+
+// Construct a coord_mapping function from the given context, metadata and vtable.
+coord_mapping_t* coord_mapping_new(const char* name, void* context, coord_mapping_vtable vtable);
+
+// Returns the name of the coord_mapping.
+const char* coord_mapping_name(coord_mapping_t* mapping);
+
+// Returns the context pointer for the given object. Sometimes useful 
+// for implementing specialized interfaces.
+void* coord_mapping_context(coord_mapping_t* mapping);
+
+// Maps the point x to the point y.
+void coord_mapping_map(coord_mapping_t* mapping, point_t* x, point_t* y);
+
+// Maps the vector v to the vector v1 at the point x.
+void coord_mapping_map_vector(coord_mapping_t* mapping, point_t* x, vector_t* v, vector_t* v1);
+
+// Computes the Jacobian at the point x, writing its components (in column-
+// major order) to the array J.
+void coord_mapping_compute_jacobian(coord_mapping_t* mapping, point_t* x, real_t* J);
+
+#endif
+
