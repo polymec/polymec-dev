307e7f1083739fcf77ca71135169cc5da0c8c0fe
Jeffrey Johnson <jjphatt@gmail.com>
2016-01-08 22:40:47 -0800

    Removing build debug message.

--- cmake/Modules/set_up_platform.cmake
+++ cmake/Modules/set_up_platform.cmake
@@ -20,7 +20,6 @@ macro(set_up_platform)
   # this macro is executed, and so PROJECT_BINARY_DIR is unavailable.
   set(Z_LIBRARY "${CMAKE_CURRENT_BINARY_DIR}/lib/libz.a")
   set(HDF5_LIBRARY "${CMAKE_CURRENT_BINARY_DIR}/lib/libhdf5${LIB_SUFFIX}")
-message("HDF5's at ${HDF5_LIBRARY}")
   set(HDF5_HL_LIBRARY "${CMAKE_CURRENT_BINARY_DIR}/lib/libhdf5_hl${LIB_SUFFIX}")
   set(HDF5_LIBRARIES hdf5_hl;hdf5)
   set(HDF5_INCLUDE_DIR "${CMAKE_CURRENT_BINARY_DIR}/include")
