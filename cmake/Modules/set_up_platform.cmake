966f5f9dfe5ef6de66864f6e576c4a497e5e4790
Jeffrey Johnson <jjphatt@gmail.com>
2016-02-19 15:19:37 -0800

    Adding Noel's logic for building Cori.

--- cmake/Modules/set_up_platform.cmake
+++ cmake/Modules/set_up_platform.cmake
@@ -42,7 +42,60 @@ macro(set_up_platform)
   # Get the hostname for this machine. 
   site_name(HOSTNAME)
 
-  if (HOSTNAME MATCHES "edison") # NERSC Edison
+  if (HOSTNAME MATCHES "cori") # NERSC Cori phase1
+    #ndk : I had to "module load cray-hdf5 silo" 
+    #ndk  make config debug=1 mpi=1 prefix=$SCRATCH/polymec
+    # (Intel's compilers don't do C11.).
+    set(CMAKE_C_COMPILER cc)
+    set(CMAKE_CXX_COMPILER CC)
+    set(CMAKE_Fortran_COMPILER ftn)
+
+   #    set(CMAKE_ADJUSTED_C_COMPILER cc -fPIE)
+   #    set(CMAKE_ADJUSTED_C_COMPILER_ID Intel)
+   #    set(CMAKE_ADJUSTED_CXX_COMPILER CC -fPIE)
+   #    set(CMAKE_ADJUSTED_CXX_COMPILER_ID Intel)
+
+    # We are cared for mathematically.
+    set(NEED_LAPACK FALSE)
+
+    # We expect the following libraries to be available.
+    set(Z_LIBRARY /usr/lib64/libz.a)
+
+    # Note that we use the hdf5 module and not cray-hdf5, since the silo 
+    # module (below) is linked against hdf5 and not cray-hdf5.
+    # FIXME: Use hdf5-parallel for parallel builds.
+    # Set up HDF5.
+    if (USE_MPI EQUAL 1)
+      set(HDF5_LOC $ENV{HDF5_DIR})
+      if (NOT HDF5_LOC)
+        message(FATAL_ERROR "HDF5_DIR not found. Please load the hdf5-parallel module.")
+      endif()
+      include_directories(${HDF5_LOC}/include)
+      link_directories(${HDF5_LOC}/lib)
+      set(HDF5_LIBRARY ${HDF5_LOC}/lib/libhdf5_parallel.a)
+      set(HDF5_LIBRARIES hdf5_parallel;hdf5_hl_parallel)
+    else()
+      set(HDF5_LOC $ENV{HDF5_DIR})
+      if (NOT HDF5_LOC)
+        message(FATAL_ERROR "HDF5_DIR not found. Please load the hdf5 module.")
+      endif()
+      include_directories(${HDF5_LOC}/include)
+      list(APPEND EXTRA_LINK_DIRECTORIES ${HDF5_LOC}/lib)
+      link_directories(${HDF5_LOC}/lib)
+      set(HDF5_LIBRARY ${HDF5_LOC}/lib/libhdf5.a)
+    endif()
+
+    set(SILO_LOC $ENV{SILO_DIR})
+    if (NOT SILO_LOC)
+      message(FATAL_ERROR "SILO_DIR not found. Please load the silo module.")
+    endif()
+    include_directories(${SILO_LOC}/include)
+    link_directories(${SILO_LOC}/lib)
+    list(APPEND EXTRA_LINK_DIRECTORIES ${SILO_LOC}/lib)
+    set(SILO_LIBRARY ${SILO_LOC}/lib/libsiloh5.a)
+    set(SILO_LIBRARIES siloh5)
+
+  elseif (HOSTNAME MATCHES "edison") # NERSC Edison
     # Edison likes Intel's compilers
     # (but Intel's compilers don't do C11.).
     set(CMAKE_C_COMPILER cc)
@@ -93,6 +146,7 @@ macro(set_up_platform)
     list(APPEND EXTRA_LINK_DIRECTORIES ${SILO_LOC}/lib)
     set(SILO_LIBRARY ${SILO_LOC}/lib/libsiloh5.a)
     set(SILO_LIBRARIES siloh5)
+
   elseif(HOSTNAME MATCHES "hopper") # NERSC Hopper
 
     # Hopper is being decommissioned soon (Dec 2015), so we aren't super 
@@ -119,6 +173,7 @@ macro(set_up_platform)
     list(APPEND EXTRA_LINK_DIRECTORIES /opt/pgi/default/linux86-64/default/lib) # built with PGI
     set(SILO_LIBRARY ${SILO_LOC}/lib/libsilo.a)
     set(SILO_LIBRARIES silo;pgc)
+
   endif()
 
 endmacro()
