27e3ddbc76a2be5b8b390df72ddae5de502a6932
Jeffrey Johnson <jjphatt@gmail.com>
2016-06-07 22:18:41 -0700

    PETSc/HYPRE are only build for shared/MPI Travis CI configurations now.

--- .travis/install-linux-deps.sh
+++ .travis/install-linux-deps.sh
@@ -2,7 +2,7 @@
 sudo apt-get update -qq
 sudo apt-get install -y cmake gcc clang libopenmpi-dev openmpi-bin liblapack-dev gfortran git 
 
-# If we're building shared library support, install PETSc and HYPRE.
-if [ $SHARED -eq 1 ]; then
+# If we're building shared library and MPI support, install PETSc and HYPRE.
+if [ $SHARED -eq 1 ] && [ $MPI -eq 1 ]; then
   . ./.travis/install-petsc-and-hypre.sh
 fi 
