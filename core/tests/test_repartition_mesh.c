e7aacb9ba5f28dc53a6a629518af54be5cb2313b
Jeffrey Johnson <jjphatt@gmail.com>
2016-06-15 09:27:33 -0700

    Getting started on more elaborate tests for mesh repartitioning.

--- core/tests/test_repartition_mesh.c
+++ core/tests/test_repartition_mesh.c
@@ -14,6 +14,76 @@
 #include "core/partition_mesh.h"
 #include "geometry/create_uniform_mesh.h"
 
+static void test_migrate_mesh_1_proc(void** state)
+{
+  // Create a mesh.
+  int nx = 10, ny = 10, nz = 10;
+  real_t dx = 1.0/MAX(MAX(1.0/nx, 1.0/ny), 1.0/nz);
+  bbox_t bbox = {.x1 = 0.0, .x2 = nx*dx, .y1 = 0.0, .y2 = ny*dx, .z1 = 0.0, .z2 = nz*dx};
+  mesh_t* mesh = create_uniform_mesh(MPI_COMM_WORLD, nx, ny, nz, &bbox);
+  mesh_verify_topology(mesh, polymec_error);
+
+  int64_t P[mesh->num_cells];
+  memset(P, 0, sizeof(int64_t) * mesh->num_cells);
+  migrator_t* m = migrate_mesh(&mesh, MPI_COMM_WORLD, P);
+  assert_true(m != NULL);
+  migrator_free(m);
+
+  mesh_free(mesh);
+}
+
+#define TEST_MIGRATE_MESH(function_name) \
+static void function_name(void** state) \
+{ \
+  int nprocs; \
+  MPI_Comm_size(MPI_COMM_WORLD, &nprocs); \
+  switch (nprocs) \
+  { \
+    case 1: \
+      test_migrate_mesh_1_proc(state); \
+      break; \
+    case 2: \
+      function_name##_2_proc(state); \
+      break; \
+    case 3: \
+      function_name##_3_proc(state); \
+      break; \
+    case 4: \
+      function_name##_4_proc(state); \
+      break; \
+  } \
+} 
+
+static void test_migrate_4x1x1_mesh_2_proc(void** state)
+{
+  // Create a 4x1x1 mesh on 2 processes.
+  int nx = 2, ny = 1, nz = 1;
+
+  real_t dx = 1.0/MAX(MAX(1.0/nx, 1.0/ny), 1.0/nz);
+  bbox_t bbox = {.x1 = 0.0, .x2 = nx*dx, .y1 = 0.0, .y2 = ny*dx, .z1 = 0.0, .z2 = nz*dx};
+  mesh_t* mesh = create_uniform_mesh(MPI_COMM_WORLD, nx, ny, nz, &bbox);
+  mesh_verify_topology(mesh, polymec_error);
+
+  // We'll swap the cells on the processes.
+  int rank;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  int other = (rank % 2);
+  int64_t P[2] = {other, other};
+  migrator_t* m = migrate_mesh(&mesh, MPI_COMM_WORLD, P);
+  assert_true(m != NULL);
+  migrator_free(m);
+}
+
+static void test_migrate_4x1x1_mesh_3_proc(void** state)
+{
+}
+
+static void test_migrate_4x1x1_mesh_4_proc(void** state)
+{
+}
+
+TEST_MIGRATE_MESH(test_migrate_4x1x1_mesh)
+
 static void test_repartition_uniform_mesh_of_size(void** state, int nx, int ny, int nz)
 {
   int rank;
@@ -110,14 +180,15 @@ int main(int argc, char* argv[])
   polymec_init(argc, argv);
   const struct CMUnitTest tests[] = 
   {
-    cmocka_unit_test(test_repartition_4x1x1_uniform_mesh),
-    cmocka_unit_test(test_repartition_2x2x1_uniform_mesh),
-    cmocka_unit_test(test_repartition_4x4x1_uniform_mesh),
-    cmocka_unit_test(test_repartition_2x2x2_uniform_mesh),
-    cmocka_unit_test(test_repartition_4x4x4_uniform_mesh),
-    cmocka_unit_test(test_repartition_128x1x1_uniform_mesh),
-    cmocka_unit_test(test_repartition_128x128x1_uniform_mesh),
-    cmocka_unit_test(test_repartition_32x32x32_uniform_mesh)
+    cmocka_unit_test(test_migrate_4x1x1_mesh),
+//    cmocka_unit_test(test_repartition_4x1x1_uniform_mesh),
+//    cmocka_unit_test(test_repartition_2x2x1_uniform_mesh),
+//    cmocka_unit_test(test_repartition_4x4x1_uniform_mesh),
+//    cmocka_unit_test(test_repartition_2x2x2_uniform_mesh),
+//    cmocka_unit_test(test_repartition_4x4x4_uniform_mesh),
+//    cmocka_unit_test(test_repartition_128x1x1_uniform_mesh),
+//    cmocka_unit_test(test_repartition_128x128x1_uniform_mesh),
+//    cmocka_unit_test(test_repartition_32x32x32_uniform_mesh)
   };
   return cmocka_run_group_tests(tests, NULL, NULL);
 }
