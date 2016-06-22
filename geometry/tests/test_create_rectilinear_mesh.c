bae456509ab5899887a860118796e230efd46162
Jeffrey Johnson <jjphatt@gmail.com>
2016-06-21 21:50:06 -0700

    Got started on a test of the 3-process rectlinear mesh.

--- geometry/tests/test_create_rectilinear_mesh.c
+++ geometry/tests/test_create_rectilinear_mesh.c
@@ -69,13 +69,82 @@ void test_plot_rectilinear_mesh(void** state)
   mesh_free(mesh);
 }
 
+void test_create_4x4x1_rectilinear_mesh(void** state)
+{
+  // Create a 4x4x1 rectilinear mesh, which isn't a big deal in and of itself, 
+  // but does seem to exhibit problems on certain numbers of processes.
+  double xs[] = {0.0, 1.0, 2.0, 3.0, 4.0};
+  double ys[] = {0.0, 1.0, 2.0, 3.0, 4.0};
+  double zs[] = {0.0, 1.0};
+  mesh_t* mesh = create_rectilinear_mesh(MPI_COMM_WORLD, xs, 5, ys, 5, zs, 2);
+  assert_true(mesh_verify_topology(mesh, polymec_error));
+
+  int nproc, rank;
+  MPI_Comm_size(mesh->comm, &nproc);
+  MPI_Comm_rank(mesh->comm, &rank);
+
+  // Check for correctness on the problematic 3-process case.
+  if (nproc == 3)
+  {
+    // Create global cell indices.
+    index_t vtx_dist[4] = {0, 5, 10, 16};
+    index_t G[mesh->num_cells + mesh->num_ghost_cells];
+    for (int i = 0; i < mesh->num_cells; ++i)
+      G[i] = vtx_dist[rank] + i;
+    exchanger_exchange(mesh_exchanger(mesh), G, 1, 0, MPI_INDEX_T);
+
+    if (rank == 0)
+    {
+      assert_int_equal(5, mesh->num_cells);
+      assert_int_equal(5, mesh->num_ghost_cells);
+
+      // Check the layouts of local and ghost cells.
+      // Note that ghost cells are created by walking through the 
+      // local cells and trying to add ghosts in each of the -/+ x/y/z 
+      // directions (in that order).
+      index_t G0[] = {0, 1, 2, 3, 4, 5, 5, 6, 7, 8};
+      for (int i = 0; i < 10; ++i)
+        assert_int_equal(G0[i], G[i]);
+    }
+    else if (rank == 1)
+    {
+      assert_int_equal(5, mesh->num_cells);
+      assert_int_equal(10, mesh->num_ghost_cells);
+
+      // Check the layouts of local and ghost cells.
+      // Note that ghost cells are created by walking through the 
+      // local cells and trying to add ghosts in each of the -/+ x/y/z 
+      // directions (in that order).
+      index_t G1[] = {5, 6, 7, 8, 9, 1, 2, 3, 10, 4, 10, 4, 11, 12, 13};
+      for (int i = 0; i < 15; ++i)
+        assert_int_equal(G1[i], G[i]);
+    }
+    else if (rank == 2)
+    {
+      assert_int_equal(6, mesh->num_cells);
+      assert_int_equal(5, mesh->num_ghost_cells);
+
+      // Check the layouts of local and ghost cells.
+      // Note that ghost cells are created by walking through the 
+      // local cells and trying to add ghosts in each of the -/+ x/y/z 
+      // directions (in that order).
+      index_t G2[] = {10, 11, 12, 13, 14, 15, 6, 7, 8, 9, 9};
+      for (int i = 0; i < 11; ++i)
+        assert_int_equal(G2[i], G[i]);
+    }
+  }
+
+  mesh_free(mesh);
+}
+
 int main(int argc, char* argv[]) 
 {
   polymec_init(argc, argv);
   const struct CMUnitTest tests[] = 
   {
     cmocka_unit_test(test_create_rectilinear_mesh),
-    cmocka_unit_test(test_plot_rectilinear_mesh)
+    cmocka_unit_test(test_plot_rectilinear_mesh),
+    cmocka_unit_test(test_create_4x4x1_rectilinear_mesh)
   };
   return cmocka_run_group_tests(tests, NULL, NULL);
 }
