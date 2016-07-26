91e21b438edcd48ac92c9d0e0b75a80ce5d6090d
Jeffrey Johnson <jeffjohnson@pobox.com>
2016-07-25 23:08:46 -0700

    Some cleanup and better discovery of MPI.

--- cmake/Modules/add_polymec_test.cmake
+++ cmake/Modules/add_polymec_test.cmake
@@ -6,7 +6,7 @@ function(add_polymec_test exe)
   add_executable(${exe} ${ARGN})
   target_link_libraries(${exe} cmocka ${POLYMEC_LIBRARIES})
   set_target_properties(${exe} PROPERTIES COMPILE_FLAGS "-DCMAKE_CURRENT_SOURCE_DIR=\\\"${CMAKE_CURRENT_SOURCE_DIR}\\\"")
-  add_test(${exe} ${exe})
+  add_test(${exe} ${POLYMEC_TEST_PREFLAGS} ${exe})
   set_tests_properties(${exe} PROPERTIES WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR})
 endfunction()
 
