c54271c347d46567c7ddccc038c23c541854cd2f
Jeffrey Johnson <jjphatt@gmail.com>
2016-08-16 20:33:30 -0700

    Trying again to fix Silo install problem on Travis MPI builds.

--- .travis/install-linux-deps.sh
+++ .travis/install-linux-deps.sh
@@ -1,6 +1,6 @@
 # Install necessary software.
 sudo apt-get update -qq
-sudo apt-get install -y cmake gcc clang libopenmpi-dev openmpi-bin liblapack-dev gfortran git valgrind
+sudo apt-get install -y cmake gcc clang autoconf libopenmpi-dev openmpi-bin liblapack-dev gfortran git valgrind
 
 # If we're building shared library and MPI support, install PETSc and HYPRE.
 if [ $SHARED -eq 1 ] && [ $MPI -eq 1 ]; then
