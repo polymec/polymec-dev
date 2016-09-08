d13538c26a4029e7785d6b96e289a91bc4087487
Jeffrey Johnson <jjphatt@gmail.com>
2016-09-07 22:08:46 -0700

    Explicating compilers in the proper place.

--- .travis/install-linux-deps.sh
+++ .travis/install-linux-deps.sh
@@ -1,7 +1,4 @@
 # Set up the compilers.
-if [ "$CC" = "gcc" ]; then export CC="gcc-5" CXX="g++-5"; fi
-if [ "$CC" = "clang" ]; then export CC="clang-3.6" CXX="clang++-3.6"; fi
-
 # If we're building shared library and MPI support, install PETSc and HYPRE.
 if [ $SHARED -eq 1 ] && [ $MPI -eq 1 ]; then
   . ./.travis/install-petsc-and-hypre.sh
