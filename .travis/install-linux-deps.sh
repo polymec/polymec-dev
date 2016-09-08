8afa0e692b1fbb7fe9319296e180532b7c3e26bf
Jeffrey Johnson <jjphatt@gmail.com>
2016-09-07 22:00:56 -0700

    Reverting to Trusty distribution, using apt.

--- .travis/install-linux-deps.sh
+++ .travis/install-linux-deps.sh
@@ -1,6 +1,6 @@
 # Set up the compilers.
 if [ "$CC" = "gcc" ]; then export CC="gcc-5" CXX="g++-5"; fi
-if [ "$CC" = "clang" ]; then export CC="clang-3.8" CXX="clang++-3.8"; fi
+if [ "$CC" = "clang" ]; then export CC="clang-3.6" CXX="clang++-3.6"; fi
 
 # If we're building shared library and MPI support, install PETSc and HYPRE.
 if [ $SHARED -eq 1 ] && [ $MPI -eq 1 ]; then
