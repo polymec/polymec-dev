f63847cb73e8f2e2708eefbca31d05d5bbf306be
Jeffrey Johnson <jjphatt@gmail.com>
2016-09-07 21:44:27 -0700

    First attempt at big-upping compilers and switching to Travis container builds.

--- .travis/install-linux-deps.sh
+++ .travis/install-linux-deps.sh
@@ -1,6 +1,6 @@
-# Install necessary software.
-sudo apt-get update -qq
-sudo apt-get install --no-install-recommends -y cmake gcc clang libopenmpi-dev openmpi-bin liblapack-dev gfortran git valgrind
+# Set up the compilers.
+if [ "$CC" = "gcc" ]; then export CC="gcc-5" CXX="g++-5"; fi
+if [ "$CC" = "clang" ]; then export CC="clang-3.8" CXX="clang++-3.8"; fi
 
 # If we're building shared library and MPI support, install PETSc and HYPRE.
 if [ $SHARED -eq 1 ] && [ $MPI -eq 1 ]; then
