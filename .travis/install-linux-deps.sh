e2d037b1b20708b417855425f143f9fc5c72f658
Jeffrey N. Johnson <jeffjohnson@pobox.com>
2016-11-26 21:06:41 -0800

    HYPRE and PETSc are now installed for serial shared builds too.

--- .travis/install-linux-deps.sh
+++ .travis/install-linux-deps.sh
@@ -15,7 +15,7 @@ if [ $MPI -eq 1 ]; then
   popd
 fi
 
-# If we're building shared library and MPI support, install PETSc and HYPRE.
-if [ $SHARED -eq 1 ] && [ $MPI -eq 1 ]; then
+# If we're building shared library support, install PETSc and HYPRE.
+if [ $SHARED -eq 1 ]; then
   . ./.travis/install-petsc-and-hypre.sh
 fi 
