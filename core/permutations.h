d91b2fe9a638030fe9f38b27b692580318d464de
Jeffrey N. Johnson <jeffjohnson@pobox.com>
2017-01-07 10:24:53 -0800

    Fixed an include guard.

--- core/permutations.h
+++ core/permutations.h
@@ -5,8 +5,8 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-#ifndef POLYMEC_PERMUTE_H
-#define POLYMEC_PERMUTE_H
+#ifndef POLYMEC_PERMUTATIONS_H
+#define POLYMEC_PERMUTATIONS_H
 
 #include "core/rng.h"
 
