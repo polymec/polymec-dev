b790da7e0369d185718fa60303ca4d612c9e17df
Jeffrey N. Johnson <jeffjohnson@pobox.com>
2017-11-04 20:04:04 -0700

    Added imported libraries as targets for expressing dependencies to exes.

--- cmake/Modules/add_polymec_executable.cmake
+++ cmake/Modules/add_polymec_executable.cmake
@@ -6,5 +6,8 @@ endfunction(add_polymec_executable_with_libs)
 
 function(add_polymec_executable exe)
   add_polymec_executable_with_libs(${exe} "${POLYMEC_LIBRARIES}" ${ARGN})
+
+  # Explicitly add the polymec libraries as dependencies for this executable.
+  add_dependencies(${exe} polymec_model polymec_io polymec_solvers polymec_geometry polymec_core)
 endfunction(add_polymec_executable)
 
