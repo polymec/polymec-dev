5128168d1dbfc1cb3cb4e7db4b91744205d26bf5
Jeffrey N. Johnson <jeffjohnson@pobox.com>
2018-04-20 22:28:45 -0700

    Picking at usilo.

---
+++ 3rdparty/usilo/src/config.h
@@ -0,0 +1,45 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// Lean on HDF5's configuration for everything.
+#include "H5pubconf.h"
+
+#ifdef H5_HAVE_UNISTD_H
+#define HAVE_UNISTD_H H5_HAVE_UNISTD_H
+#endif
+
+#ifdef H5_HAVE_STDLIB_H 
+#define HAVE_STDLIB_H H5_HAVE_STDLIB_H
+#endif
+
+#ifdef H5_HAVE_SYS_TYPES_H 
+#define HAVE_SYS_TYPES_H H5_HAVE_SYS_TYPES_H
+#endif
+
+#ifdef H5_HAVE_SYS_STAT_H 
+#define HAVE_SYS_STAT_H H5_HAVE_SYS_STAT_H
+#endif
+
+#define HAVE_SYS_FCNTL_H 1
+
+#ifdef H5_HAVE_FCNTL_H 
+#define HAVE_FCNTL_H H5_HAVE_FCNTL_H
+#endif
+
+#ifdef H5_HAVE_STRING_H 
+#define HAVE_STRING_H H5_HAVE_STRING_H
+#endif
+
+#ifdef H5_HAVE_STRINGS_H 1
+#define HAVE_STRINGS_H H5_HAVE_STRINGS_H
+#endif
+
+#define HAVE_STRERROR 1
+
+#define HAVE_HDF5_H 1
+#define HAVE_LIBHDF5 1
+#define HAVE_HDF5_DRIVER 1
+#define HAVE_LIBZ 1
+
+
+#endif
