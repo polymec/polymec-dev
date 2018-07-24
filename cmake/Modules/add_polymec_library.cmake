b8b372d3db3d2574fef40145fc9c86a0e5502817
Jeffrey N. Johnson <jeffjohnson@pobox.com>
2018-07-23 19:14:13 -0700

    Removed all_3rdparty_libs dep from add_polymec_library.

--- cmake/Modules/add_polymec_library.cmake
+++ cmake/Modules/add_polymec_library.cmake
@@ -1,6 +1,5 @@
 function(add_polymec_library lib)
   add_library(${lib} ${ARGN})
-  add_dependencies(${lib} all_3rdparty_libs)
   set_target_properties(${lib} PROPERTIES FOLDER Libraries)
   if (BUILD_SHARED_LIBS)
     target_link_libraries(${lib} ${POLYMEC_LIBRARIES})
