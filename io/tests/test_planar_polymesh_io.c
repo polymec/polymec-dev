f7e2f6969b70ae3b39217609f1019711404c4a8b
Jeffrey N. Johnson <jeffjohnson@pobox.com>
2018-09-22 11:30:05 -0700

    Added a hex mesh test to the set of I/O tests for planar polymeshes.

--- io/tests/test_planar_polymesh_io.c
+++ io/tests/test_planar_polymesh_io.c
@@ -12,6 +12,7 @@
 #include "cmocka.h"
 #include "core/array_utils.h"
 #include "geometry/create_quad_planar_polymesh.h"
+#include "geometry/create_hex_planar_polymesh.h"
 #include "io/silo_file.h"
 
 static void test_plot_quad_mesh(void** state)
@@ -46,6 +47,39 @@ static void test_plot_quad_mesh(void** state)
   planar_polymesh_free(mesh);
 }
 
+static void test_plot_hex_mesh(void** state)
+{
+  // Create a set of uniform hexes with the given alignment.
+  size_t radius = 5;
+  real_t h = 0.1;
+  planar_polymesh_t* mesh = create_hex_planar_polymesh(HEX_LATTICE_X_ALIGNED, radius, h);
+
+  // Plot it.
+  silo_file_t* silo = silo_file_new(MPI_COMM_WORLD, "hex_r=5", "", 1, 0, 0.0);
+  silo_file_write_planar_polymesh(silo, "mesh", mesh);
+  silo_file_close(silo);
+
+  // Clean up.
+  planar_polymesh_free(mesh);
+
+  // Now read the mesh from the file.
+  real_t t;
+  silo = silo_file_open(MPI_COMM_WORLD, "hex_r=5", "", 0, &t);
+  assert_true(reals_equal(t, 0.0));
+  assert_true(silo_file_contains_planar_polymesh(silo, "mesh"));
+  mesh = silo_file_read_planar_polymesh(silo, "mesh");
+
+  // Check its numbers.
+  assert_int_equal(10*10, mesh->num_cells);
+  assert_int_equal(2*10*11, mesh->num_edges);
+  assert_int_equal(11*11, mesh->num_nodes);
+
+  silo_file_close(silo);
+
+  // Clean up.
+  planar_polymesh_free(mesh);
+}
+
 int main(int argc, char* argv[]) 
 {
   polymec_init(argc, argv);
@@ -53,7 +87,8 @@ int main(int argc, char* argv[])
   set_log_level(LOG_DEBUG);
   const struct CMUnitTest tests[] = 
   {
-    cmocka_unit_test(test_plot_quad_mesh)
+    cmocka_unit_test(test_plot_quad_mesh),
+    cmocka_unit_test(test_plot_hex_mesh)
   };
   return cmocka_run_group_tests(tests, NULL, NULL);
 }
