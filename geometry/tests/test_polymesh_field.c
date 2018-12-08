7fdec128a6ea860cbbddca7ae9e799ec85e62268
Jeffrey N. Johnson <jeffjohnson@pobox.com>
2018-12-08 09:50:40 -0800

    Added polymesh_field_set_exchanger to unit test.

--- geometry/tests/test_polymesh_field.c
+++ geometry/tests/test_polymesh_field.c
@@ -25,6 +25,7 @@ static void test_single_cell_mesh_no_topo(void** state)
     assert_int_equal(c, fc->num_components);
     assert_int_equal(mesh->num_cells, fc->num_local_values);
     assert_int_equal(mesh->num_ghost_cells, fc->num_ghost_values);
+    polymesh_field_set_exchanger(fc, polymesh_exchanger(mesh, POLYMESH_CELL));
     polymesh_field_free(fc);
 
     polymesh_field_t* ff = polymesh_field_new(mesh, POLYMESH_FACE, c);
@@ -32,6 +33,7 @@ static void test_single_cell_mesh_no_topo(void** state)
     assert_int_equal(c, ff->num_components);
     assert_int_equal(mesh->num_faces, ff->num_local_values);
     assert_int_equal(0, ff->num_ghost_values);
+    polymesh_field_set_exchanger(ff, polymesh_exchanger(mesh, POLYMESH_FACE));
     polymesh_field_free(ff);
 
     polymesh_field_t* fe = polymesh_field_new(mesh, POLYMESH_EDGE, c);
@@ -39,6 +41,7 @@ static void test_single_cell_mesh_no_topo(void** state)
     assert_int_equal(c, fe->num_components);
     assert_int_equal(mesh->num_edges, fe->num_local_values);
     assert_int_equal(0, fe->num_ghost_values);
+    polymesh_field_set_exchanger(fe, polymesh_exchanger(mesh, POLYMESH_EDGE));
     polymesh_field_free(fe);
 
     polymesh_field_t* fn = polymesh_field_new(mesh, POLYMESH_NODE, c);
@@ -46,6 +49,7 @@ static void test_single_cell_mesh_no_topo(void** state)
     assert_int_equal(c, fn->num_components);
     assert_int_equal(mesh->num_nodes, fn->num_local_values);
     assert_int_equal(0, fn->num_ghost_values);
+    polymesh_field_set_exchanger(fn, polymesh_exchanger(mesh, POLYMESH_NODE));
     polymesh_field_free(fn);
   }
   polymesh_free(mesh);
