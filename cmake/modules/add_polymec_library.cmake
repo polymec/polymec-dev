d5ceb24f27ba0d76a9e6b4572539d5143820e2e9
Jeffrey N. Johnson <jeffjohnson@pobox.com>
2019-02-18 20:07:10 -0800

    Got rid of a bad transitive dependency.

--- cmake/modules/add_polymec_library.cmake
+++ cmake/modules/add_polymec_library.cmake
@@ -2,6 +2,5 @@ function(add_polymec_library lib)
   add_library(${lib} ${ARGN})
   set_target_properties(${lib} PROPERTIES FOLDER Libraries)
   target_link_libraries(${lib} ${POLYMEC_LIBRARIES})
-  add_dependencies(${lib} all_3rdparty_libs)
 endfunction(add_polymec_library)
 
